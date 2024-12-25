4cab27d297f454e769fa35f04d7f673d97664a68
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2024-12-25 02:47:33 -0500

    Time: 19 ms (14.83%), Space: 12.5 MB (5.85%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000} ## Decalare the roman representation to refer and compute the value from
+
+        result = 0 ## Value to store the result
+
+        for index in range(len(s)): #3 Looping through the elements in the string
+            if index + 1 < len(s) and roman[s[index]] < roman[s[index + 1]]:
+                # 1. Check if the next index is not out of range 
+                # 2. check if its ascending or descending value 
+                result -= roman[s[index]]
+            else:
+                result += roman[s[index]]
+        return result
