e7d97b668d8a378e3f8c7a4ef94004a51aaaca0b
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2024-12-25 02:50:38 -0500

    Update 0013-roman-to-integer.py

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -16,3 +16,28 @@ class Solution(object):
             else:
                 result += roman[s[index]]
         return result
+
+## Fastest result code 
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        value_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+            }
+
+        total = 0
+        prev = 0
+
+        for i in reversed(s):
+            cur = value_map[i]
+            if cur < prev:
+                total -= cur
+            else:
+                total += cur
+            prev = cur
+
+        return total
