96f77eb2a58f822de982ae441aca41b270518296
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-01-07 11:14:46 -0500

    Time: 43 ms (49.18%), Space: 13.9 MB (40.96%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -9,12 +9,11 @@ class Solution(object):
 
         max_ = max(arr[1:])
 
-        for i in range(len(arr)-1):
+        for i in range(0,len(arr)-1, 1):
             if arr[i] != max_:
                 arr[i] = max_
             else:
                 max_ = max(arr[i+1:])
                 arr[i] = max_
         arr[-1] = -1
-
         return arr
