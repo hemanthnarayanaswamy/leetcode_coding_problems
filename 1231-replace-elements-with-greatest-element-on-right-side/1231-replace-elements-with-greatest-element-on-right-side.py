3a272864c1bf4bf17a1c076d70763afbb7fdc8f1
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-01-07 11:01:26 -0500

    Update 1231-replace-elements-with-greatest-element-on-right-side.py

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -16,3 +16,22 @@ class Solution(object):
                 m = temp
             i -= 1 # Iterate to next element 
         return arr
+
+### Fastest Solution 
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        if len(arr) == 1:
+            return [-1]
+        max_ = max(arr[1:])
+        for i in range(0, len(arr)-1, 1):
+            if arr[i] != max_:
+                arr[i] = max_
+            else:
+                max_ = max(arr[i+1:])
+                arr[i] = max_
+        arr[-1] = -1
+        return arr
