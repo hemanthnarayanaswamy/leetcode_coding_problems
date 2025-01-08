c78a834bec27a57090af455c0db52f680949c9ea
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-01-08 13:51:04 -0500

    Time: 3 ms (37.14%), Space: 12.6 MB (10.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,11 +5,10 @@ class Solution(object):
         :rtype: str
         """
         result = ""
-        for i in range(len(strs[0])): # Taking the first letter as reference # Taking first words letters as reference to compare it with all other letters 
-            for str in strs: #  Now iterating through each letter and checking if our refercnce letter is same as the one in the letters 
+        for i in range(len(strs[0])):
+            for str in strs:
                 if i == len(str) or strs[0][i] != str[i]:
-                    # if the i is out of range because we are using first letter it can be large or small compared to other letters if its large than we'll encounter the out of bound error so that condition and checking the letter is same or not.
-                    return result 
+                    return result
             result += strs[0][i]
+        
         return result
-    
