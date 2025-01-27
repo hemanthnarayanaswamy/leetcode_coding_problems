dc8c14c2ad71217539d5449ac842d97fae044620
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-01-26 21:27:52 -0500

    Time: 33 ms (57.31%), Space: 28.9 MB (73.87%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -4,13 +4,13 @@ class Solution:
         array_len = len(arr)
         for i in range(array_len):
             if i+1 < array_len:
-                if arr[i] > arr[i+1]:
+                current_ele, next_ele = arr[i], arr[i+1]
+                if current_ele > next_ele:
                     is_decreasing = False
-                if arr[i] < arr[i+1]:
+                if current_ele < next_ele:
                     is_increasing = False
-                if arr[i] == arr[i+1]:
+                if current_ele == next_ele:
                     is_decreasing, is_increasing = is_decreasing, is_increasing
-            print(is_decreasing, is_increasing)
             if is_decreasing == False and is_increasing == False:
                 return False
         return True
