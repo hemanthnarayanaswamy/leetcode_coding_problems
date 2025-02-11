9cec63b1bfa4f2b486975d9419e057abf2b0a845
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-02-11 11:49:47 -0500

    Time: 12 ms (65.45%), Space: 18.6 MB (89.44%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
@@ -1,15 +1,16 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        if len(nums) < 3: ## If the given lengths are less than 3 sides return 0
+        nums_lenght = len(nums)
+        if nums_lenght < 3: ## If the given lengths are less than 3 sides return 0
             return 0
     
         nums.sort() ## sorting the array 
-        print(nums)
 
-        for i in range(len(nums)-1, -1, -1): ## We are starting from the largest side
-            print(nums[i], nums[i-1], nums[i-2])
-            if i-2 >= 0 and  nums[i] < nums[i-1] + nums[i-2]:
-                return nums[i] + nums[i-1] + nums[i-2]
+        for i in range(nums_lenght-1, -1, -1): ## We are starting from the largest side
+            sum_of_sides = nums[i-1] + nums[i-2]
+            largest_side = nums[i]
+            if i-2 >= 0 and largest_side < sum_of_sides:
+                return largest_side + sum_of_sides
         return 0
 
         
