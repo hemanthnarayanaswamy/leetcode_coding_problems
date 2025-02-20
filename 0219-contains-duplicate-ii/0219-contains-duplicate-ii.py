b7ea9f3593ad7150428e9971a81c71417a78e6d9
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-02-20 11:31:44 -0500

    Time: 20 ms (65.1%), Space: 30.7 MB (43.17%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -2,8 +2,8 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         found_nums = {} ## HAsh Map to track the number
 
-        if len(nums) == len(set(nums)): ## A small edge case to resolve much faster
-            return False
+        # if len(nums) == len(set(nums)): ## A small edge case to resolve much faster
+        #     return False
 
         for i,num in enumerate(nums):
             if num in found_nums and abs(i-found_nums[num]) <= k: # checking conditions 
