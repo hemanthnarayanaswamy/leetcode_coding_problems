c9e678d2c3f08bdf4ac1ca7ac0186915dfe5eb2b
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-02-27 12:16:46 -0500

    Time: 592 ms (92.27%), Space: 33 MB (72.77%) - LeetHub

--- 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/1787-sum-of-absolute-differences-in-a-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(nums)
         result = [0] * n
         
-        for i in range(len(nums)):
+        for i in range(n):
             right_sum -= nums[i]
             result[i] = (nums[i]*i - left_sum) + (right_sum - nums[i] * (n-i-1))
             left_sum += nums[i]
