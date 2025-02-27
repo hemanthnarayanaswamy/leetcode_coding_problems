779b013d2e75420c4bdd90960e857feea51e24ce
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-02-27 14:37:49 -0500

    Time: 0 ms (100%), Space: 17.7 MB (71.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,7 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2 
-        nums1.sort()
-        
+        p1, p2, insert_pointer = m-1, n-1, m+n-1 
+
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[insert_pointer] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[insert_pointer] = nums2[p2]
+                p2 -= 1
+            insert_pointer -= 1
+
+            
         
