16653835413cdb8081c5f6185acfb95935b32e87
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-02-28 10:26:49 -0500

    Time: 47 ms (81.69%), Space: 18.1 MB (63.49%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+        
+        min_penalty = float('inf')  # Store the minimum penalty found
+        best_time = 0  # Store the best closing time
+        penalty = customers.count('Y')  # Initial penalty if the shop closes at j = 0
+
+        for j in range(n + 1):  # Check all possible closing times (0 to n)
+            if penalty < min_penalty:
+                min_penalty = penalty
+                best_time = j  # Store the earliest minimum penalty closing hour
+            
+            if j < n:
+                # If shop is open at 'N' (no customers), increase penalty
+                # If shop is closed at 'Y' (customers inside), decrease penalty
+                penalty += -1 if customers[j] == 'Y' else 1  
+            
+        return best_time
