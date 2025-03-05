e51487b32e34c54f9906e2d621ef7053aa6094b8
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-05 11:50:59 -0500

    Time: 7 ms (96.06%), Space: 20.2 MB (20.34%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -5,7 +5,8 @@ class Solution:
             if num == 1:
                 current_count += 1  # Increment count for consecutive 1s
             else:
-                max_count = max(max_count, current_count)  # Update max before resetting
+                if max_count < current_count:
+                    max_count = current_count  # Update max before resetting
                 current_count = 0  # Reset counter on encountering 0
 
         return max(max_count, current_count)  # Final check for ending with 1s
