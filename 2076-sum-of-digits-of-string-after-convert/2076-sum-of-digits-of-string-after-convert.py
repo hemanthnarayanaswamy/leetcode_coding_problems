499e3abd51c941da91f6836ff48b48a3c57ebd8e
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-06 11:10:50 -0500

    Time: 2 ms (74.77%), Space: 18 MB (15.93%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -1,18 +1,15 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         result = []
-        temp_sum, final_sum = 0, 0
-        
+        compute_sum = 0
         for char in s:
             result.append(str(ord(char)-96))
         
         result = ''.join(result)
 
-        while k > 0:
-            temp_sum = sum([int(i) for i in result])
-            final_sum = temp_sum
-            result = str(temp_sum)
-            temp_sum = 0
-            k -= 1
-        return final_sum
+        for _ in range(k):
+            compute_sum = sum(int(i) for i in result)
+            result = str(compute_sum)
+
+        return compute_sum
         
