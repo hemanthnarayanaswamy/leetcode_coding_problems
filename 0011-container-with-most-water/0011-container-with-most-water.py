e34e4f6ea185e23a0d0b344c8e9d50d83f5b7209
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-10 11:18:39 -0400

    Time: 76 ms (83.38%), Space: 28.5 MB (24.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,7 +3,7 @@ class Solution:
         result = 0
         l, r = 0, len(heights)-1 
         while l < r:
-            max_area = min(heights[l], heights[r])*abs(r-l)
+            max_area = min(heights[l], heights[r])*(r-l)
             if result < max_area:
                 result = max_area
             if heights[l] > heights[r]:
