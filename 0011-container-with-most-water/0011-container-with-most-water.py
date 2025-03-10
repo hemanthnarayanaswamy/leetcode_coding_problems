80f1440760677021a528527966491e5c81467f6e
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-10 11:12:25 -0400

    Time: 75 ms (84.61%), Space: 28.6 MB (24.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,7 +3,6 @@ class Solution:
         result = []
         l, r = 0, len(heights)-1 
         while l < r:
-            print(heights[l], heights[r],r,l, '------>', min(heights[l], heights[r])*(r-l))
             result.append(min(heights[l], heights[r])*abs(r-l))
             if heights[l] > heights[r]:
                 r -= 1
