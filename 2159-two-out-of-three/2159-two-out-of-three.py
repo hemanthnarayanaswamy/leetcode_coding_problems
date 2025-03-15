66693a9a02e3001cf4b15ef80f964277b37af06e
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-15 14:04:27 -0400

    Time: 4 ms (42.33%), Space: 17.9 MB (35.62%) - LeetHub

--- 2159-two-out-of-three/2159-two-out-of-three.py
+++ 2159-two-out-of-three/2159-two-out-of-three.py
@@ -5,13 +5,9 @@ class Solution:
         nums2 = set(nums2)
         nums3 = set(nums3)
         for i in nums1:
-            print(i)
             if i in nums2 or i in nums3:
                 s.add(i)
         for j in nums2:
-            if j in nums1 or j in nums3:
+            if j in nums3:
                 s.add(j)
-        for k in nums3:
-            if k in nums1 or k in nums2:
-                s.add(k)
         return list(s)
