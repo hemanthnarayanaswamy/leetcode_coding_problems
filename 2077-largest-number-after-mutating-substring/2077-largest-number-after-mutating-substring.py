18e5216007d8f48b6606db4c0ccadf7826e4ff5e
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-19 10:26:26 -0400

    Time: 141 ms (34.34%), Space: 25.4 MB (18.69%) - LeetHub

--- 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.py
+++ 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.py
@@ -1,15 +1,15 @@
 class Solution:
     def maximumNumber(self, num: str, change: List[int]) -> str:
-        change_map = {i:num for i, num in enumerate(change)}
+        #change_map = {i:num for i, num in enumerate(change)}
         num = [s for s in num] 
         i = 0 
         while i < len(num):
-            if int(num[i]) < change_map[int(num[i])]:
+            if int(num[i]) < change[int(num[i])]:
                 while i < len(num):
-                    if int(num[i]) > change_map[int(num[i])]:
+                    if int(num[i]) > change[int(num[i])]:
                         return ''.join(num)
                     else:
-                        num[i] = str(change_map[int(num[i])])
+                        num[i] = str(change[int(num[i])])
                         i += 1
             i += 1               
 
