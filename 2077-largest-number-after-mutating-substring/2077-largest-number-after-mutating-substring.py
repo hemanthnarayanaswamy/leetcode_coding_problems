39335ee134302208729ac2053a63069f75c7df27
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-19 10:45:23 -0400

    Time: 75 ms (81.82%), Space: 25.2 MB (57.58%) - LeetHub

--- 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.py
+++ 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.py
@@ -1,17 +1,17 @@
 class Solution:
     def maximumNumber(self, num: str, change: List[int]) -> str:
-        num_list = list(num)  
+        num = list(num)  
         mutation_started = False  # Flag to track mutation start
 
-        for i in range(len(num_list)):
-            digit = int(num_list[i])
+        for i in range(len(num)):
+            digit = int(num[i])
 
             if change[digit] > digit:  # Start mutation if change[digit] > digit
-                num_list[i] = str(change[digit])
+                num[i] = str(change[digit])
                 mutation_started = True
             elif change[digit] < digit and mutation_started:  
                 # Stop as soon as we see a decreasing transformation
                 break
-            # If change[digit] == digit, we can continue mutation
 
-        return ''.join(num_list)  # Convert list back to string
+
+        return ''.join(num)  # Convert list back to string
