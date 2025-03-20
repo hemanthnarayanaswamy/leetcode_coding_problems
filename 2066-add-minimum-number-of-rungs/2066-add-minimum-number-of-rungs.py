8b443da91eb82eaae262a74824e640bf860046ca
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-20 09:36:19 -0400

    Time: 13 ms (80.95%), Space: 31.1 MB (64.07%) - LeetHub

--- 2066-add-minimum-number-of-rungs/2066-add-minimum-number-of-rungs.py
+++ 2066-add-minimum-number-of-rungs/2066-add-minimum-number-of-rungs.py
@@ -1,16 +1,11 @@
 class Solution:
     def addRungs(self, rungs: List[int], dist: int) -> int:
-        if not rungs:
-            return 0
-            
-        i = 0       # Track Steps
-        addons = 0  ## To track number of inserctions
-        current_step = 0 ## Current Step
-        while current_step != rungs[-1]:
-            next_step = rungs[i]
-            if abs(next_step - current_step)  > dist:
-                addons += ((next_step - current_step) - 1) // dist
-            current_step = next_step
-            i += 1
+        addons = 0
+        previous = 0
+        for current in rungs:
+            diff = current - previous
+            if diff > dist:
+                addons += (diff -1) // dist
+            previous = current
         return addons
         
