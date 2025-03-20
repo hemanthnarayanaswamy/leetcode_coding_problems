c322e61e84b6ae2a9a00e96b0b3244681b30b1a0
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-20 09:31:03 -0400

    Time: 27 ms (27.71%), Space: 31.1 MB (64.07%) - LeetHub

--- 2066-add-minimum-number-of-rungs/2066-add-minimum-number-of-rungs.py
+++ 2066-add-minimum-number-of-rungs/2066-add-minimum-number-of-rungs.py
@@ -3,15 +3,11 @@ class Solution:
         i = 0
         addons = 0
         current_step = 0
-        while current_step != rungs[-1] and i < len(rungs):
+        while current_step != rungs[-1]:
             next_step = rungs[i]
-            ## print(current_step, next_step, i)
-            if abs(next_step - current_step)  <= dist:
-                current_step = next_step
-                i += 1
-            else:
+            if abs(next_step - current_step)  > dist:
                 addons += ((next_step - current_step) - 1) // dist
-                current_step = next_step
-                i += 1
+            current_step = next_step
+            i += 1
         return addons
         
