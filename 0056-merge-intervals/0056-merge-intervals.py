a8f7db55f73e7b85b9a07cd36c20e5cbf27c166c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-21 10:32:59 -0400

    Time: 8 ms (49.95%), Space: 21 MB (62.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,8 +3,9 @@ class Solution:
         intervals.sort()
         result = [intervals[0]]  ## Initiating result with first element of intervals for references 
         for i in range(1, len(intervals)):
-            if result[-1][1] >= intervals[i][0]:
-                result[-1][1] = max(result[-1][1],intervals[i][1])
+            current_element = result[-1][1]
+            if current_element >= intervals[i][0]:
+                result[-1][1] = max(current_element,intervals[i][1])
             else:
                 result.append(intervals[i])
         return result
