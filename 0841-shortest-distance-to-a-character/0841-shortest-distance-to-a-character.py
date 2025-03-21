305ac4de4caa7338ce46a597f623518f6003f844
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-21 11:55:41 -0400

    Time: 18 ms (29.05%), Space: 17.8 MB (45.05%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
@@ -2,12 +2,13 @@ class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         result = []
         track_index = set()
+        n = len(s)
 
-        for i in range(len(s)):
+        for i in range(n):
             if s[i] == c:
                 track_index.add(i)
         
-        for i in range(len(s)):
+        for i in range(n):
             diff_val = min(abs(i - x) for x in track_index)
             result.append(diff_val)
 
