2ee0981e0b99ca33335306b32832a456516cacbc
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-21 10:34:44 -0400

    Time: 8 ms (49.95%), Space: 20.9 MB (89.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,6 +1,8 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
+        # intervals.sort()
+        #sort the intervals by the first values
+        intervals.sort(key = lambda i: i[0])
         result = [intervals[0]]  ## Initiating result with first element of intervals for references 
         for i in range(1, len(intervals)):
             current_element = result[-1][1]
@@ -8,5 +10,6 @@ class Solution:
                 result[-1][1] = max(current_element,intervals[i][1])
             else:
                 result.append(intervals[i])
+
         return result
         
