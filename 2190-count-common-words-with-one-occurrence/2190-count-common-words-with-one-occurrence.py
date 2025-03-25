e9c7817aad2b5eb81e70ebbc6210db10ca0b6e4b
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-25 10:24:45 -0400

    Time: 0 ms (100%), Space: 18.1 MB (68.11%) - LeetHub

--- 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.py
@@ -5,7 +5,6 @@ class Solution:
         count = 0
         words1_freq = Counter(words1)
         words2_freq = Counter(words2)
-        print(words1_freq,words2_freq )
 
         for word in words1_freq:
             if word not in words2_freq:
