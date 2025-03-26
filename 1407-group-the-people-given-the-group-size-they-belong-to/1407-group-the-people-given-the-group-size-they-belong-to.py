35fd3ecac7a42f227dc351e507300a2dc6246f0f
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-26 09:56:17 -0400

    Time: 3 ms (62.6%), Space: 18.1 MB (27.46%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,10 +1,13 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        result = defaultdict(list)
+        result = {}
         groups = []
         
-        for i, size in enumerate(groupSizes):
-            result[size].append(i)
+        for i in range(len(groupSizes)):
+            if groupSizes[i] not in result:
+                result[groupSizes[i]] = [i]
+            else:
+                result[groupSizes[i]].append(i)
 
         for size, people in result.items():
             for i in range(0, len(people), size):
