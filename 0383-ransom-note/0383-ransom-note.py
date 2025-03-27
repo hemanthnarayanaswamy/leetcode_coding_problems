b64e584ef2648d334b153004567d967736fdc7f4
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-27 09:57:43 -0400

    Time: 7 ms (92.59%), Space: 17.9 MB (82.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         ransomNote_freq = Counter(ransomNote)
-        magazine_freq = Counter(magazine)
 
         for letter in ransomNote_freq:
-            if letter not in magazine_freq:
-                return False
-            if ransomNote_freq[letter] > magazine_freq[letter]:
+            if ransomNote_freq[letter] > magazine.count(letter):
                 return False 
         
         return True 
