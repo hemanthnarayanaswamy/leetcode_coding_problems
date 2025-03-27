7ea53a62d5fba45f385390057f218e8619a4fdde
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-27 09:46:28 -0400

    Time: 0 ms (100%), Space: 18.4 MB (26.18%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,22 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = [] # Return the Answer
-
-        for num in range(1, n+1):
-            ans_3 = num % 3  # Store the values to reassess 
-            ans_5 = num % 5
-
-            if ans_3 != 0 and ans_5 != 0:
-                answer.append(str(num))
-
-            elif ans_3 == 0:
-                if ans_5 == 0:
-                     answer.append("FizzBuzz")
-                else:
-                    answer.append("Fizz") 
-            else:
+        answer = []
+        for i in range(1, (n+1)):
+            if i%5==0 and i%3==0:
+                answer.append("FizzBuzz")
+            elif i%3 == 0:
+                answer.append("Fizz")
+            elif i%5==0:
                 answer.append("Buzz")
-                
+            else:
+                answer.append(str(i))
         return answer
-        
-        
