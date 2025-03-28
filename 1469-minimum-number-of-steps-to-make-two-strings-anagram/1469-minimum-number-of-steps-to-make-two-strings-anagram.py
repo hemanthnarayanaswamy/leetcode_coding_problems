8e202ba7eb00d9648e2b09a201c19b21d4d7eb21
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-28 09:34:51 -0400

    Time: 61 ms (87.18%), Space: 18 MB (55.24%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,22 +1,14 @@
-
-
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        s_feq, t_feq = {}, {} 
-        for i in range(len(s)):
-            s_feq[s[i]] = s_feq.get(s[i], 0) + 1
-            t_feq[t[i]] = t_feq.get(t[i], 0) + 1
-
-
-        count = 0           # To track the Changes Required
-
-        for char in t_feq:  # Check each char in t
-            if char not in s_feq:   # If that char is not in s
-                count += t_feq[char]  # All that char needs to be changed
-            else:
-                temp_diff = t_feq[char] - s_feq[char]
-                if temp_diff > 0:  # If that element is present and If its count is excess than s 
-                    count += temp_diff  # it also needs to be changed
-        return count
+        replacementCount = 0
+        
+        s = Counter(s)
+        t = Counter(t)
+        
+        for val, count in s.items():
+            if t[val] < count:
+                replacementCount += count - t[val]
+        
+        return replacementCount
 
         
