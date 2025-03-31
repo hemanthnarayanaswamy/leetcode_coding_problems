f40930725bdd4e320c5dc160dd6256ede913b69e
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-31 10:03:23 -0400

    Time: 0 ms (100%), Space: 17.7 MB (52.75%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -2,10 +2,10 @@ from collections import Counter
 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        stones_freq = Counter(stones)
+        stones = Counter(stones)
         count = 0
 
         for jewel in jewels:
-            count += stones_freq.get(jewel, 0)
+            count += stones.get(jewel, 0)
         
         return count
