aae930d7f7a903cf0d4b3bda2ca296f5f80a57dd
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-03-31 09:48:42 -0400

    Time: 0 ms (100%), Space: 19 MB (78.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
         major_condition = len(nums) // 3
-        result = []
-        nums_freq = Counter(nums)
-
-        for key, value in nums_freq.items():
-            if value > major_condition:
-                result.append(key)
-
-        return result
+        return [key for key, value in Counter(nums).items() if value > major_condition]
