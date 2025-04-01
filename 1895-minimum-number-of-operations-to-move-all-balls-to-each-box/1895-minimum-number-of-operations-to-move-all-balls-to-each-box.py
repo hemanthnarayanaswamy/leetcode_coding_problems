074530f7c5bcb05be5e3ef065f1536b34d913cdd
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-01 09:41:10 -0400

    Time: 7 ms (91.18%), Space: 18.2 MB (33.57%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,17 +1,26 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        right_shift_sum = [0]*len(boxes)
-        left_shift_sum = [0]*len(boxes)
+        n = len(boxes)
+        result = [0]*n
 
-        for i in range(len(boxes)):
-            for j in range(i+1, len(boxes)):
-                if boxes[j] == '1':
-                    right_shift_sum[i] += (j - i)
-        
-        for i in range(len(boxes)-1,-1,-1):
-            for j in range(i-1,-1,-1):
-                if boxes[j] == '1':
-                    left_shift_sum[i] += (i - j)
+        # Left to Right Prefix Operation 
+        count = 0  # To track number of balls to the left
+        operations = 0 # Operations required to move ball to current 
 
-        return [x+y for x,y in zip(right_shift_sum, left_shift_sum)]
+        for i in range(n):
+            result[i] += operations
+            if boxes[i] == '1':
+                count += 1
+            operations += count # Add Count to operations for the next position 
+
+        # Right to Left Prefix Operation 
+        count = 0  # To track number of balls to the right
+        operations = 0 # Operations required to move ball to current 
+
+        for i in range(n-1, -1, -1):
+            result[i] += operations
+            if boxes[i] == '1':
+                count += 1
+            operations += count # Add Count to operations for the next position 
         
+        return result 
