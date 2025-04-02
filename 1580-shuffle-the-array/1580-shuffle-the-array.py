c024fa85e1459ad07aa020e6cb53823d6970102f
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-02 11:13:52 -0400

    Time: 67 ms (9.26%), Space: 18.2 MB (21.82%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,15 +1,8 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        half_len = len(nums)//2
-        result = [0] * 2 * n
-        current = 0
-        
-        for i,j in zip(range(0, n),range(n, 2*n)):
-            result[current] = nums[i]
-            current += 1
-            result[current] = nums[j]
-            current += 1
-        
-        return result
-
+        L = []
+        for i in range(n):
+            L.append(nums[i])
+            L.append(nums[n+i])
+        return L
         
