7011e9dfa38e5604206b725c69b3052b39f9c9fa
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-03 09:34:26 -0400

    Time: 32 ms (90.91%), Space: 17.6 MB (95.78%) - LeetHub

--- 1026-string-without-aaa-or-bbb/1026-string-without-aaa-or-bbb.py
+++ 1026-string-without-aaa-or-bbb/1026-string-without-aaa-or-bbb.py
@@ -5,12 +5,10 @@ class Solution:
 
         while len(result) < total_lenght:
             if a > b:
-                print('a is greater')
                 result += 'aab'
                 a -= 2
                 b -= 1
             elif b > a:
-                print('b is greater')
                 result += 'bba'
                 b -= 2
                 a -= 1
@@ -21,11 +19,9 @@ class Solution:
 
             if a == 0 or b == 0:
                 if a == 0:
-                    print('a is zero')
                     result += 'b'*b
                     break
                 else:
-                    print('b is zero')
                     result += 'a'*a
                     break
 
