a88d1f5c114d1914dc8ca48f3179372242233c76
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-07 12:39:00 -0400

    Time: 3 ms (92.07%), Space: 17.7 MB (96.07%) - LeetHub

--- 0829-subdomain-visit-count/0829-subdomain-visit-count.py
+++ 0829-subdomain-visit-count/0829-subdomain-visit-count.py
@@ -1,21 +1,15 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         subdomain_visit = dict()
-        result = []
       
         for data in cpdomains:
-            data_split = data.split(" ")
-            count = int(data_split[0])
-            domain_parts = data_split[1].split('.')
+            count, domain_parts = data.split(" ")
+            count ,domain_parts = int(count), domain_parts.split('.')
 
             # Update subdomain visit counts for all levels
-            for i in range(len(domain_parts)):
-                subdomain = '.'.join(domain_parts[i:])
-                subdomain_visit[subdomain] = subdomain_visit.get(subdomain, 0) + count
+            for i in range(len(domain_parts)):  ## Iterate through the All different Parts 
+                subdomain = '.'.join(domain_parts[i:])             # Join from Start/current to end of domains
+                subdomain_visit[subdomain] = subdomain_visit.get(subdomain, 0) + count      # If its exisits get that data and add the current count to update the hashMap
 
-        # Combine results
-        for chars, count in subdomain_visit.items():
-            result.append(str(count) + " " + chars)
-
-        return result
-        
+        return [f"{v} {k}" for k, v in subdomain_visit.items()]
+            
