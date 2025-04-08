412c638ff9d56e3f8ec0f648d3b92321be957cff
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-08 10:40:06 -0400

    Time: 6 ms (82.62%), Space: 19.8 MB (41.64%) - LeetHub

--- 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.py
+++ 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.py
@@ -1,4 +1,10 @@
 class Solution:
-    def arrangeWords(self, s: str) -> str:
-        return " ".join(sorted(s.split(),key=len)).lower().capitalize()
+    def arrangeWords(self, text: str) -> str:
+        # return " ".join(sorted(text.split(),key=len)).lower().capitalize()
+        words = text.split(' ')
+        words.sort(key = lambda x: len(x))
+
+        res = " ".join(words)
+        res = res.lower()
+        return res[0].upper() + res[1:]
         
