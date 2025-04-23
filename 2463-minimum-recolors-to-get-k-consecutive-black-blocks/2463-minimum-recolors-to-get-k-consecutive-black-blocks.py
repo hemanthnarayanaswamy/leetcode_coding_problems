649661715bbc60ecc9449b1f8542ce219575b550
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-23 09:15:23 -0400

    Time: 0 ms (100%), Space: 17.8 MB (61.46%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -3,9 +3,6 @@ class Solution:
         n = len(blocks)
         result = n
 
-        if n == k:
-            return blocks.count('W')
-
         for i in range(n-k+1):
             result = min(result, blocks[i:i+k].count('W'))
         
