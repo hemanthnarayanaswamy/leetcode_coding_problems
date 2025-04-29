faf42f7df0301e965ad91f753883128dc84b9e8c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-29 09:00:26 -0400

    Time: 0 ms (100%), Space: 23 MB (13.04%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.py
@@ -1,14 +1,11 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
         scoreMap = {}
-        result = []
 
         for i in range(len(score)):
             scoreMap[score[i][k]] = score[i]
 
-        sorted_scores = dict(sorted(scoreMap.items(), key=lambda item: item[0], reverse=True))
-        
-        for scr in sorted_scores.values():
-            result.append(scr)
-        
-        return result
+        # To get keys sorted by their values (descending):
+        sorted_score = [v for k, v in sorted(scoreMap.items(), key=lambda item: item[0], reverse=True)]
+       
+        return sorted_score
