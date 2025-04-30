e2074d224eb966f41f05d18575f8108d2d1a9be4
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-04-30 09:11:39 -0400

    Time: 103 ms (89%), Space: 59.4 MB (81.88%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.py
@@ -6,18 +6,23 @@ class Solution:
 
         for match in matches:
             w, l = match[0], match[1]
-            lossTracker[w] = lossTracker.get(w, 0) + 0
             lossTracker[l] = lossTracker.get(l, 0) + 1
+            if w not in lossTracker:
+                lossTracker[w] = 0
+
         
-        lossTrackerSort = dict(sorted(lossTracker.items(), key=lambda item: item[0]))
+        #lossTrackerSort = dict(sorted(lossTracker.items(), key=lambda item: item[0]))
         
-        for player, lose in lossTrackerSort.items():
+        for player, lose in lossTracker.items():
             if lose == 0:
                 nolose.append(player)
             
             elif lose == 1:
                 onelose.append(player)
-        
+
+        nolose.sort()
+        onelose.sort()
+
         return [nolose, onelose]
 
 
