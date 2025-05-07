f1ec5653f247d089380df00e8a19f5907edbe3a8
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-07 10:30:59 -0400

    Time: 89 ms (96.72%), Space: 23.2 MB (97.02%) - LeetHub

--- 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,8 +1,9 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort(reverse=True)
+        nums = sorted(nums, reverse=True)
         opperations = 0
+
         for i in range(1, n):
             if nums[i] != nums[i - 1]:
                 opperations += i
