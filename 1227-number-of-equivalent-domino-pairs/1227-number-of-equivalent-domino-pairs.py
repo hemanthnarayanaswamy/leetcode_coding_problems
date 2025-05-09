e422391b2d241adca2811322da963ec3cdadfcb2
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-09 13:07:34 -0400

    Time: 3 ms (97.52%), Space: 24.2 MB (81.36%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -1,15 +1,14 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        num = [0] * 100
+        num = {}
         result = 0
 
         for x, y in dominoes:
             if x > y: x, y = y, x
-
             val = x * 10 + y
-
-            result += num[val]
-            num[val] += 1
+            count = num.get(val, 0)
+            result += count
+            num[val] = count + 1
         
         return result
         
