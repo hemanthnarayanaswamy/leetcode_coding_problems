d8403229e4270bbebcd07295a332f4d0ae025855
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-10 18:50:50 -0400

    Time: 0 ms (100%), Space: 17.7 MB (60.5%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,14 +1,15 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        result = ''
+        result = []
         sfreq = Counter(s)
 
         for char in order:
-            result += char * sfreq.get(char, 0)
+            if char in sfreq:
+                result.append(char * sfreq[char])
         
         for char, n in sfreq.items():
-            if char not in result:
-                result += char * n
+            if char not in order:
+                result.append(char * n)
         
-        return result
+        return ''.join(result)
      
