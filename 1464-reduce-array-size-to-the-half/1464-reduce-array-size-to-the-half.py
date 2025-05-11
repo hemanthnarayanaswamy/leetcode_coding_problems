96386ce2eea490113886eea6ec8907288eecd9b7
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-11 10:59:20 -0400

    Time: 107 ms (22.46%), Space: 48.2 MB (5.12%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        arrFreq = Counter(arr)
+        arrFreq = dict(sorted(arrFreq.items(), key=lambda item: item[1], reverse=True))
+        print(arrFreq)
+        n = len(arr)
+        answer, counter = 0, 0
+
+        for key, val in arrFreq.items():
+            if answer < n // 2:
+                answer += val 
+                counter += 1
+            elif answer > n // 2:
+                return counter
+        
+        return counter 
+        
