1a20e3202cba6135225007581866100f0a6e7f66
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-13 15:38:40 -0400

    Time: 0 ms (100%), Space: 17.9 MB (22.33%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
@@ -1,15 +1,14 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
         target = Counter(target)
-        s = Counter(s)
-        st = {k:v for (k, v) in s.items() if k in target}
+        s = {k:v for (k, v) in Counter(s).items() if k in target}
 
         for char in target:
-            if char not in st:
+            if char not in s:
                 return 0
             else:
-                st[char] = (st[char] // target[char])
+                s[char] = (s[char] // target[char])
         
-        return min(st.values())
+        return min(s.values())
 
         
