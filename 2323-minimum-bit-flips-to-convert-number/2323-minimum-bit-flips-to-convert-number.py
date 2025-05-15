6c53be8fb6bb0bcb46ccbb118c273d90cc2cf2ec
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-15 11:17:57 -0400

    Time: 0 ms (100%), Space: 17.9 MB (13.25%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,13 +1,3 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        if start == goal:
-            return 0
-
-        xor_result = start ^ goal # it return a new number after xor operation
-
-        # the xorResult is int type (Eg: 21, 77, 54 something like this)
-
-        # convert that number to binary that will make it a string and count char 1 and return it or 
-
-        return bin(xor_result).count('1')
-        
+        return (start ^ goal).bit_count()
