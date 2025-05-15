5ee00bef513d508aebecbfa785faef679a67f3b1
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-15 10:17:23 -0400

    Time: 2 ms (82.1%), Space: 19.1 MB (12.41%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
@@ -3,10 +3,6 @@ class Solution:
         n = len(nums)
         nums = sorted(nums)
 
-        if n%2 == 1:
-            median = nums[(n - 1) // 2]
-        else:
-            i = n // 2
-            median = (nums[i] + nums[i-1]) // 2
+        median = nums[n// 2]
 
         return sum([abs(num - median) for num in nums])
