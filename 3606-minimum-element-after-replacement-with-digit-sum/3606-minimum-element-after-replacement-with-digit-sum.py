99b16d05d86b4350f45d116a77ba729389f8c1a1
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-19 09:26:30 -0400

    Time: 11 ms (14.19%), Space: 17.8 MB (64.86%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
@@ -4,9 +4,6 @@ class Solution:
         digitSum = [0] * n
 
         for i in range(n):
-            if nums[i] < 10:
-                digitSum[i] = nums[i]
-            else:
-                digitSum[i] = sum([int(num) for num in str(nums[i])])
+            digitSum[i] = sum([int(num) for num in str(nums[i])])
         
         return min(digitSum)
