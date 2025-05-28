b0e15f0b0ad2dca21943aaff088c70ef5399b1f3
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-28 09:34:13 -0400

    Time: 3 ms (26.7%), Space: 17.7 MB (83.9%) - LeetHub

---
+++ 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        digit_index = 0
+        digit_found = False
+        number_list = set()
+        for i in range(len(word)):
+            if word[i].isdigit() and not digit_found: 
+                digit_index = i 
+                digit_found = True
+
+            if not word[i].isdigit() and digit_found: 
+                number_list.add(int(word[digit_index:i]))
+                digit_found = False
+
+            if i == len(word) -1 and digit_found: 
+                number_list.add(int(word[digit_index:i+1])) 
+
+        return len(number_list) 
