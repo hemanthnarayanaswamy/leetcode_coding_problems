5c25463362a9df7dc31d3807295190524d737006
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-05-29 09:28:50 -0400

    Time: 28 ms (95.74%), Space: 17.6 MB (72.11%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.py
@@ -1,3 +1,8 @@
 class Solution:
-    def numberOfMatches(self, num: int) -> int:
-        return num - 1
+    def numberOfMatches(self, n: int) -> int:
+        res, teams = 0, n
+        while teams > 1:
+            res += teams // 2
+            teams = teams // 2 + teams % 2
+
+        return res
