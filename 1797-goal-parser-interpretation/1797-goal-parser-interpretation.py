fee69869cd05530f997c6c5969d7561028e9b661
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-03 09:01:48 -0400

    Time: 33 ms (83.72%), Space: 17.8 MB (28.42%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,3 +1,16 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace('()', 'o').replace('(al)', 'al')
+        output = ''
+        i = 0 
+        while i < len(command):
+            if command[i] == '(':
+                if command[i+1] == 'a':
+                    output += 'al'
+                    i += 4
+                else:
+                    output += 'o'
+                    i += 2
+            else:
+                output += 'G'
+                i += 1
+        return output
