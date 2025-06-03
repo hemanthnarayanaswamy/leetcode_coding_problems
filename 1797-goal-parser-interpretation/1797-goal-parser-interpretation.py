d6c1caa8f7ee8e4a8d11b787a656700d2733e358
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-03 08:58:17 -0400

    Time: 38 ms (56.76%), Space: 17.7 MB (66.28%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,19 +1,19 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        output = []
+        output = ''
 
         i = 0 
 
         while i < len(command):
             if command[i] == '(':
-                if command[i+1] == ')':
-                    output.append('o')
-                    i += 2
-                else:
-                    output.append('al')
+                if command[i+1] == 'a':
+                    output += 'al'
                     i += 4
+                else:
+                    output += 'o'
+                    i += 2
             else:
-                output.append('G')
+                output += 'G'
                 i += 1
         
-        return ''.join(output)
+        return output
