473870452e56d2c4a09d4ec64d9ce67d5285286f
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-05 08:39:51 -0400

    Time: 0 ms (100%), Space: 17.9 MB (28.16%) - LeetHub

--- 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.py
+++ 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.py
@@ -3,8 +3,8 @@ class Solution:
         result = 0
         n = len(nums)
 
-        for i in range(n):
-            if n % (i+1) == 0:
-                result += nums[i]**2
+        for i in range(1,n+1):
+            if n % (i) == 0:
+                result += nums[i-1]**2
         
         return result
