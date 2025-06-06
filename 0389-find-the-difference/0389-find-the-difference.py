bc9e9536685177d289eef9f78e7af4ee3875d5b5
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-06 09:16:36 -0400

    Time: 0 ms (100%), Space: 17.9 MB (23.48%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -3,6 +3,7 @@ class Solution:
         sFreq = Counter(s)
         tFreq = Counter(t)
 
+        # Compute the Freq of the both strings and compare the values and return the char that has mis matched values.
         for char in tFreq:
             if sFreq.get(char, 0) != tFreq.get(char):
                 return char
