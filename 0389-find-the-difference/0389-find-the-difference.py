a320f15b8ea251e80c0babc947ca40f99a9b4163
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-06 09:13:11 -0400

    Time: 2 ms (60.99%), Space: 18 MB (23.48%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,11 +2,7 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         sFreq = Counter(s)
         tFreq = Counter(t)
-        print(sFreq, tFreq)
 
         for char in tFreq:
-            if char not in sFreq:
-                return char
-            
-            if tFreq[char] != sFreq[char]:
+            if sFreq.get(char, 0) != tFreq.get(char):
                 return char
