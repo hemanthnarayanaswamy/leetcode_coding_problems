394ed59fc506f8a7b42b443a0a1e14ad02a60ceb
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-07 14:02:15 -0400

    Time: 9 ms (20.95%), Space: 17.7 MB (51.84%) - LeetHub

--- 2825-minimize-string-length/2825-minimize-string-length.py
+++ 2825-minimize-string-length/2825-minimize-string-length.py
@@ -1,3 +1,11 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        return len(set(s))
+        Uniq = set()
+        count = 0
+
+        for char in s:
+            if char not in Uniq:
+                count += 1
+                Uniq.add(char)
+
+        return count
