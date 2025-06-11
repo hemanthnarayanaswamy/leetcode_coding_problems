72bd7b18c645230fd5826f0c53b47ec249d8b5de
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-11 10:06:53 -0400

    Time: 3 ms (78.76%), Space: 18.5 MB (80.6%) - LeetHub

---
+++ 2447-merge-similar-items/2447-merge-similar-items.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        items_combined = items1 + items2 
+        items_hash = {}
+        print(items_combined)
+
+        for item in items_combined:
+            items_hash[item[0]] = items_hash.get(item[0], 0) + item[1]
+        
+        items_hash_sort = sorted(items_hash.items(), key=lambda item: item[0])
+
+        return items_hash_sort
+
+    
+
+
