a3daf5ac217685fe8cc00aeb5832976f84142597
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-12 10:34:21 -0400

    Time: 41 ms (39.78%), Space: 17.9 MB (18.44%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,10 +1,11 @@
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         alphabet = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8}
+
         firstSum = alphabet[coordinate1[0]] + int(coordinate1[1])
         secondSum = alphabet[coordinate2[0]] + int(coordinate2[1])
 
-        return True if (firstSum % 2 == secondSum % 2) else False
+        return firstSum % 2 == secondSum % 2
 
 
         
