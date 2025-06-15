0d550c62c8b3aedc500aaa5b0861546100a29676
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-15 10:06:45 -0400

    Time: 22 ms (90.11%), Space: 17.9 MB (27.69%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.py
+++ 1500-count-largest-group/1500-count-largest-group.py
@@ -2,13 +2,19 @@ class Solution:
     def countLargestGroup(self, n: int) -> int:
         numGroup = {}
 
+        def digit_sum(x: int) -> int:
+            total = 0
+            while x:
+                total += x % 10
+                x //= 10
+            return total
+
         for i in range(1, n+1):
-            temp = sum([int(j) for j in str(i)])
+            temp = digit_sum(i)
             numGroup[temp] = numGroup.get(temp, 0) + 1
         
-        groupSize = [v for v in numGroup.values()]
-        
-        return groupSize.count(max(groupSize)) 
+        maxSize = max(numGroup.values())
+        return list(numGroup.values()).count(maxSize)
 
 
 
