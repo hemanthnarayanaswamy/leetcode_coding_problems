742722fd8f81b12f33a3f4bcc603e7c426dc4db1
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-17 09:12:34 -0400

    Time: 4 ms (4.03%), Space: 17.7 MB (89.57%) - LeetHub

--- 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
@@ -1,9 +1,4 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        result = 0
-
-        for t1,t2 in zip(startTime, endTime):
-            if queryTime in range(t1, t2+1):
-                result += 1
-        
-        return result
+        # sum up 1 for each (s,e) where s <= queryTime <= e
+        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
