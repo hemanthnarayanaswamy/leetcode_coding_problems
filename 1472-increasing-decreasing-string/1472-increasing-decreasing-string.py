a54c9ddc11e7d98e6bf4827ba27f759af1cf9395
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-17 10:07:07 -0400

    Time: 15 ms (82.66%), Space: 17.7 MB (95.72%) - LeetHub

--- 1472-increasing-decreasing-string/1472-increasing-decreasing-string.py
+++ 1472-increasing-decreasing-string/1472-increasing-decreasing-string.py
@@ -1,12 +1,12 @@
 class Solution:
     def sortString(self, s: str) -> str:
+        alphabets = 'abcdefghijklmnopqrstuvwxyz'
         sFreq = Counter(s)
 
         result = []
 
         while len(result) != len(s):
-            for i in range(ord('a'), ord('z')+1):
-                c1 = chr(i)
+            for c1 in alphabets:
                 if sFreq.get(c1,0) > 0:
                     result.append(c1)
                     sFreq[c1] -= 1
@@ -14,12 +14,9 @@ class Solution:
             if len(result) == len(s):
                 break
             
-            for j in range(ord('z'), ord('a')-1, -1):
-                c2 = chr(j)
+            for c2 in alphabets[::-1]:
                 if sFreq.get(c2,0) > 0:
                     result.append(c2)
-                    sFreq[c2] -= 1
-            
-            print(result)
+                    sFreq[c2] -= 1       
         
         return ''.join(result)
