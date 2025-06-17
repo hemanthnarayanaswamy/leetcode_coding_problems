6986495449e941583008262f3a67240dd6a951bb
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-17 09:37:22 -0400

    Time: 37 ms (69.48%), Space: 17.8 MB (35.76%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -5,10 +5,8 @@ class Solution:
         while num1 and num2:
             if num1 > num2:
                 num1 -= num2
-            elif num1 < num2:
-                num2 -= num1
             else:
-                num1 = num2 = 0
+                num2 -= num1
             
             result += 1
         
