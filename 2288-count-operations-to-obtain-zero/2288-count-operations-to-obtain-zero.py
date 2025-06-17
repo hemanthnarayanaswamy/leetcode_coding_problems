f68dd04213ab6d63fdf22cb2ce8d645403b3d289
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-17 09:42:08 -0400

    Time: 0 ms (100%), Space: 17.7 MB (54.02%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -1,13 +1,13 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        result = 0
-
+        count = 0
+        # keep subtracting the smaller from the larger in bulk
         while num1 and num2:
-            if num1 > num2:
-                num1 -= num2
+            if num1 >= num2:
+                # subtract num2 as many times as possible in one go
+                q, num1 = divmod(num1, num2)  # does the same in one C-level operation (q = num1 // num2 and num1 %= num2).
+                count += q
             else:
-                num2 -= num1
-            
-            result += 1
-        
-        return result 
+                q, num2 = divmod(num2, num1)
+                count += q
+        return count
