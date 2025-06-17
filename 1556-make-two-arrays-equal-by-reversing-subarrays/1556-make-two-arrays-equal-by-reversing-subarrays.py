f4ab00d2a7e64186eb259f33dc6b9f54a4fb9f3c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-17 09:00:24 -0400

    Time: 7 ms (13.84%), Space: 18.2 MB (7.98%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,6 +1,8 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        targetFreq = Counter(target)
-        arrFreq = Counter(arr)
-
-        return targetFreq == arrFreq
+        if len(target) == len(arr):
+            targetFreq = Counter(target)
+            arrFreq = Counter(arr)
+            return targetFreq == arrFreq
+        else:
+            return False
