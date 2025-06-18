3818c325c17ef74f3eb8754887c4dd679ad8f71c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-18 10:26:47 -0400

    Time: 5 ms (17.58%), Space: 18.4 MB (5.51%) - LeetHub

--- 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
@@ -1,16 +1,7 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        n_lists = len(nums)
-        counts = [0] * 1001
-        # Count each distinct element per sub-array
-        for arr in nums:
-            # arr elements are guaranteed distinct, so no need for set(arr)
-            for x in arr:
-                counts[x] += 1
-
-        # Now scan 1…100; if counts[i] == n_lists, it appeared in _every_ list
-        res = []
-        for val in range(1, 1001):
-            if counts[val] == n_lists:
-                res.append(val)
-        return res
+        
+        intersection_nums = set(nums[0])
+        for i in range(1, len(nums)):
+            intersection_nums &= set(nums[i])
+        return sorted(intersection_nums)
