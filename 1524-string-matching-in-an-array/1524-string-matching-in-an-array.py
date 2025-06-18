42845552787fdd59b1aa78914350539a1c3dea8c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-18 09:17:03 -0400

    Time: 0 ms (100%), Space: 17.8 MB (56.29%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,10 +1,15 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        result = set()
+        res = set()
         
-        for i in range(len(words)):
-            for j in range(len(words)):
-                if i != j and words[i] in words[j]:
-                    result.add(words[i])
-        
-        return list(result)
+        # sort ascending by length
+        words_sorted = sorted(words, key=len)
+
+        for i, w in enumerate(words_sorted):
+            # only check against words longer than w
+            for longer in words_sorted[i+1:]:
+                if w in longer:
+                    res.add(w)
+                    break        # no need to check other longer words
+
+        return list(res)
