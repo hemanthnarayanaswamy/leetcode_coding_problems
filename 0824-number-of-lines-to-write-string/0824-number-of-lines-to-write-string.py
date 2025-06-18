c3a8ae54323bcc4fe9a4a08a9f807d7c78610944
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-18 09:49:58 -0400

    Time: 34 ms (82.8%), Space: 17.6 MB (94.37%) - LeetHub

--- 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.py
+++ 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.py
@@ -1,15 +1,18 @@
 class Solution:
-    def numberOfLines(self, widths, s):
-        max_width = 100
-        lines = 1
-        current_width = 0
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
 
-        for ch in s:
-            w = widths[ord(ch) - ord('a')]
-            if current_width + w > max_width:
+        lines, total = 1, 0
+        L = []
+        for c in s:
+            total += widths[ord(c) - ord('a')]
+            
+            if total > 100:
                 lines += 1
-                current_width = w
-            else:
-                current_width += w
+                total = widths[ord(c) - ord('a')]
 
-        return [lines, current_width]
+        L.append(lines)
+        L.append(total)
+
+        return L
+       
+        
