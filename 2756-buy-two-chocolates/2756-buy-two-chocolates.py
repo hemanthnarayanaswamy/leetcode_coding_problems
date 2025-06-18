c9cfb06625d246462bad2253ef2c3a3409eb67e7
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-18 08:56:33 -0400

    Time: 3 ms (37.42%), Space: 17.7 MB (88.53%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.py
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.py
@@ -1,14 +1,7 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        firstCho, secondCho = float(inf), float(inf)
-
-        for p in prices:
-            if p < firstCho:
-                secondCho = firstCho 
-                firstCho = p 
-            elif p < secondCho:
-                secondCho = p
+        prices.sort()
         
-        choPrice = firstCho + secondCho
+        choPrice = prices[0] + prices[1]
         
         return money if choPrice > money else money - choPrice
