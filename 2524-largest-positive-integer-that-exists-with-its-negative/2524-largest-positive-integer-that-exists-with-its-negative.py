9d4da427d64d30896b04ef55a1567e827ea18379
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-19 09:29:03 -0400

    Time: 3 ms (93.37%), Space: 18 MB (28.2%) - LeetHub

--- 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.py
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.py
@@ -1,5 +1,11 @@
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        s = set(nums)
-        # generate abs(x) for every x whose negation is also in s
-        return max((abs(x) for x in s if -x in s), default=-1)
+        larNum = -1 
+
+        numsUniq = set(nums)
+
+        for num in numsUniq:
+            if -num in numsUniq and abs(num) > larNum:
+                larNum = abs(num)
+        
+        return larNum
