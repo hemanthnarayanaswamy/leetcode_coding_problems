024c77eefa56b3b6ce8c2fd9345dd1566c8afdf6
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-23 02:47:56 -0400

    Time: 0 ms (100%), Space: 17.9 MB (67.26%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,17 +1,4 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        freq = Counter(arr1)
-        result = []
-        
-        # 1) Place everything in the order of arr2
-        for x in arr2:
-            c = freq.pop(x, 0)
-            result.extend([x] * c)
-        
-        # 2) Sort whatever remains in freq and append
-        leftovers = dict(sorted(freq.items(), key=lambda x:x[0]))
-        
-        for n, f in leftovers.items():
-            result.extend([n] * f)
-        
-        return result
+        order = {x:i for i,x in enumerate(arr2)}
+        return sorted(arr1, key=lambda x: (order.get(x, len(arr2)), x))
