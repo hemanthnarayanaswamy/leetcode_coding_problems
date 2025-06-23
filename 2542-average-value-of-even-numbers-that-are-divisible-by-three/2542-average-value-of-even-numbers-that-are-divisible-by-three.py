f34dca1e11deb5cb962c1f516620be01ff584f0f
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-23 13:47:23 -0400

    Time: 0 ms (100%), Space: 18 MB (42%) - LeetHub

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/2542-average-value-of-even-numbers-that-are-divisible-by-three.py
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/2542-average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -3,13 +3,8 @@ class Solution:
         avgsum  = []
 
         for num in nums:
-            if num % 2: 
-                continue 
-            
-            if num % 3:
-                continue 
+            if num % 2 == 0 and num % 3 == 0: 
+                avgsum.append(num)
 
-            avgsum.append(num)
-        
         return sum(avgsum) // len(avgsum) if avgsum else 0
 
