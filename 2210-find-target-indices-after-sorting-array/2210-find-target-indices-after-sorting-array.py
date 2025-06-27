90879d5f020d354ebe6394ebe02a4b74ddec50b9
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-27 12:35:54 -0400

    Time: 4 ms (7.79%), Space: 17.9 MB (15.27%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -1,32 +1,11 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        # 1) Sort in-place
+        my_list = []
         nums.sort()
-        n = len(nums)
+
+        for index, value in enumerate(nums):
+            if value == target:
+                my_list.append(index)
+
+        return my_list
         
-        # 2) Find leftmost index where nums[idx] == target (lower_bound)
-        lo, hi = 0, n
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] < target:
-                lo = mid + 1
-            else:
-                hi = mid
-        left = lo
-        
-        # If out of bounds or not actually target, no occurrences
-        if left == n or nums[left] != target:
-            return []
-        
-        # 3) Find first index > target (upper_bound)
-        lo, hi = 0, n
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] <= target:
-                lo = mid + 1
-            else:
-                hi = mid
-        right = lo
-        
-        # 4) All indices in [left, right) are equal to target
-        return list(range(left, right))
