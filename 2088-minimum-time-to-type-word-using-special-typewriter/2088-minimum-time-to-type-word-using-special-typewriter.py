1229e736500885623eb90fe309998a55722c451c
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-29 10:15:21 -0400

    Time: 3 ms (21.86%), Space: 17.8 MB (34.28%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
@@ -1,11 +1,15 @@
 class Solution:
     def minTimeToType(self, word: str) -> int:
-        time = 0
-        last = 'a'
+        totalTime = 0
+        currentPosition = 'a'
 
-        for curr in word:
-            diff = abs(ord(curr) - ord(last))
-            time += min(diff, 26 - diff) + 1
-            last = curr
+        alphabet = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
 
-        return time
+        for c in word:
+            forwardTime = abs(alphabet[currentPosition] - alphabet[c])
+            backwardTime = abs(26 - forwardTime)
+
+            totalTime += min(forwardTime, backwardTime) + 1
+            currentPosition = c
+        
+        return totalTime
