c1e38131b0bb8d373c759ab894aadf9fec9855c2
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-30 09:42:46 -0400

    Time: 351 ms (92.74%), Space: 166 MB (42.08%) - LeetHub

--- 3707-find-the-most-common-response/3707-find-the-most-common-response.py
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.py
@@ -7,18 +7,11 @@ class Solution:
             uniqRes = set(responses[i])
             for r in uniqRes:
                 responsesFreq[r] = responsesFreq.get(r, 0) + 1
-            
-        print(responsesFreq)
 
         maxFreq = max(responsesFreq.values())
 
         for r,v in responsesFreq.items():
             if v == maxFreq:
                 commonRes.append(r)
-        
-        print(commonRes)
 
-        if len(commonRes) == 1:
-            return commonRes[0]
-        else:
-            return sorted(commonRes)[0]
+        return sorted(commonRes)[0]
