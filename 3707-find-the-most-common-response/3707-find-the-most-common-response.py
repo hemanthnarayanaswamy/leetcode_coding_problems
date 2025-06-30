1ad2175723ef384cc8aa639ca6dd33fac348ceef
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-30 09:53:42 -0400

    Time: 311 ms (98.45%), Space: 165.9 MB (49.74%) - LeetHub

--- 3707-find-the-most-common-response/3707-find-the-most-common-response.py
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.py
@@ -1,17 +1,11 @@
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
-        responsesFreq = {}
+        responsesFreq = Counter()
         commonRes = []
 
-        for i in range(len(responses)):
-            uniqRes = set(responses[i])
-            for r in uniqRes:
-                responsesFreq[r] = responsesFreq.get(r, 0) + 1
+        for response in responses:
+            responsesFreq.update(set(response))
 
         maxFreq = max(responsesFreq.values())
-
-        for r,v in responsesFreq.items():
-            if v == maxFreq:
-                commonRes.append(r)
-
-        return sorted(commonRes)[0]
+            
+        return min((r for r,v in responsesFreq.items() if v == maxFreq))
