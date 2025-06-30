3bdaba5e7663823fed37b375c4e7a49ef6198c88
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-06-30 09:57:51 -0400

    Time: 301 ms (98.88%), Space: 165.1 MB (99.34%) - LeetHub

--- 3707-find-the-most-common-response/3707-find-the-most-common-response.py
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.py
@@ -1,11 +1,22 @@
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
-        responsesFreq = Counter()
-        commonRes = []
-
-        for response in responses:
-            responsesFreq.update(set(response))
-
-        maxFreq = max(responsesFreq.values())
-            
-        return min((r for r,v in responsesFreq.items() if v == maxFreq))
+        freq_map = {}
+        highest_freq = -1
+        highest_freq_word = ""
+        for l in responses:
+            used_words = set()
+            for word in l:
+                if word in used_words:
+                    continue
+                if word in freq_map:
+                    freq_map[word] += 1
+                else:
+                    freq_map[word] = 1
+                used_words.add(word)
+        for key, val in freq_map.items():
+            if val > highest_freq:
+                highest_freq = val
+                highest_freq_word = key
+            elif val == highest_freq:
+                highest_freq_word = min(key, highest_freq_word)
+        return highest_freq_word
