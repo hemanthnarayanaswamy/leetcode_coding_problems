0a3284bcf3b348237c0405dc912f012e4e1141ef
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-02 10:15:52 -0400

    Time: 2 ms (90.06%), Space: 18.1 MB (16.42%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,16 +1,3 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        Horizontal, Vertical = 0, 0
-        
-        for m in moves:
-            if m == 'U':
-                Vertical += 1
-            elif m == 'D':
-                Vertical -= 1
-            elif m == 'R':
-                Horizontal += 1
-            else:
-                Horizontal -= 1
-        
-        return False if Horizontal or Vertical else True
-        
+        return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
