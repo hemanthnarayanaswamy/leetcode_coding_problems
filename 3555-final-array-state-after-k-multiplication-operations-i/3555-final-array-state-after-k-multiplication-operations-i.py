ec580b190584e1470bc0165a3a0d8b86e16c3e65
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-02 09:45:50 -0400

    Time: 0 ms (100%), Space: 17.7 MB (94.99%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -1,11 +1,12 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        while k:
-            minNum = min(nums)
-            for i in range(len(nums)):
-                if nums[i] == minNum:
-                    nums[i] *= multiplier
-                    break
-            k -= 1
-        
+        n = len(nums)
+        for _ in range(k):
+            # find the index of the minimum element
+            min_idx = 0
+            for i in range(1, n):
+                if nums[i] < nums[min_idx]:
+                    min_idx = i
+            # multiply that minimum
+            nums[min_idx] *= multiplier
         return nums
