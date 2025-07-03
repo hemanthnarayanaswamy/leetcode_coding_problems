006289e4b19799741b8c3c25df3199676bfdf926
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-03 09:03:11 -0400

    Time: 0 ms (100%), Space: 18 MB (18.54%) - LeetHub

--- 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.py
+++ 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.py
@@ -1,13 +1,13 @@
 class Solution:
     def evenOddBit(self, n: int) -> List[int]:
         binN = bin(n)[2::][::-1]
-        e, o = 0, 0
+        res = [0, 0]
 
         for i in range(len(binN)):
             if binN[i] == '1':
                 if i % 2:
-                    o += 1
+                   res[1] += 1
                 else:
-                    e += 1
+                    res[0] += 1
         
-        return [e, o]
+        return res
