b3402064a4609f4593bec7f310590332e9c2ebf8
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-03 08:40:12 -0400

    Time: 0 ms (100%), Space: 17.6 MB (94.37%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.py
@@ -1,11 +1,3 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        seenB = False 
-
-        for c in s:
-            if c == 'b':
-                seenB = True
-            elif seenB: # check c == a after already seen b
-                return False
-        
-        return True
+        return 'ba' not in s
