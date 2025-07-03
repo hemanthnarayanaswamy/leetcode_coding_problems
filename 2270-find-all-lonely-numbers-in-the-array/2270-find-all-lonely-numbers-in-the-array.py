82697df63cb797769992830bd598e05f42829114
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-03 09:19:12 -0400

    Time: 121 ms (82.44%), Space: 39.8 MB (85.55%) - LeetHub

--- 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.py
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.py
@@ -1,12 +1,10 @@
 class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
-        res = []
-
-        numsFreq = Counter(nums)
-
-        for num in numsFreq:
-            if numsFreq[num] == 1:
-                if num + 1 not in numsFreq and num - 1 not in numsFreq:
-                    res.append(num)
-        
-        return res
+        freq = Counter(nums)
+        return [
+            x
+            for x, c in freq.items()
+            if c == 1
+            and (x - 1) not in freq
+            and (x + 1) not in freq
+        ]
