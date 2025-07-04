09507af5ff7595cc3160aaa0ee6e919841f4f6b7
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-04 10:23:04 -0400

    Time: 1 ms (65.84%), Space: 17.8 MB (46.83%) - LeetHub

--- 3723-sum-of-good-numbers/3723-sum-of-good-numbers.py
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.py
@@ -1,21 +1,14 @@
 class Solution:
     def sumOfGoodNumbers(self, nums: List[int], k: int) -> int:
-        res = 0
-
-        for i in range(len(nums)):
-            p = i - k
-            n = i + k
-
-            if p > -1 and n < len(nums):
-                if nums[i] > nums[p] and nums[i] > nums[n]:
-                    res += nums[i]
-            elif p < 0 and n < len(nums):
-                if nums[i] > nums[n]:
-                    res += nums[i]
-            elif p > -1 and n >= len(nums):
-                if nums[i] > nums[p]:
-                    res += nums[i]
-            else:
-                res += nums[i]
-
-        return res   
+        n = len(nums)
+        total = 0
+        NEG_INF = float('-inf')
+        
+        for i, v in enumerate(nums):
+            left  = nums[i-k] if i - k >= 0   else NEG_INF
+            right = nums[i+k] if i + k < n    else NEG_INF
+            
+            if v > left and v > right:
+                total += v
+        
+        return total
