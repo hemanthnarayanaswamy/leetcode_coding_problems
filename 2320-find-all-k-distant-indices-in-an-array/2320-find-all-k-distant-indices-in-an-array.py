703dac979938e0928632f391447be430437089bb
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-07 09:19:13 -0400

    Time: 2 ms (84%), Space: 18 MB (63.45%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
@@ -2,11 +2,12 @@ class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
         res = []
         n = len(nums)
-        # traverse number pairs
-        for i in range(n):
-            for j in range(n):
-                if nums[j] == key and abs(i - j) <= k:
+        r = 0
+        for j in range(n):
+            if nums[j] == key:
+                l = max(r, j - k)
+                r = min(n - 1, j + k) + 1
+                for i in range(l, r):
                     res.append(i)
-                    break  # early termination to prevent duplicate addition
         return res
         
