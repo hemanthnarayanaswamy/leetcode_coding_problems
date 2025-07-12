d4f6ceb4ec66e74a9c3d57eb463f71517ca721a7
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-12 13:14:41 -0400

    Time: 169 ms (42.06%), Space: 18.5 MB (20.89%) - LeetHub

--- 1843-number-of-rectangles-that-can-form-the-largest-square/1843-number-of-rectangles-that-can-form-the-largest-square.py
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/1843-number-of-rectangles-that-can-form-the-largest-square.py
@@ -5,11 +5,11 @@ class Solution:
 
         for l, w in rectangles:
             side_len = min(l, w)
-            
-            if side_len == max_len:
-                count += 1
-            elif side_len > max_len:
+
+            if side_len > max_len:
                 max_len = side_len
                 count = 1
+            elif side_len == max_len:
+                count += 1
 
         return count
