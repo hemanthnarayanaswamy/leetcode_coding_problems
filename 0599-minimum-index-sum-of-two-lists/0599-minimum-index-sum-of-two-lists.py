83c90bd9bfeb41a417f20d8a6c39f7be786e8956
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-13 12:54:08 -0400

    Time: 8 ms (61.38%), Space: 18.1 MB (91.47%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,23 +1,19 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        map_lst1 = {}
-        comm_str = []
-        min_idxSum = float('inf')
+        if len(list1) == 1 and len(list2) == 1:
+            return list1 # = list2
+        
+        hash_map = {word: i for i, word in enumerate(list1)}
+        
+        min_sum = 1000*2
+        result = []
+        for j, word in enumerate(list2):
+            if word in hash_map:
+                if hash_map[word] + j < min_sum:
+                    min_sum = hash_map[word] + j
+                    result = [word]
+                elif hash_map[word] + j == min_sum:
+                    result.append(word)
 
-        for i,s in enumerate(list1):
-            map_lst1[s] = i
-        
-        for i,s in enumerate(list2):
-            if s in map_lst1:
-                tmp_sum = i + map_lst1[s]
-                
-                if tmp_sum < min_idxSum:
-                    comm_str.clear()
-                    comm_str.append(s)
-                    min_idxSum = tmp_sum
-                elif tmp_sum == min_idxSum:
-                    comm_str.append(s)
-        
-        return comm_str
+        return result
             
-        
