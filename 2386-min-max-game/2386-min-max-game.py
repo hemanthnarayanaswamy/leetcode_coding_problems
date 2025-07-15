e127f65fb27ed29ceb3067d01779b804a0c3248f
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-15 10:14:51 -0400

    Time: 3 ms (64.31%), Space: 18.2 MB (16.62%) - LeetHub

--- 2386-min-max-game/2386-min-max-game.py
+++ 2386-min-max-game/2386-min-max-game.py
@@ -1,11 +1,14 @@
 class Solution:
     def minMaxGame(self, nums: List[int]) -> int:
-        while len(nums) > 1:
-            # pair up (nums[0],nums[1]), (nums[2],nums[3]), …
-            pairs = zip(nums[::2], nums[1::2])
-            # for each pair, take min if i is even, max if i is odd
-            nums = [
-                (a if a < b else b) if i % 2 == 0 else (a if a > b else b)
-                for i, (a, b) in enumerate(pairs)
-            ]
+        while len(nums) != 1:
+            newNums = []
+            count = 0
+            for i in range(0, len(nums), 2):
+                if count % 2 == 0:
+                    newNums.append(min(nums[i], nums[i + 1]))
+                else:
+                    newNums.append(max(nums[i], nums[i + 1]))
+                count += 1
+            nums = newNums
+            
         return nums[0]
