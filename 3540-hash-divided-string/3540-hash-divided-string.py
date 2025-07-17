7e551cc73cf606b52d30fdabf4dcc707028b781b
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-17 11:19:40 -0400

    Time: 15 ms (73.4%), Space: 17.9 MB (47.47%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.py
+++ 3540-hash-divided-string/3540-hash-divided-string.py
@@ -1,14 +1,7 @@
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
-        res = []
-        alphatoidx = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12,       'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
-        idxtoalpha = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
-
-        for i in range(0, len(s), k):
-            tmp = 0
-            for j in range(i, i+k):
-                tmp += alphatoidx[s[j]]
-            
-            res.append(idxtoalpha[tmp % 26])
-        
-        return ''.join(res)
+        ords = [ord(i)-97 for i in s]
+        res = ""
+        for i in range(0, len(ords), k):
+            res+= chr((sum(ords[i:i+k])%26)+97)
+        return (res)
