a0fcc7b6f776f31be1d425febed8f62e3afcc8b5
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-18 08:02:19 -0400

    Time: 0 ms (100%), Space: 17.9 MB (15.96%) - LeetHub

--- 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.py
+++ 3220-count-tested-devices-after-test-operations/3220-count-tested-devices-after-test-operations.py
@@ -1,14 +1,10 @@
 class Solution:
-    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
-        testedDevice = 0
-        decreasedBattery = 0
-
-        for b in batteryPercentages:
-            if decreasedBattery:
-                b = b - decreasedBattery 
-            
-            if b > 0:
-                testedDevice += 1
-                decreasedBattery += 1
-        
-        return testedDevice
+    def countTestedDevices(self, battery_percentages: List[int]) -> int:
+        tested_count = 0
+        for battery in battery_percentages:
+            # after testing tested_count devices, each remaining device has
+            # “lost” tested_count% battery, so we can test this one iff
+            # battery – tested_count > 0 ⇔ battery > tested_count
+            if battery > tested_count:
+                tested_count += 1
+        return tested_count
