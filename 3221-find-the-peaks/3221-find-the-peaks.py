bcca6e4831e0dcb8b6b81500367e0247b2412d08
Hemanth Narayanaswamy <hemanthnarayanswamy@gmail.com>
2025-07-19 11:29:25 -0400

    Time: 1 ms (31.52%), Space: 18 MB (15.05%) - LeetHub

--- 3221-find-the-peaks/3221-find-the-peaks.py
+++ 3221-find-the-peaks/3221-find-the-peaks.py
@@ -2,10 +2,17 @@ class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
         peaks = []
 
-        for i in range(1, len(mountain)-1):
+        i = 1
+
+        while i < len(mountain) - 1:
             p, c, n = mountain[i-1], mountain[i], mountain[i+1]
 
             if c > p and c > n:
                 peaks.append(i)
+                i += 2
+            else:
+                i += 1
         
         return peaks
+
+
