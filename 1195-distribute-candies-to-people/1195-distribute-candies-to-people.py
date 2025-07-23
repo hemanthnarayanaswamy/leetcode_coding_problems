7e7a64afe5971f1d8c984351a579226f80003779
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-23 10:26:18 -0400

    Time: 3 ms (84.12%), Space: 17.8 MB (53.32%) - LeetHub

--- 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
@@ -3,11 +3,15 @@ class Solution:
         res = [0] * num_people
         counter = 1
 
-        while candies > 0:
+        while candies:
             for i in range(num_people):
-                if candies > 0:
-                    res[i] += min(counter, candies)
+                if counter <= candies:
+                    res[i] += counter
                     candies -= counter
-                    counter += 1 
+                    counter += 1
+                else:
+                    res[i] += candies
+                    return res
 
         return res
+                    
