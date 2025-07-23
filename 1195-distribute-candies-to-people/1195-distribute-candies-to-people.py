cd05fb7a329661bf29713e7c16f6d00287f30fde
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-23 10:13:04 -0400

    Time: 5 ms (48.82%), Space: 17.9 MB (53.32%) - LeetHub

---
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        res = [0] * num_people
+        counter = 1
+
+        while candies:
+            for i in range(num_people):
+                if counter <= candies:
+                    res[i] += counter
+                    candies -= counter
+                    counter += 1
+                else:
+                    res[i] += candies
+                    return res
+
+            print(res, counter, candies)
+                    
