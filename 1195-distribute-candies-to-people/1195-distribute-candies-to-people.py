6fb041ad99e59353561047861621cc68a39332a1
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-23 10:25:52 -0400

    Time: 9 ms (7.58%), Space: 17.8 MB (80.57%) - LeetHub

--- 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
@@ -1,18 +1,13 @@
 class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
         res = [0] * num_people
-        idx = 0
-        give = 1
-        
-        # keep giving until we run out
+        counter = 1
+
         while candies > 0:
-            # give either `give` or whatever's left
-            take = min(give, candies)
-            res[idx] += take
-            
-            # update for next round
-            candies -= take
-            give     += 1
-            idx      = (idx + 1) % num_people
-        
+            for i in range(num_people):
+                if candies > 0:
+                    res[i] += min(counter, candies)
+                    candies -= counter
+                    counter += 1 
+
         return res
