e59f59f5b3baaf57abed63bc89cc16208cdd4962
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-24 09:48:26 -0400

    Time: 0 ms (100%), Space: 17.8 MB (34.88%) - LeetHub

--- 0851-goat-latin/0851-goat-latin.py
+++ 0851-goat-latin/0851-goat-latin.py
@@ -1,13 +1,18 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
-        goatLst = sentence.split()
-
-        for i in range(len(goatLst)):
-            if goatLst[i][0] in 'aeiouAEIOU':
-                goatLst[i] = goatLst[i] + 'ma' + (i+1) * 'a'
-            else:
-                goatLst[i] = goatLst[i][1::] + goatLst[i][0] + 'ma' + (i+1) * 'a'
-
+        if not sentence:
+            return ""
         
-        return ' '.join(goatLst)
+        res = []
+        
+        vowels = {'a','e','i','o','u','A','E','I','O','U'}
+        
+        for i, word in enumerate(sentence.split()):
+            if word[0] in vowels:
+                res.append(word + 'ma' + (i+1) * 'a')
+            else:
+                res.append(word[1::] + word[0] + 'ma' + (i+1) * 'a')
+        
+        return " ".join(res)
+
 
