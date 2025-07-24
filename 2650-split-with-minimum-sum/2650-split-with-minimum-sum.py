fec03f8febdb98a12c422d9dc9a846d76bcb77b8
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-24 09:25:04 -0400

    Time: 0 ms (100%), Space: 17.7 MB (85.12%) - LeetHub

--- 2650-split-with-minimum-sum/2650-split-with-minimum-sum.py
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.py
@@ -1,17 +1,5 @@
 class Solution:
     def splitNum(self, num: int) -> int:
-        num1 = []
-        num2 = []
+        s = ''.join(sorted(str(num)))
 
-        num = sorted(str(num))
-
-        for i in range(len(num)):
-            if i % 2:
-                num2.append(num[i])
-            else:
-                num1.append(num[i])
-        
-        num1 = int(''.join(num1))
-        num2 = int(''.join(num2))
-
-        return num1 + num2
+        return int(s[::2]) + int(s[1::2])
