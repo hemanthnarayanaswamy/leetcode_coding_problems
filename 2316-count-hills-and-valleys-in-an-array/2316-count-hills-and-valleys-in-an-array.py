a66d2d879a03f09032451b338334c28d24b3db89
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-26 07:57:36 -0400

    Time: 0 ms (100%), Space: 17.7 MB (90.84%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -2,16 +2,12 @@ class Solution:
     def countHillValley(self, nums: List[int]) -> int:
         numsU = []
         ans = 0
-        p = nums[0]
 
         for i in range(len(nums)):
             if i == 0 or i == len(nums)-1:
                 numsU.append(nums[i])
-            elif nums[i] != p:
+            elif nums[i] !=  nums[i-1]:
                 numsU.append(nums[i])
-                p = nums[i]
-    
-        print(numsU)
 
         for i in range(1, len(numsU)-1):
             p, c, n = numsU[i-1], numsU[i], numsU[i+1]
