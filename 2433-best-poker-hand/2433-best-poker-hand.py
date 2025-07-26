4aa6c3700f5785c1f40db6114e4d6ebde1401529
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-26 08:15:35 -0400

    Time: 3 ms (6.92%), Space: 17.9 MB (36.48%) - LeetHub

---
+++ 2433-best-poker-hand/2433-best-poker-hand.py
@@ -0,0 +1,21 @@
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        if len(set(suits)) == 1:
+            return "Flush"
+
+        maxKind = max(Counter(ranks).values())
+        print(maxKind)
+        print(Counter(ranks).values())
+
+        if maxKind > 2:
+            return "Three of a Kind"
+        elif maxKind > 1:
+            return "Pair"
+        else:
+            return "High Card"
+
+
+
+        
+        
+        
