bf519266afa6fc3b5449a6f6649bb5b3b6171e14
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-26 21:26:45 -0400

    Time: 0 ms (100%), Space: 17.8 MB (39.28%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -4,17 +4,14 @@ class Solution:
         ans = 0
 
         for i in range(len(nums)):
-            if i == 0 or i == len(nums)-1:
-                numsU.append(nums[i])
-            elif nums[i] !=  nums[i-1]:
-                numsU.append(nums[i])
+            if numsU and numsU[-1] == nums[i]:
+                continue
+            numsU.append(nums[i])
 
         for i in range(1, len(numsU)-1):
             p, c, n = numsU[i-1], numsU[i], numsU[i+1]
 
-            if c > p and c > n:
-                ans += 1
-            elif c < p and c < n:
+            if p < c > n or p > c < n:
                 ans += 1
         
         return ans
