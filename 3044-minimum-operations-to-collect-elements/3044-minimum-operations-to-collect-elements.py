2e5ebf659e6cfc31466c81a659fa6cfc8fff206e
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-28 09:51:27 -0400

    Time: 4 ms (11.24%), Space: 17.6 MB (89.14%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
@@ -1,15 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        operations = 0
-        collected = set()
+        numsMap = {nu: i for i, nu in enumerate(nums)}
+        idx = []
+
+        for i in range(1, k+1):
+            idx.append(numsMap[i])
         
-        for i in range(len(nums) - 1, -1, -1):
-            operations += 1
-            
-            # If we found one of our target numbers
-            if 1 <= nums[i] <= k:
-                collected.add(nums[i])
-            
-            # Check if we have collected all numbers from 1 to k
-            if len(collected) == k:
-                return operations
+        return len(nums) - min(idx)
