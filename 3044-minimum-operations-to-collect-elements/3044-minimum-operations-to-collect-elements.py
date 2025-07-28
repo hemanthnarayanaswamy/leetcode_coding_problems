fa651fa5dec372db66752bda301bc28dd1927c2b
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-28 09:39:04 -0400

    Time: 2 ms (32.21%), Space: 17.9 MB (13.11%) - LeetHub

---
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        numsMap = {nu: i for i, nu in enumerate(nums)}
+        idx = []
+
+        for i in range(1, k+1):
+            idx.append(numsMap[i])
+        
+        return len(nums) - min(idx)
+            
