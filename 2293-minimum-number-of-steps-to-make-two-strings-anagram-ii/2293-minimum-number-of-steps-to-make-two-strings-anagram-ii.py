6ced94d92892511dec5dffbce20549046ff3f7a2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-29 09:39:55 -0400

    Time: 150 ms (68.62%), Space: 19.2 MB (13.39%) - LeetHub

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.py
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.py
@@ -6,11 +6,6 @@ class Solution:
 
         for i in range(97, 123):
             c = chr(i)
-            if c in sFreq and c not in tFreq:
-                operations += sFreq[c]
-            elif c in tFreq and c not in sFreq:
-                operations += tFreq[c]
-            else:
-                operations += abs(sFreq.get(c, 0) - tFreq.get(c, 0))
+            operations += abs(sFreq.get(c, 0) - tFreq.get(c, 0))
         
         return operations
