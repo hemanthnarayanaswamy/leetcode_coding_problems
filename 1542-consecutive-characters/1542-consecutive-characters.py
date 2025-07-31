82712e35dd5aa94d5b011b54275db6d61cff8c5c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-31 10:31:51 -0400

    Time: 4 ms (77.97%), Space: 17.7 MB (82.86%) - LeetHub

--- 1542-consecutive-characters/1542-consecutive-characters.py
+++ 1542-consecutive-characters/1542-consecutive-characters.py
@@ -6,7 +6,8 @@ class Solution:
             if s[i] == s[i-1]:
                 current_power += 1
             else:
-                max_power = max(max_power, current_power)
+                if current_power > max_power:
+                    max_power = current_power
                 current_power = 1
         
         return max(max_power, current_power)
