0a32151f3772a5a3f6bcd511d12ceb9fd2434128
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-07-31 10:17:17 -0400

    Time: 3 ms (68.03%), Space: 17.9 MB (47.54%) - LeetHub

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -1,26 +1,24 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
-        numsFreq = Counter(nums)
+        nums_freq = Counter(nums)  
         count = 0
+        target_len = len(target)   
 
         for prefix in set(nums):
-            np = len(prefix)
-            if np >= len(target):
+            prefix_len = len(prefix)
+            if prefix_len >= target_len:
                 continue
 
             if target.startswith(prefix):
-                suffix = target[np:]
+                suffix = target[prefix_len:]
 
-                if suffix in numsFreq:
-                    y = numsFreq[suffix]
-                    x = numsFreq[prefix]
+                if suffix in nums_freq:
+                    suffix_count = nums_freq[suffix]
+                    prefix_count = nums_freq[prefix]
 
                     if prefix != suffix:
-                        count += y * x
+                        count += suffix_count * prefix_count
                     else: 
-                        count += x * (x - 1)
+                        count += prefix_count * (prefix_count - 1)
                     
         return count
-                    
-
-
