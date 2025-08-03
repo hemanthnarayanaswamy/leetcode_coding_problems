73001592dc6b670d32bc3c3b19e8d0c45428cae0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-03 10:27:58 -0400

    Time: 55 ms (5.07%), Space: 22.1 MB (7.82%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        myCounter = {5: 0, 10: 0}
+
+        for cash in bills:
+            if cash == 20:
+                if myCounter[10] != 0 and myCounter[5] != 0:
+                    myCounter[10] = myCounter.get(10) - 1
+                    myCounter[5] = myCounter.get(5) - 1
+                elif myCounter[10] == 0 and myCounter[5] >= 3:
+                    myCounter[5] = myCounter.get(5) - 3
+                else:
+                    return False
+            elif cash == 10:
+                if myCounter[5] == 0:
+                    return False
+                else:
+                    myCounter[5] = myCounter.get(5) - 1
+                    myCounter[10] = myCounter.get(10) + 1
+            else:
+                myCounter[5] = myCounter.get(5) + 1
+            print(myCounter, cash)
+
+        return True
+
+
+
+
+            
