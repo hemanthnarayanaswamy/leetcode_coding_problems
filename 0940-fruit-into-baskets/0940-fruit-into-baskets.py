3a94b88b0818cf340b6ca47a21705bca58e69b75
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-04 10:11:42 -0400

    Time: 167 ms (80.27%), Space: 23.3 MB (93.46%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,20 +1,21 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        fruitsFreq = Counter()
+        fruitsFreq = defaultdict(int)
         startIdx = 0
         ans = 0
 
-        for i, fruit in enumerate(fruits):
+        for end, fruit in enumerate(fruits):
             fruitsFreq[fruit] += 1
 
-            while len(fruitsFreq) > 2:
+            if len(fruitsFreq) > 2:
                 fruitsFreq[fruits[startIdx]] -= 1
 
                 if fruitsFreq[fruits[startIdx]] == 0:
                     del fruitsFreq[fruits[startIdx]]
                 
                 startIdx += 1
+                ans = max(ans, end - startIdx + 1)
             
-            ans = max(ans, i - startIdx + 1)
+            
         
-        return ans
+        return max(ans, end - startIdx + 1)
