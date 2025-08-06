2fbd7bc944d034d73d28c74a746a47fd223feb3d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-06 09:21:13 -0400

    Time: 29 ms (98.44%), Space: 33.1 MB (69.78%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.py
@@ -3,16 +3,10 @@ class Solution:
         tasksFreq = Counter(tasks)
         count = 0
 
-        for v in tasksFreq.values():
-            if v == 1:
+        for _, v in tasksFreq.items():
+            if v < 2:
                 return -1 
             
-            q = v // 3
-            r = v % 3
+            count += (v - 1)//3 + 1
 
-            if r == 0:
-                count += q
-            else:
-                count += q + 1
-        
         return count
