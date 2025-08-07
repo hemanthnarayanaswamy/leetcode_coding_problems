7e312e8edad0690cab7e5ae2ac77d5a13cb882a1
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-07 11:34:05 -0400

    Time: 15 ms (89.59%), Space: 18.6 MB (75.22%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,36 +1,34 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         n = len(num)
-
+        
         if k >= n:
             return '0'
-
+        
         stack = []
         i = 0
-
-        while i < n and k > 0:
-            if stack:
-                if int(stack[-1]) > int(num[i]):
-                    stack.pop()
-                    k -= 1
-                else:
-                    stack.append(num[i])
-                    i += 1
-            else:
-                stack.append(num[i])
-                i += 1
-
-        res = stack + list(num[i:])
         
-        while k:
-            res.pop()
+        # Phase 1: Greedy removal using stack
+        while i < n and k > 0:
+            # Remove digits from stack if current digit is smaller
+            while stack and k > 0 and stack[-1] > num[i]:
+                stack.pop()
+                k -= 1
+            stack.append(num[i])
+            i += 1
+        
+        # Add remaining digits
+        while i < n:
+            stack.append(num[i])
+            i += 1
+        
+        # Phase 2: Remove remaining k digits from end
+        while k > 0:
+            stack.pop()
             k -= 1
         
-        res = (''.join(res)).lstrip('0')
+        # Phase 3: Handle leading zeros and empty result
+        result = ''.join(stack).lstrip('0')
         
-        return res if res else '0'
-                
-
-
-
+        return result if result else '0'
 
