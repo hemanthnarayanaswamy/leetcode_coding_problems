f37831342a1299b24c29b0b11c92a7d8908b38a6
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-11 12:25:56 -0400

    Time: 1 ms (53.89%), Space: 18 MB (46.03%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,26 +1,26 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        write = 0  # Where to write in the array
-        read = 0   # Where we're reading from
+        compressed = []
+        curr_char = ""
+        curr_char_count = 0
+        for c in chars:
+            if curr_char_count == 0:
+                curr_char = c
+                curr_char_count = 1
+            elif c == curr_char:
+                curr_char_count += 1
+            else: 
+                compressed.append(curr_char)
+                if curr_char_count != 1:
+                    compressed.extend(list(str(curr_char_count)))
+                curr_char = c
+                curr_char_count = 1
         
-        while read < len(chars):
-            # Step 1: Get current character
-            current_char = chars[read]
-            count = 0
-            
-            # Step 2: Count how many times it repeats
-            while read < len(chars) and chars[read] == current_char:
-                count += 1
-                read += 1
-            
-            # Step 3: Write the character
-            chars[write] = current_char
-            write += 1
-            
-            # Step 4: If more than 1, write the count
-            if count > 1:
-                for digit in str(count):
-                    chars[write] = digit
-                    write += 1
-        
-        return write
+        compressed.append(curr_char)
+        if curr_char_count != 1:
+            compressed.extend(list(str(curr_char_count)))
+        print(compressed)
+        chars[:len(chars)] = compressed
+        print(chars)
+        print(len(chars))
+        return len(chars)
