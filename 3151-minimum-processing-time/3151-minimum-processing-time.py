3afa29ad874dfa55d0f5f19b5a07ad23d0c057b2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-13 11:10:55 -0400

    Time: 67 ms (59.7%), Space: 32.8 MB (89.93%) - LeetHub

--- 3151-minimum-processing-time/3151-minimum-processing-time.py
+++ 3151-minimum-processing-time/3151-minimum-processing-time.py
@@ -2,13 +2,9 @@ class Solution:
     def minProcessingTime(self, processorTimes, taskTimes):
         processorTimes.sort()
         taskTimes.sort(reverse=True)
-        processorIndex = 0
         answer = 0
 
         for i in range(len(processorTimes)):
-            currentMax = 0
-            taskCount = 0
-
             answer = max(answer, processorTimes[i] + taskTimes[i*4])
         
         return answer
