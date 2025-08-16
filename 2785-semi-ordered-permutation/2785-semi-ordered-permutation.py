5e950a62c636d98fd1d01dc3a6a2e2c2404f97d7
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-16 09:26:24 -0400

    Time: 4 ms (19.08%), Space: 17.8 MB (36.18%) - LeetHub

--- 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
+++ 2785-semi-ordered-permutation/2785-semi-ordered-permutation.py
@@ -1,9 +1,6 @@
 class Solution:
     def semiOrderedPermutation(self, nums: List[int]) -> int:
         n = len(nums)
-        if nums[0] == 1 and nums[-1] == n:
-            return 0
-        
         idx1, idxn = 0, 0
 
         for i, num in enumerate(nums):
@@ -11,13 +8,7 @@ class Solution:
                 idx1 = i
             elif num == n:
                 idxn = i
-            
-            if idx1 and idxn:
-                break 
         
-        if idx1 > idxn:
-            count = (idx1 - 0) + (n-1 - idxn) - 1
-        else:
-            count = (idx1 - 0) + (n-1 - idxn)
-        
-        return count
+        count = (idx1 - 0) + (n-1 - idxn)
+
+        return count - 1 if idx1 > idxn else count 
