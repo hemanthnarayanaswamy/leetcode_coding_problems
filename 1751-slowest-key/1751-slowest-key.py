dd6707a90a0466a1e0bc99a025df77213536bae3
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-17 12:30:55 -0400

    Time: 0 ms (100%), Space: 17.9 MB (74.6%) - LeetHub

--- 1751-slowest-key/1751-slowest-key.py
+++ 1751-slowest-key/1751-slowest-key.py
@@ -1,27 +1,17 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        keyMap = {keysPressed[0]:releaseTimes[0]}
+        res, val = keysPressed[0], releaseTimes[0]
 
         for i in range(1, len(keysPressed)):
             tmp = releaseTimes[i] - releaseTimes[i-1]
             c = keysPressed[i]
-            
-            if c in keyMap:
-                if keyMap[c] < tmp:
-                    keyMap[c] = tmp
-            else:
-                keyMap[c] = tmp
-        
-        res, val = 0, 0
 
-        for c, v in keyMap.items():
-            if v > val:
-                val = v
+            if tmp > val:
+                val = tmp
                 res = c
-            elif v == val:
+            elif tmp == val:
                 if c > res:
-                    val = v
+                    val = tmp
                     res = c
         
         return res
-
