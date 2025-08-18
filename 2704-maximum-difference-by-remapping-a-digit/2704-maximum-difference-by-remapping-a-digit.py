3018777a63bd7234e0b58fdae388ab3caee3bbcb
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-18 09:38:21 -0400

    Time: 0 ms (100%), Space: 17.9 MB (32.36%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -1,29 +1,17 @@
 class Solution:
     def minMaxDifference(self, num: int) -> int:
         numStr = str(num)
-        replace_digit1 = None
-        replace_digit2 = None
-
+        replace_digit1 = replace_digit2 = None
+        
         for d in numStr:
             if replace_digit1 is None and d != '9':
                 replace_digit1 = d
-            
-            
             if replace_digit2 is None and d != '0':
                 replace_digit2 = d
-            
-            
-            if replace_digit1 is not None and replace_digit2 is not None:
+            if replace_digit1 and replace_digit2:  
                 break
-
-        if replace_digit2:
-            min_num = int(numStr.replace(replace_digit2, '0'))
-        else:
-            min_num = num
         
-        if replace_digit1:
-            max_num = int(numStr.replace(replace_digit1, '9'))
-        else:
-            max_num = num
-
+        max_num = int(numStr.replace(replace_digit1, '9')) if replace_digit1 else num
+        min_num = int(numStr.replace(replace_digit2, '0')) if replace_digit2 else num
+        
         return max_num - min_num
