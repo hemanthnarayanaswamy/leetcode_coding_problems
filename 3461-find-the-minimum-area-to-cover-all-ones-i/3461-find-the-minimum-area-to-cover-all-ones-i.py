5029c31063b144136610c818a7b100a1b79ec3ae
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-22 09:29:42 -0400

    Time: 3033 ms (10.07%), Space: 47.3 MB (97.84%) - LeetHub

---
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        min_height, min_width = float('inf'), float('inf')
+        max_height, max_width = 0, 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    min_height = min(min_height, i)
+                    min_width = min(min_width, j)
+                    max_height = max(max_height, i)
+                    max_width = max(max_width, j)
+        
+        print(min_height, max_height)
+        print(min_width, max_width)
+        area = (max_height - min_height + 1) * (max_width - min_width + 1)
+        
+        return area
