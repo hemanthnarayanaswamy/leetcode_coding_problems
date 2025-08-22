e7bad1e26ab636f45eb076caad51463c62f592ea
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-22 09:43:23 -0400

    Time: 2825 ms (61.87%), Space: 47.6 MB (64.75%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.py
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.py
@@ -6,9 +6,16 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
-                    min_height = min(min_height, i)
-                    min_width = min(min_width, j)
-                    max_height = max(max_height, i)
-                    max_width = max(max_width, j)
+                    if min_height == 0:
+                        max_height = max(max_height, i)
+                    else: 
+                        min_height = min(min_height, i)
+                        max_height = max(max_height, i)
+                    
+                    if min_width == 0:
+                        max_width = max(max_width, j)
+                    else:
+                        min_width = min(min_width, j)
+                        max_width = max(max_width, j)
         
         return (max_height - min_height + 1) * (max_width - min_width + 1)
