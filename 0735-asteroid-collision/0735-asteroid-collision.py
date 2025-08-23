b855a7f4f96060b57d3adbc629ba516cf75b67bf
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-23 11:12:11 -0400

    Time: 12 ms (8.97%), Space: 18.9 MB (37.53%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -4,17 +4,13 @@ class Solution:
         i = 0
 
         while i < len(asteroids):
-            if stack:
-                if stack[-1] > 0 and asteroids[i] < 0:
-                    if stack[-1] < abs(asteroids[i]):
-                        stack.pop()
-                    elif stack[-1] > abs(asteroids[i]):
-                        i += 1
-                    else:
-                        stack.pop()
-                        i += 1
+            if stack and (stack[-1] > 0 and asteroids[i] < 0):
+                if stack[-1] < abs(asteroids[i]):
+                    stack.pop()
+                elif stack[-1] > abs(asteroids[i]):
+                    i += 1
                 else:
-                    stack.append(asteroids[i])
+                    stack.pop()
                     i += 1
             else:
                 stack.append(asteroids[i])
