e7feeef6ba3c6eca0c7f9cd64ed37f33f49ab768
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-24 10:22:54 -0400

    Time: 168 ms (5.11%), Space: 23 MB (6.2%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,38 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        leftOnes = [0] * len(nums)
+        rightOnes = [0] * len(nums)
+
+        numsFreq = Counter(nums)
+
+        count1 = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count1 += 1
+            else:
+                leftOnes[i] = count1
+                count1 = 0
+        
+        count1 = 0
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] == 1:
+                count1 += 1
+            else:
+                rightOnes[i] = count1
+                count1 = 0
+        print(rightOnes, leftOnes)
+        longestLen = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                print(leftOnes[i], rightOnes[i])
+                tmp = leftOnes[i] + rightOnes[i]
+                if tmp > longestLen:
+                    longestLen = tmp
+        
+        if numsFreq.get(0, 0) == 0:
+            return len(nums) - 1
+        elif numsFreq.get(1, 0) == 0:
+            return 0
+        else:
+            return longestLen
