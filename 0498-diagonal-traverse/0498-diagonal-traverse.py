8da5cb8cdd2d33c1c26467736fc38de206aa6a23
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-25 10:14:01 -0400

    Time: 15 ms (42.84%), Space: 20.7 MB (29.99%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,17 +1,16 @@
-def findDiagonalOrder(mat):
-    diagonalMat = defaultdict(list)
-    res = []
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        diagonalMat = defaultdict(list)
+        res = []
 
-    for i in range(len(mat)):
-        for j in range(len(mat[0])):
-            diagonalMat[i+j].append(mat[i][j])
-    
-    # diagonalMat = dict(sorted(diagonalMat.items(), key=lambda item: item[0]))
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                diagonalMat[i+j].append(mat[i][j])
+        
+        for i in diagonalMat:
+            if i % 2:
+                res.extend(diagonalMat[i])
+            else:
+                res.extend(diagonalMat[i][::-1])
 
-    for i, val in diagonalMat.items():
-        if i % 2:
-            res.extend(val)
-        else:
-            res.extend(val[::-1])
-
-    return res
+        return res
