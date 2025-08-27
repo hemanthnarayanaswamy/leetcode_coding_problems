c6dd0c3825ecfa652d90a5db527c9c2d6b9a1e7d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-27 10:13:20 -0400

    Time: 0 ms (100%), Space: 18.6 MB (26.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,28 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
-        preMid = 0
-
-        if nums[l] == target: return l
-        if nums[r] == target: return r
-
-        while l <= r:
-            mid = (l+r)//2
-            print(nums[mid])
-            if mid == preMid:
-                break
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                l = mid
+                left = mid + 1
             else:
-                r = mid
-            
-            preMid = mid
+                right = mid - 1
         
-        if target > nums[r]:
-            return r + 1
-        elif target < nums[l]:
-            return l
-        else:
-            return l + 1
+        return left  # This is always the correct insertion point
