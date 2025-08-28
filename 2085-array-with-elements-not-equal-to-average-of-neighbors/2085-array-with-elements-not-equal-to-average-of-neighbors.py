11593c19b6c62d8a20cbdd16a066885ea30fce42
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-28 09:52:33 -0400

    Time: 62 ms (98.5%), Space: 30.9 MB (90.98%) - LeetHub

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,12 +1,20 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        i = 1
-
-        while i < len(nums)-1:
-            x, y = nums[i], nums[i+1]
-            nums[i] = y
-            nums[i+1] = x
-            i += 2
+        for i in range(1, len(nums)):
+            if i % 2: 
+                should_be_peak = True
+            else:
+                should_be_peak = False
+            
+            a, b = nums[i], nums[i-1]
+            
+            if should_be_peak and a > b:
+                continue
+            elif not should_be_peak and a < b:
+                continue
+            else:
+                    nums[i] = b
+                    nums[i-1] = a
         
         return nums
+        
