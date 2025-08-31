e21b2d5f4b9f1b7ab86e7016ed9d1f5c532c43b7
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-08-31 11:55:52 -0400

    Time: 158 ms (98.1%), Space: 17.8 MB (60.38%) - LeetHub

--- 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.py
+++ 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.py
@@ -1,11 +1,14 @@
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
-        def digitSum(num):
-            total = 0
-            while num > 0:
-                total += num % 10
-                num //= 10
-            return total
+        LIMIT = 45  # 1 <= input <= 10 ** 5, implies sum of digits <= 45
+        count = [0] * (LIMIT + 1)
 
-        counter = Counter(digitSum(i) for i in range(lowLimit, highLimit + 1))
-        return max(counter.values())
+        for x in range(lowLimit, highLimit + 1):
+            digit_sum = 0
+            while x:
+                digit_sum += x % 10
+                x //= 10
+            
+            count[digit_sum] += 1
+        
+        return max(count)
