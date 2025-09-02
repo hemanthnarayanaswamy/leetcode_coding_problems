641c1b767d2bd6a6bfda820428518c675169d444
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-02 11:46:36 -0400

    Time: 495 ms (87.07%), Space: 18 MB (29.31%) - LeetHub

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/2239-execution-of-all-suffix-instructions-staying-in-a-grid.py
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/2239-execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -1,19 +1,19 @@
 class Solution:
     def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
         directions = {'L': (0, -1), 'R': (0, 1), 'U': (-1, 0), 'D': (1, 0)}
-        result = []
+        m = len(s)
+        result = [0] * m
 
-        for start in range(len(s)):
-            x, y = startPos[0], startPos[1]
+        for i in range(len(s)):
+            x, y = startPos
             count = 0
-            for i in range(start, len(s)):
-                dx, dy = directions[s[i]]
+            for dir in s[i:]:
+                dx, dy = directions[dir]
                 x += dx
                 y += dy
 
                 if not (0 <= x < n and 0 <= y < n):
                     break
                 count += 1
-            result.append(count)
-        
+            result[i] = count
         return result
