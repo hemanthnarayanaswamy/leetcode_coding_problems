6f8911b9f8333fde022a2e7a1434bde3bdf61df5
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-10 09:47:34 -0400

    Time: 2 ms (86.64%), Space: 21.2 MB (67.71%) - LeetHub

--- 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/1888-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/1888-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -6,9 +6,13 @@ class Solution:
         for i, cor in enumerate(points):
             x1, y1 = cor 
             
-            if x1 == x or y1 == y:
-                tmpDistance = abs(x - x1) + abs(y - y1)
-
+            if x1 == x:
+                tmpDistance = abs(y - y1)
+                if tmpDistance < minDistance:
+                    minDistance = tmpDistance
+                    index = i
+            elif y1 == y:
+                tmpDistance = abs(x - x1)
                 if tmpDistance < minDistance:
                     minDistance = tmpDistance
                     index = i
