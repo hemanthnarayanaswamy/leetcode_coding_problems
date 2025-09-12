a73fe50fd1a0d259007b703bd6371ff7e3196288
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-12 10:28:01 -0400

    Time: 8 ms (42.84%), Space: 17.9 MB (47.07%) - LeetHub

--- 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.py
+++ 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.py
@@ -1,21 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
         res = 0
-        start = 0
-        totalSum = 0
-
+        
         for i, num in enumerate(nums):
-            totalSum += num 
-
-            newStart = max(0 , i - num)
-            if newStart > start:
-                totalSum -= sum(nums[start:newStart])
-                start = newStart
-            elif 0 <= newStart < start:
-                totalSum += sum(nums[newStart:start])
-                start = newStart
-            
-            res += totalSum
+            start = max(0, i - num)
+            # Calculate sum from start to i
+            subarray_sum = sum(nums[start:i+1])
+            res += subarray_sum
         
         return res
 
