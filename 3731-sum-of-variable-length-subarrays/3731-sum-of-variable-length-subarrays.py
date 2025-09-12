dedd7f08b665090d1ef39ebea33a255a1261074d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-12 10:36:25 -0400

    Time: 3 ms (88.97%), Space: 17.7 MB (75.7%) - LeetHub

--- 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.py
+++ 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.py
@@ -1,17 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
-        res = 0
-        prefix_sum = [0]
+        pre = [0]
+        for i, c in enumerate(nums):
+            pre.append(pre[-1] + c)
+            
+        ans = 0
+        for i, c in enumerate(nums):
+            start = max(0, i-c)
+            ans += pre[i+1] - pre[start]
         
-        # Build prefix sum array
-        for num in nums:
-            prefix_sum.append(prefix_sum[-1] + num)
-        
-        for i, num in enumerate(nums):
-            start = max(0, i - num)
-            # Sum from start to i using prefix sums
-            subarray_sum = prefix_sum[i + 1] - prefix_sum[start]
-            res += subarray_sum
-        
-        return res
-
+        return ans
