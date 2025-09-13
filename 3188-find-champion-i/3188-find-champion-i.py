a6e6f9b5f3ade6d2f4f0fe6094b7581089a8a1f3
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-13 12:40:15 -0400

    Time: 15 ms (24.32%), Space: 18.5 MB (26.35%) - LeetHub

--- 3188-find-champion-i/3188-find-champion-i.py
+++ 3188-find-champion-i/3188-find-champion-i.py
@@ -1,18 +1,16 @@
 class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        winsCount = [0] * n
-
-        for i in range(n):
-            for j in range(n):
-                if i != j and grid[i][j] == 1:
-                    winsCount[i] += 1
         maxWins = 0
         idx = -1
 
-        for i, w in enumerate(winsCount):
-            if w > maxWins:
+        for i in range(n):
+            wins = 0
+            for j in range(n):
+                if i != j and grid[i][j] == 1:
+                    wins += 1
+            if wins > maxWins:
+                maxWins = wins
                 idx = i
-                maxWins = w
         
         return idx
