d78281e7b5bd814cb8bad18a2dbafd250c8e6e0c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-18 20:33:35 -0400

    Time: 3 ms (80.15%), Space: 17.7 MB (68.81%) - LeetHub

--- 3736-find-valid-pair-of-adjacent-digits-in-string/3736-find-valid-pair-of-adjacent-digits-in-string.py
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/3736-find-valid-pair-of-adjacent-digits-in-string.py
@@ -1,15 +1,9 @@
 class Solution:
     def findValidPair(self, s: str) -> str:
-        sMap = Counter(s)
-
-        for i in range(len(s)-1):
-            a, b = s[i], s[i+1]
-
-            if a == b:
+        counter_map = collections.Counter(s)
+        for i in range(len(s) - 1):
+            if s[i] == s[i+1]:
                 continue
-            
-            if sMap[a] == int(a):
-                if sMap[b] == int(b):
-                    return a + b
-        
-        return ''
+            if counter_map[s[i]] == int(s[i]) and counter_map[s[i+1]] == int(s[i+1]):
+                return s[i:i+2]
+        return ""
