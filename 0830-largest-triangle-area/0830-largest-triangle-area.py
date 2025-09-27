fc9636d9f8f9a4952c08bdc4341349fdbc05f39c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-27 12:00:14 -0400

    Time: 27 ms (95.21%), Space: 17.5 MB (99.85%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.py
+++ 0830-largest-triangle-area/0830-largest-triangle-area.py
@@ -5,17 +5,13 @@ class Solution:
 
         for i in range(n):
             x1, y1 = points[i]
-            for j in range(n):
-                if i == j:
-                    continue
+            for j in range(i+1, n):
                 x2, y2 = points[j]
-                for k in range(n):
-                    if k == j or k == i:
-                        continue 
+                for k in range(j+1, n):
                     x3, y3 = points[k]
                     
-                    area = 0.5 * abs((x1 * (y2 - y3))+(x2 * (y3 - y1))+(x3 * (y1 - y2)))
+                    area = abs((x1 * (y2 - y3))+(x2 * (y3 - y1))+(x3 * (y1 - y2)))
                     if area > maxArea:
                         maxArea = area
         
-        return maxArea
+        return maxArea * 0.5
