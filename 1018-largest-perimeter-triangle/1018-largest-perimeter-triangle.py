c6c4081002f6c3634aae5c890815a78766771826
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-09-28 08:00:59 -0400

    Time: 15 ms (41.34%), Space: 18.7 MB (71.91%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.py
@@ -1,15 +1,14 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        # if nums_lenght < 3: ## If the given lengths are less than 3 sides return 0
-        #     return 0        ## It is not necessary as it'll be fine 
-    
-        nums.sort() ## sorting the array 
+         # inequality triangle a + b > c, a + c > b, and b + c > a.
 
-        for i in range(len(nums)-1, 1, -1): ## We are starting from the largest side
-            sum_of_sides = nums[i-1] + nums[i-2]
-            largest_side = nums[i]
-            if largest_side < sum_of_sides:
-                return largest_side + sum_of_sides
+        # sort list in descending order 
+        nums.sort(reverse=True) ## REversing the sorting order to make it easier
+        # print(nums)
+
+        for i in range(len(nums) - 2): ## using range normally will help 
+        # if meets condition (a+b > c), then return sum
+            if nums[i] < nums[i + 1] + nums[i + 2]:
+                return nums[i] + nums[i + 1] + nums[i + 2]
+        # else return 0;
         return 0
-
-        
