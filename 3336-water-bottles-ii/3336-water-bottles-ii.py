0a6ff3a95eb147cde704b98bb08f57b72c247bcd
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-02 09:44:11 -0400

    Time: 40 ms (48.33%), Space: 17.6 MB (90%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.py
+++ 3336-water-bottles-ii/3336-water-bottles-ii.py
@@ -3,9 +3,10 @@ class Solution:
         bottlesDrunk = emptyBottles = numBottles 
 
         while emptyBottles >= numExchange:
-            emptyBottles -= numExchange - 1
+            emptyBottles += 1 - numExchange  # +1 new empty, -numExchange used
             numExchange += 1
             bottlesDrunk += 1
 
         return bottlesDrunk
-        
+            
+    
