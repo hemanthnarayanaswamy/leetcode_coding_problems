c293e316361f4229431468cbf27c8ca1f25bd699
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-03 10:00:07 -0400

    Time: 3 ms (61.04%), Space: 17.8 MB (89.41%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.py
@@ -4,13 +4,12 @@ class Solution:
         column = {}
         count = 0
 
-        for r,c in indices:
-            row[r] = row.get(r, 0) + 1
+        for r,c in indices: # cumulative values 
+            row[r] = row.get(r, 0) + 1  
             column[c] = column.get(c, 0) + 1
         
         for i in range(m):
             for j in range(n): 
-                
                 if (row.get(i, 0) + column.get(j, 0)) % 2:
                     count += 1 
 
