0ca1f613433824837a80183d24778cc980cc8343
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-05 13:53:34 -0400

    Time: 2 ms (41.05%), Space: 17.9 MB (27.28%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.py
+++ 4008-restore-finishing-order/4008-restore-finishing-order.py
@@ -1,6 +1,3 @@
 class Solution:
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
-        order_index = {val: idx for idx, val in enumerate(order)}
-    
-        # Sort friends based on their index in order; missing friends go to the end
-        return sorted(friends, key=lambda x: order_index.get(x, len(order)))
+        return [i for i in order if i in friends]
