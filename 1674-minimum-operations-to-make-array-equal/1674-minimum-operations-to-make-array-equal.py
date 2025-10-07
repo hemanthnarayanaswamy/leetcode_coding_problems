c076d94d48338aa45e5a251e1d6afc5e25fc3bbf
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-07 12:41:11 -0400

    Time: 0 ms (100%), Space: 17.6 MB (95.6%) - LeetHub

--- 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.py
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.py
@@ -1,11 +1,4 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        arr = [(2*i)+1 for i in range(n)]
-        count = 0
-        target = sum(arr) // n
-
-        for i in arr:
-            if i < target:
-                count += target - i 
+        return n*n//4
         
-        return count
