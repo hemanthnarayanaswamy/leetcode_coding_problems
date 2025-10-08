a62ee5d7e4bd16b1a65170fcb214b5b93d704149
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-08 13:33:37 -0400

    Time: 0 ms (100%), Space: 17.8 MB (49.76%) - LeetHub

--- 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.py
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.py
@@ -1,11 +1,3 @@
 class Solution:
     def earliestTime(self, tasks: List[List[int]]) -> int:
-        result = float('inf')
-
-        for s,t in tasks:
-            tmp = s+t
-            if tmp < result:
-                result = tmp
-        
-        return result
-        
+        return min(s+t for s,t in tasks)
