99a666bd2f018aa77cb7d44d5deb5d2f76f6f285
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-11 16:09:21 -0400

    Time: 15 ms (70.24%), Space: 18.7 MB (81.26%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        attackTime = duration
+
+        for i in range(len(timeSeries)-1):
+            if timeSeries[i] + duration < timeSeries[i+1]:
+                attackTime += duration
+            else:
+                attackTime += (timeSeries[i+1] - timeSeries[i])
+        
+        return attackTime
