edc4bf876f8e38bd838bf2906394678ff101a36b
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-13 10:02:57 -0400

    Time: 23 ms (21.66%), Space: 17.8 MB (47.41%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         stack = []
+        prev_sig = None
         
         for word in words:
-            if stack and Counter(stack[-1]) == Counter(word):
+            sig = Counter(word)
+            if stack and prev_sig == sig:
                     continue
            
             stack.append(word)
+            prev_sig = sig
         
         return stack
