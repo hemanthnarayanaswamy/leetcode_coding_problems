10a4d60f0308fdb8ef4dfae90e901ed6037c815f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-17 10:48:18 -0400

    Time: 205 ms (93.66%), Space: 54 MB (49.49%) - LeetHub

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -2,15 +2,17 @@ class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         uniqueSet = set()
         n = len(s)
-        allcodesCount = 2 ** k
-        count = 0 
+        reqCount = 2 ** k
+
+        if k > n:
+            return False
 
         for i in range(n - k + 1):
             codes = s[i:i+k]
             if codes not in uniqueSet:
                 uniqueSet.add(codes)
-                count += 1
-            if count == allcodesCount:
+                reqCount -= 1
+            if reqCount == 0:
                 return True
         
-        return count == allcodesCount
+        return False
