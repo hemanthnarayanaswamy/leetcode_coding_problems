d6567f38da2d8878e67bb84f24665ade287bf3ec
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-21 12:05:39 -0400

    Time: 47 ms (68.37%), Space: 23.3 MB (57.43%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -2,16 +2,7 @@ class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         boats = 0
         people.sort()
-        n = len(people)
-        l, r = 0, n-1
-
-        for i in range(n):
-            if people[i] >= limit:
-                r = i - 1
-                break 
-        # New r idx will be where that person can the paired with min weight person to share a boat
-        
-        boats += len(people[r+1:]) # Boats for people greater in Weight
+        l, r = 0, len(people)-1
 
         while l <= r:
             if people[l] + people[r] <= limit:
