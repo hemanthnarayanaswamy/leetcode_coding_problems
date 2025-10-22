4e295c7027e20b70321d31cd4b2455471680382d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-22 10:39:35 -0400

    Time: 11 ms (46.86%), Space: 28.2 MB (95.89%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -3,10 +3,13 @@ class Solution:
         left = 0
         curr = 0
         best = float('inf')
+
         for right, x in enumerate(nums):
             curr += x
+
             while curr >= target:
                 best = min(best, right - left + 1)
                 curr -= nums[left]
                 left += 1
+                
         return 0 if best == float('inf') else best
