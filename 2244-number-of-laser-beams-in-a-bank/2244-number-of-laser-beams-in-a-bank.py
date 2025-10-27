b01ee09e7618031ea4e64610b7a6225153e573e2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-10-27 00:17:23 -0400

    Time: 3 ms (93.37%), Space: 19.5 MB (77.78%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -1,15 +1,14 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
+        numberDev = []
         lasers = 0
-        x = bank[0].count('1')
 
-        for i in range(1, len(bank)): 
-            y = bank[i].count('1')
-            if y:
-                lasers += x * bank[i].count('1')
-                x = y
+        for r in bank: # Create a Interger Array of number of devices per row ignoring zeros
+            d = r.count('1')
+            if d:
+                numberDev.append(d)
+        
+        for i in range(len(numberDev)-1):
+            lasers += numberDev[i] * numberDev[i+1]
         
         return lasers
-        
-
-        
