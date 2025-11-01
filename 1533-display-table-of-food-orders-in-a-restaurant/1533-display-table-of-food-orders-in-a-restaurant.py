22a9234ff81aef068aa41498a43e75bfd7250d9c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-01 13:56:07 -0400

    Time: 361 ms (15.99%), Space: 48.8 MB (11.9%) - LeetHub

---
+++ 1533-display-table-of-food-orders-in-a-restaurant/1533-display-table-of-food-orders-in-a-restaurant.py
@@ -0,0 +1,29 @@
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        header = set()
+        orderDetails = defaultdict(lambda: defaultdict(int))
+        tables = set()
+        displayTable = []
+
+        for order in orders:
+            table = int(order[1])
+            tables.add(table)
+            for item in order[2:]:
+                header.add(item)
+                orderDetails[table][item] += 1
+        
+        header = ["Table"]+sorted(header)
+        tables = sorted(tables)
+        print(tables)
+        displayTable.append(header)
+
+        for t in tables:
+            tmp = []
+            tmp.append(str(t))
+            for item in header[1:]:
+                tmp.append(str(orderDetails[t][item]))
+            displayTable.append(tmp)
+
+        return displayTable 
+
+
