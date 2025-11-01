fead5052517bd580611bb706088c2f97b246c915
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-01 14:26:26 -0400

    Time: 348 ms (40.15%), Space: 42.4 MB (57.99%) - LeetHub

--- 1533-display-table-of-food-orders-in-a-restaurant/1533-display-table-of-food-orders-in-a-restaurant.py
+++ 1533-display-table-of-food-orders-in-a-restaurant/1533-display-table-of-food-orders-in-a-restaurant.py
@@ -1,25 +1,16 @@
 class Solution:
     def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
-        header = set()
-        orderDetails = defaultdict(lambda: defaultdict(int))
-        tables = set()
+        tables = defaultdict(Counter)   # table -> {food: count}
+        foods = set()
 
-        for order in orders:
-            table = int(order[1])
-            tables.add(table)
-            for item in order[2:]:
-                header.add(item)
-                orderDetails[table][item] += 1
-        
-        header = ["Table"]+sorted(header)
-        tables = sorted(tables)
-        displayTable = [header]
+        for _, table, food in orders:   # each order has one food
+            t = int(table)
+            tables[t][food] += 1
+            foods.add(food)
 
-        for t in tables:
-            tmp = []
-            tmp.append(str(t))
-            for item in header[1:]:
-                tmp.append(str(orderDetails[t][item]))
-            displayTable.append(tmp)
-
-        return displayTable 
+        food_list = sorted(foods)
+        out = [["Table"] + food_list]
+        for t in sorted(tables):
+            c = tables[t]
+            out.append([str(t)] + [str(c[f]) for f in food_list])
+        return out
