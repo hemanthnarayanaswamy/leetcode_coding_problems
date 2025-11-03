1734e9df9a5a9a190341b138c7eead866a2404d9
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-03 09:59:53 -0500

    Time: 6 ms (79.05%), Space: 18.2 MB (9.46%) - LeetHub

--- 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.py
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.py
@@ -7,8 +7,6 @@ class Solution:
                 if grid[i-1][j] >= grid[i][j]:
                     tmp = abs(grid[i-1][j] - grid[i][j]) + 1
                     grid[i][j] += tmp
-                    print(grid[i][j], (abs(grid[i-1][j] - grid[i][j]) + 1))
                     operations += tmp
-                    print(operations)
-        print(grid)
+
         return operations
