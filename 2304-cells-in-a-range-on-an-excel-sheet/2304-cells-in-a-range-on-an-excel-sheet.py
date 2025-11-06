80801e08a1fa878e5faed350e8f02608f51ed243
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-06 09:40:46 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.22%) - LeetHub

--- 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.py
+++ 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.py
@@ -1,13 +1,6 @@
 class Solution:
     def cellsInRange(self, s: str) -> List[str]:
-        lower, upper = s.split(':')
-        c1, r1 = ord(lower[0]), int(lower[1])
-        c2, r2 = ord(upper[0]), int(upper[1])
-        res = []
-        
-        for cc in range(c1, c2+1):
-            cc = chr(cc)
-            for rr in range(r1, r2+1):
-                res.append(cc+str(rr))
-        
-        return res
+        lo, hi = s.split(':')
+        c1, c2 = ord(lo[0]), ord(hi[0])
+        r1, r2 = int(lo[1:]), int(hi[1:])
+        return [f"{chr(c)}{r}" for c in range(c1, c2 + 1) for r in range(r1, r2 + 1)]
