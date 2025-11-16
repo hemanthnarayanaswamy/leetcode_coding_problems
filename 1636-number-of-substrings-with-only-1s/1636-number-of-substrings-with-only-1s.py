5348897805779e0915945f0d191addadc431549f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-16 11:49:18 -0500

    Time: 11 ms (83.15%), Space: 18.1 MB (82.4%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
@@ -3,13 +3,16 @@ class Solution:
         MOD = 10**9 + 7
         total = 0
         run = 0
+        
         for ch in s:
             if ch == '1':
                 run += 1
-                total += run
             else:
+                total += (run * (run+1)) // 2
                 run = 0
-            if total >= MOD:  # optional, keeps value small
-                total %= MOD
-                
+                if total > MOD: 
+                    total %= MOD
+        
+        total += (run * (run+1)) // 2
+
         return total % MOD
