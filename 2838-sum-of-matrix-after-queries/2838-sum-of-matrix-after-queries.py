9fc86c9824b7568a86e8f16c2ccf8536e710c231
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-18 11:43:19 -0500

    Time: 47 ms (56.41%), Space: 36.4 MB (77.78%) - LeetHub

--- 2838-sum-of-matrix-after-queries/2838-sum-of-matrix-after-queries.py
+++ 2838-sum-of-matrix-after-queries/2838-sum-of-matrix-after-queries.py
@@ -1,17 +1,24 @@
 class Solution:
     def matrixSumQueries(self, n: int, queries: List[List[int]]) -> int:
-        rowSeen = set()
-        colSeen = set()
-        totalSum = 0
+        row_seen = set()    # 0/1 flags faster than set lookups
+        col_seen = set()
+        rem_rows = n
+        rem_cols = n
+        total = 0
 
-        for query in queries[::-1]:
-            t, idx, val = query
-            if t == 0:
-                if idx not in rowSeen:
-                    totalSum += val * (n - len(colSeen))
-                    rowSeen.add(idx)
+        for t, idx, val in reversed(queries):
+            if rem_rows == 0 and rem_cols == 0:
+                break
+
+            if t:
+                if idx not in col_seen:
+                    total += val * rem_rows
+                    col_seen.add(idx)
+                    rem_cols -= 1
             else:
-                if idx not in colSeen:
-                    totalSum += val * (n - len(rowSeen))
-                    colSeen.add(idx)
-        return totalSum
+                if idx not in row_seen:
+                    total += val * rem_cols
+                    row_seen.add(idx)
+                    rem_rows -= 1
+            
+        return total
