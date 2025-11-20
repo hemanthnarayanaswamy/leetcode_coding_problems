10198413a9f89997b69184579da710c2da1dd36e
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-20 10:57:59 -0500

    Time: 0 ms (100%), Space: 17.8 MB (51.82%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.py
@@ -4,16 +4,13 @@ class Solution:
 
         for c in s:
             if c == ')':
-                print(f'we have found the ( {stack}')
                 temp = []
                 while stack and stack[-1] != '(':
                     temp.append(stack.pop()[::-1])
-                print(f'This is temp {temp}')
                 stack.pop()
                 stack.append(''.join(temp))
             else:
                 stack.append(c)
-                print(stack)
 
         return ''.join(stack) if stack else ''
             
