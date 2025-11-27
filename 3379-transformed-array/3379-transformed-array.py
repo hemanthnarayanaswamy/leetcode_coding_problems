f8f2136e100b0848cc42343342fdf5cf3a5efab4
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-11-27 10:21:24 -0500

    Time: 63 ms (15.09%), Space: 17.7 MB (63.21%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -4,10 +4,6 @@ class Solution:
         res = [0] * n
 
         for i,num in enumerate(nums):
-            if nums[i] > 0:
-                j = (i+nums[i]) % n
-                res[i] = nums[j]
-            else:
                 j = (i + nums[i]) % n
                 res[i] = nums[j]
         
