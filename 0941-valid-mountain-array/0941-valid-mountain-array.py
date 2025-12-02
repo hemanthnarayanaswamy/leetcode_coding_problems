fecb566d2aeb2a95c47740088d815232f79fea2a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-02 11:47:07 -0500

    Time: 166 ms (38.62%), Space: 19.3 MB (27.46%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -2,12 +2,11 @@ class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         mountainPeak = False
         n = len(arr)
+        sortedArr = sorted(arr)
 
         if n < 3:
             return False
-        elif sorted(arr) == arr:
-            return False
-        elif sorted(arr, reverse=True) == arr:
+        elif sortedArr == arr or sortedArr == arr[::-1]:
             return False
 
         for i in range(n - 1):
