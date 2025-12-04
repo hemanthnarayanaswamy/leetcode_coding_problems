f118b7b3a67f2c806e7ec67c28b2e5632bddb3ac
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-04 11:30:22 -0500

    Time: 54 ms (27.79%), Space: 18.6 MB (50.91%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -1,14 +1,11 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        clockWiseDistance = anticlockWiseDistance = 0
-        n = len(distance)
+        clockWiseDistance = 0
 
         if start > destination:
             start, destination = destination, start
 
         for i in range(start, destination):
             clockWiseDistance += distance[i]
-
-        anticlockWiseDistance = sum(distance) - clockWiseDistance
         
-        return clockWiseDistance if clockWiseDistance < anticlockWiseDistance else anticlockWiseDistance
+        return min(clockWiseDistance, sum(distance) - clockWiseDistance)
