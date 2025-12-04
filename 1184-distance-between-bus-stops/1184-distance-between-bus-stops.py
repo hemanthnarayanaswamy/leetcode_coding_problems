4a777a0a731d888a26a29df0e658f86ace9f9816
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-04 11:31:50 -0500

    Time: 49 ms (58.18%), Space: 18.7 MB (33.25%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -1,12 +1,7 @@
 class Solution:
-    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-
-        total=sum(distance)
-        direct=0
-        l=min(start,destination)
-        r=max(start,destination)
-        while l<r:
-            direct+=distance[l]
-            l+=1
-        return min(direct,total-direct)
-        
+    def distanceBetweenBusStops(self, distance: list[int], start: int, destination: int) -> int:
+        if start > destination:
+            start, destination = destination, start
+        clockwise = sum(distance[start:destination])
+        total = sum(distance)
+        return min(clockwise, total - clockwise)
