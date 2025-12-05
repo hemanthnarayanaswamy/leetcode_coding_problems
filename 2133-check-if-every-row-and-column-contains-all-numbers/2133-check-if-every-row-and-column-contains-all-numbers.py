eb933cabd57363c8a2c1f1012a5af46e5ece69b5
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-05 03:44:55 -0500

    Time: 7 ms (99.16%), Space: 18.3 MB (94.88%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,15 +1,13 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
-
+        
         for row in matrix:
             if len(set(row)) != n:
                 return False
         
-        matrix_transpose = list(map(list, zip(*matrix)))
-
-        for row in matrix_transpose:
-            if len(set(row)) != n:
-                return False 
+        for col in zip(*matrix):
+            if len(set(col)) != n:
+                return False
         
         return True
