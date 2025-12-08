e0d76c05f79a9867985380170cfbab3f399f15ed
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-08 09:10:22 -0500

    Time: 374 ms (25.27%), Space: 17.9 MB (11.68%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        triplesCount = 0
+
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                c = int(sqrt(a**2 + b**2))
+                if c <= n and c**2 == a**2 + b**2:
+                    print(a,b,c)
+                    triplesCount += 1
+        
+        return triplesCount
