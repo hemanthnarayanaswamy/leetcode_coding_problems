1fa32d3988f1dda7970656f9e645b2d70211125c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-10 11:59:29 -0500

    Time: 0 ms (100%), Space: 17.8 MB (43.67%) - LeetHub

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
@@ -1,14 +1,13 @@
 class Solution:
     def winningPlayer(self, x: int, y: int) -> str:
-        play = False
-
-        while x and y:
+        count = 0
+        while x > 0 and y > 0:
             x -= 1
             y -= 4
 
-            if x < 0 or y < 0:
-                break
-            else:
-                play = not play
+            if x >= 0 and y >= 0:
+                count += 1
         
-        return "Alice" if play else "Bob"
+        count %= 2
+        
+        return 'Alice' if count else 'Bob'
