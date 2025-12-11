feba6989b50e8573ecdee0e801e6bfa6e01a194a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-11 12:08:56 -0500

    Time: 0 ms (100%), Space: 18 MB (19.1%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -2,14 +2,13 @@ class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
         startTime = 0
         maxTaskTime = 0
-        EmpId = float('inf')
+        EmpId = None
 
         for id, endTime in logs:
             taskTime = endTime - startTime
             if taskTime > maxTaskTime:
                 EmpId = id
                 maxTaskTime = taskTime
-                print(EmpId, taskTime, maxTaskTime)
             elif taskTime == maxTaskTime:
                 EmpId = min(id, EmpId)
             
