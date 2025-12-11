d77e9574b994a2e7dcdf296cf69943e280bb7077
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-11 11:27:59 -0500

    Time: 0 ms (100%), Space: 17.9 MB (14.03%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -1,14 +1,15 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
+        if n == 0:
+            return 0
+
         nums = [0, 1] + [0] * (n-1)
 
         for i in range(2, n+1):
             q, r = divmod(i, 2)
-
-            if r == 0:
-                nums[i] = nums[q]
-            else:
-                nums[i] = nums[q] + nums[q+1]
+            nums[i] += nums[q]
+            if r:
+                nums[i] += nums[q+1]          
 
         return max(nums) 
         
