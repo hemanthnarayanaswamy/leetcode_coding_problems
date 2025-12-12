2ef1315eadaea67690b9337c8bd70f59af95e3e9
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-12 10:53:25 -0500

    Time: 128 ms (7.92%), Space: 18.1 MB (35.85%) - LeetHub

---
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countMentions(self, numberOfUsers: int, events: List[List[str]]) -> List[int]:
+        offlineTime = {str(i): 0 for i in range(numberOfUsers)}
+        mentioned = {str(i): 0 for i in range(numberOfUsers)}
+        order = {'OFFLINE': 0, 'MESSAGE': 1}
+
+        def processOffline(t, users):
+            offlineTime[users] = t + 60
+            
+        def processMessage(t, users):
+            if users == 'ALL':
+                for k in mentioned:
+                    mentioned[k] += 1
+            elif users == 'HERE':
+                for k in mentioned:
+                    if offlineTime[k] and t >= offlineTime[k]:
+                        mentioned[k] += 1
+                    elif not offlineTime[k]:
+                        mentioned[k] += 1
+            else:
+                users = users.split()
+                for user in users:
+                    id = user[2:]
+                    mentioned[id] += 1
+        
+        events = sorted(events, key=lambda x: (int(x[1]), order[x[0]]))
+        print(events)
+        for event in events:
+            e, t, users = event 
+            t = int(t)
+            if e == 'OFFLINE':
+                processOffline(t, users)
+                print(offlineTime)
+            elif e == 'MESSAGE':
+                processMessage(t, users)
+                
+        return [v for v in mentioned.values()]
