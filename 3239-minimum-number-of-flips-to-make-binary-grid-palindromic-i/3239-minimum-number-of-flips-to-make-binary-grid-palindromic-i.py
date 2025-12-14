9348c17f86e064cc892049fb6de8eb240a10e674
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-14 11:51:30 -0500

    Time: 233 ms (18.9%), Space: 58.3 MB (24.41%) - LeetHub

---
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minFlips(self, grid: List[List[int]]) -> int:
+        flipr = 0
+        flipc = 0
+        
+        def checkPalindrome(seq):
+            flip = 0
+            l, r = 0, len(seq)-1
+            while l <= r:
+                if seq[l] != seq[r]:
+                    flip += 1
+                l += 1
+                r -= 1
+            return flip
+        
+        for row in grid:
+            flipr += checkPalindrome(row)
+        
+        for col in zip(*grid):
+            flipc += checkPalindrome(list(col))
+        
+        return min(flipr, flipc)
+        
