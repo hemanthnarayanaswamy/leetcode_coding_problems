e868f3afddd70192363cf3f1e0b7cd58c18a0935
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-15 12:41:52 -0500

    Time: 55 ms (72.76%), Space: 30.1 MB (21.27%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -1,15 +1,14 @@
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        descentDays = 0
+        descentDays = 1
         contiguousDays = 1
 
         for i in range(1, len(prices)):
             if prices[i] == prices[i-1]-1:
                 contiguousDays += 1
             else:
-                descentDays += (contiguousDays*(contiguousDays+1)) // 2
                 contiguousDays = 1
         
-        descentDays += (contiguousDays*(contiguousDays+1)) // 2
+            descentDays += contiguousDays 
 
         return descentDays
