5f1016450732e1b3d4b294b88e7b7e71633412ff
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-17 10:31:07 -0500

    Time: 4 ms (71.6%), Space: 17.9 MB (59.22%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        n = len(grid)
         rowMax = [max(row) for row in grid]
         colMax = [max(col) for col in zip(*grid)]
-        totalSum = 0
-        n = len(grid)
         
+        totalSum = 0
         for i in range(n):
             for j in range(n):
                 totalSum += abs(min(rowMax[i], colMax[j])) - grid[i][j]
         
         return totalSum
-
