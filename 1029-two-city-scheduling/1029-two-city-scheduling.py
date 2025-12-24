9ff692ed3358791f939ede35dda12e2db044c389
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-24 10:47:58 -0500

    Time: 0 ms (100%), Space: 17.4 MB (96.01%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,21 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        a = b = len(costs) // 2
-        sortedCosts = sorted(costs, key=lambda x: abs(x[0] - x[1]), reverse=True)
+        n = len(costs) // 2
+        sortedCosts = sorted(costs, key=lambda x: x[0] - x[1])  # Sort by cost difference (A - B)
         totalCost = 0
 
         for costA, costB in sortedCosts:
-            if a and costA < costB:
+            if n:
                 totalCost += costA
-                a -= 1
-            elif b and costB < costA:
-                totalCost += costB
-                b -= 1
-            elif not a and costA < costB:
-                totalCost += costB
-                b -= 1
+                n -= 1
             else:
-                totalCost += costA
-                a -= 1
-        
+                totalCost += costB
+
         return totalCost
