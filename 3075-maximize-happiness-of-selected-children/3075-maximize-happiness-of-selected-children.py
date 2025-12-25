8815e09d5936c2dbcca99779b667b09d231dbe3d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2025-12-25 00:32:39 -0500

    Time: 793 ms (87.94%), Space: 43.4 MB (95.74%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -4,13 +4,11 @@ class Solution:
         maxHappy = 0
         
         for i in range(len(happiness)):
-            if k:
-                tmp = happiness[i] - i 
-                if tmp > 0:
-                    maxHappy += tmp
+            tmp = happiness[i] - i 
+            if tmp > 0:
+                maxHappy += tmp
             k -= 1
             if k == 0:
                 break
 
         return maxHappy
-        
