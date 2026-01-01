f6ed049a2b4ba6cdf14b607b6a68a37a27535431
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-01-01 04:20:20 -0500

    Time: 0 ms (100%), Space: 17.4 MB (86.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,14 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
+        num = ''.join([str(d) for d in digits])
+        num = int(num) + 1
         
-        # Traverse from the last digit
-        for i in range(n - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1  # Increment the current digit
-                return digits  # Return early (no need to modify other digits)
-            digits[i] = 0  # Set current digit to 0 (carry to next)
+        num = list(str(num))
 
-        # If all digits were 9 (e.g., [9,9,9] → [1,0,0,0])
-        return [1] + digits
-        
+        return [int(d) for d in num]
