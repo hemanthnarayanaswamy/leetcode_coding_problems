cfb7f737d8e70c87eb61af8edaba2ceba64cf750
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-01-20 09:05:39 -0500

    Time: 55 ms (58.13%), Space: 19.3 MB (23.89%) - LeetHub

--- 3206-alternating-groups-i/3206-alternating-groups-i.py
+++ 3206-alternating-groups-i/3206-alternating-groups-i.py
@@ -8,7 +8,7 @@ class Solution:
             curr = colors[i]
             nxt = colors[(i + 1) % n]
             
-            if prev != curr != nxt:
+            if prev != curr != prev and curr != nxt:
                 count += 1
                 
         return count  
