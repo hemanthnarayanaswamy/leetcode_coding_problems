2871db5f1327d3c68290bb755785e2452f60a486
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-01-27 10:07:08 -0500

    Time: 19 ms (84.68%), Space: 19.2 MB (34.27%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.py
@@ -1,9 +1,8 @@
 class Solution:
     def sumCounts(self, nums: List[int]) -> int:
-        n = len(nums)
         count = 0
 
-        for i in range(n):
+        for i in range(len(nums)):
             distinct = set()
             l = 0  # To track lenght instead of re calculating everytime
             for c in nums[i:]:
