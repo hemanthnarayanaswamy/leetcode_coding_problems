1ae761cbcb53e222f6c717343fd9771a3e3476af
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-01-31 16:19:11 -0500

    Time: 0 ms (100%), Space: 19.4 MB (27.17%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def missingInteger(self, nums: List[int]) -> int:
-        total = prev = nums[0]
+        res = prev = nums[0]
         n = len(nums)
 
         for i in range(1, n):
             if nums[i] == prev + 1:
-                total += nums[i]
+                res += nums[i]
                 prev = nums[i]
             else:
                 break
         
         numsSearch = set(nums)
-        res = total
 
         while res in numsSearch:
             res += 1
