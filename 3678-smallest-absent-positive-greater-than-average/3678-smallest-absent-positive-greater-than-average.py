9a1b3fac1285eddf98c9018130d3b44ff74644ff
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-03 10:31:19 -0500

    Time: 5 ms (60.8%), Space: 19.5 MB (13.85%) - LeetHub

--- 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
+++ 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
@@ -4,7 +4,6 @@ class Solution:
         avg = total / len(nums)
         numsPresent = set(nums)
         num = floor(avg) + 1 # Instead of using the int, floor the avg and add 1
-        print(num, avg)
         
         if num <= 0:
             num = 1
