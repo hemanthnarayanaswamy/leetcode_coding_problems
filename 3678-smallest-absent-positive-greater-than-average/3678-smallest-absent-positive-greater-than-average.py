18c197ca9aa613195f82c50bd71117c3f06e2e33
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-03 10:49:11 -0500

    Time: 4 ms (70.89%), Space: 19.5 MB (13.85%) - LeetHub

--- 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
+++ 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
@@ -1,12 +1,8 @@
 class Solution:
     def smallestAbsent(self, nums: List[int]) -> int:
         avg = sum(nums) / len(nums)
-        num = floor(avg) + 1 #Instead of using the int, floor the avg and add 1
-
-        if num <= 0:
-            num = 1
-
-        numsPresent = set(nums)
+        num = max(1, floor(avg) + 1) #Instead of using the int, floor the avg and add 1
+        numsPresent = set(nums) # if num is negative we are resetting its start value to 1
         
         while num in numsPresent:
             num += 1
