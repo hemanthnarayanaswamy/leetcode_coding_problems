d33915dafc769958e0428d3aa33f541424354b4a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-04 10:53:45 -0500

    Time: 57 ms (53.31%), Space: 19.5 MB (9.93%) - LeetHub

---
+++ 2908-minimum-sum-of-mountain-triplets-i/2908-minimum-sum-of-mountain-triplets-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumSum(self, nums: List[int]) -> int:
+        res = float('inf')
+        n = len(nums)
+
+        for i in range(n-2):
+            for j in range(i+1, n-1):
+                if nums[i] >= nums[j]:
+                    break
+                print(i, j)
+                for k in range(j+1, n):
+                    if nums[k] >= nums[j]:
+                        continue
+                    res = min(res, nums[i]+nums[j]+nums[k])
+        
+        return res if res != float('inf') else -1
