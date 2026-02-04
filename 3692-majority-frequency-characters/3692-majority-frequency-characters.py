85c4eb718a24c097449b623537cc4ceba8846e78
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-04 15:29:36 -0500

    Time: 3 ms (74.66%), Space: 19.3 MB (37.47%) - LeetHub

--- 3692-majority-frequency-characters/3692-majority-frequency-characters.py
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.py
@@ -1,16 +1,11 @@
 class Solution:
     def majorityFrequencyGroup(self, s: str) -> str:
-        sFreq = Counter(s)
-        freqGroups = Counter([sFreq[k] for k in sFreq])
-        majorFreqGroup = groupSize = 0
+        counter = Counter(s)
+        d = defaultdict(list)
 
-        for k, v in freqGroups.items():
-            if v > groupSize:
-                majorFreqGroup = k
-                groupSize = v
-            elif v == groupSize:
-                majorFreqGroup = max(k, majorFreqGroup)
+        for ch, freq in counter.items():
+            d[freq].append(ch)
+
+        ans = max(d, key = lambda x: (len(d[x]), x)) 
         
-        res = [k for k, v in sFreq.items() if v == majorFreqGroup]
-        
-        return ''.join(res)
+        return ''.join(d[ans])
