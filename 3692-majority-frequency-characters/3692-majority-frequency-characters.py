c84eb30e7b7e58a0e959a2f4839dc05ee9c29291
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-04 14:44:27 -0500

    Time: 9 ms (6.06%), Space: 19.4 MB (21.21%) - LeetHub

---
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def majorityFrequencyGroup(self, s: str) -> str:
+        sFreq = Counter(s)
+        print(sFreq)
+        freq = [v for v in sFreq.values()]
+        freqGroups = Counter(freq)
+        majorityGroup = 0
+        groupSize = 0
+
+        for k, v in freqGroups.items():
+            if v > groupSize:
+                majorityGroup = k
+                groupSize = v
+            elif v == groupSize:
+                majorityGroup = max(k, majorityGroup)
+            print(majorityGroup)
+        
+        res = ''
+        for k, v in sFreq.items():
+            if v == majorityGroup:
+                res += k
+        print(freqGroups)
+        print(majorityGroup)
+        return res
