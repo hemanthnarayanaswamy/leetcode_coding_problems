e01a403528c7480288720d8a01a5f63e7e45ac61
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-05 08:50:12 -0500

    Time: 62 ms (43.95%), Space: 19.3 MB (34.53%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        res = []
+        res = [0] * n
 
         for i,num in enumerate(nums):
                 j = (i + num) % n
-                res.append(nums[j])
+                res[i] = nums[j]
         
         return res
