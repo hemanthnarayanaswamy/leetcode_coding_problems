2fadaa63768091ade35a26753a7c87ab13630d67
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-06 12:19:23 -0500

    Time: 63 ms (99.64%), Space: 34.7 MB (28.52%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
@@ -2,19 +2,10 @@ class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
         n = len(nums)
-        moves = float('inf')
+        l = 0
 
-        l, r = 0, 1
-
-        while r < n:
+        for r in range(n):
             if nums[r] > k * nums[l]:
-                moves = min(moves, n - (r - l))
                 l += 1
-            else:
-                r += 1
         
-        moves = min(moves, n - (r - l))
-        
-        return 0 if moves == float('inf') else moves
-
-        
+        return l
