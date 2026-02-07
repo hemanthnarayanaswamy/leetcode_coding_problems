a046c93c0effea5a0bb7d83fe7f8d5733d23e360
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-07 12:23:34 -0500

    Time: 0 ms (100%), Space: 19.4 MB (77.7%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.py
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseByType(self, s: str) -> str:
         s = list(s)
-        special_characters = '"!@#$%^&*()-+?_=,<>/"'
         def reverseLetter(s):
             l, r = 0, len(s)-1
             while l < r:
@@ -13,24 +12,20 @@ class Solution:
                     s[l], s[r] = s[r], s[l]
                     l += 1
                     r -= 1
-            return s
+            return reverseSpecialChar(s)
         
         def reverseSpecialChar(s):
             l, r = 0, len(s)-1
             while l < r:
-                if s[l] not in special_characters:
+                if s[l].isalpha():
                     l += 1
-                elif s[r] not in special_characters:
+                elif s[r].isalpha():
                     r -= 1
                 else:
                     s[l], s[r] = s[r], s[l]
                     l += 1
                     r -= 1
-            return s
-        
-        s = list(s)
-        s = reverseLetter(s)
-        s = reverseSpecialChar(s)
+            return ''.join(s)
 
-        return ''.join(s)
+        return reverseLetter(s)
         
