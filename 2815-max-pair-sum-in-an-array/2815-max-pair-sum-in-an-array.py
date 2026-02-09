4ef1c7ecdc78e72c260f5b38378dfaffc0a9292e
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-09 15:03:11 -0500

    Time: 21 ms (72.3%), Space: 19.3 MB (54.05%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.py
@@ -2,17 +2,17 @@ class Solution:
     def maxSum(self, nums: List[int]) -> int:
         numsMap = defaultdict(list)
         res = -1
-        # def maxDigit(num):
-        #     d = 0
-        #     while num:
-        #         q, r = divmod(num, 10)
-        #         d = max(d, r)
-        #         num = q
-        #     return d
+        
+        def maxDigit(num):
+            d = 0
+            while num:
+                q, r = divmod(num, 10)
+                d = max(d, r)
+                num = q
+            return d
         
         for num in nums:
-            # d = maxDigit(num)
-            d = max(int(n) for n in str(num))
+            d = maxDigit(num)
             numsMap[d].append(num)
         
         for _, numList in numsMap.items():
