d0f782f04483eae334378efbb79e33e7468464fc
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-10 12:08:17 -0500

    Time: 4 ms (14.67%), Space: 19.5 MB (15.22%) - LeetHub

--- 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.py
+++ 3216-lexicographically-smallest-string-after-a-swap/3216-lexicographically-smallest-string-after-a-swap.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(n-1):
             a, b = s[i], s[i+1]
 
-            if int(a) % 2 == int(b) % 2:
+            if int(a) % 2 == int(b) % 2 and b < a:
                 num = s[:i]+b+a+s[i+2:] 
                 if num < res:
                     res = num
-                    
+
         return res
