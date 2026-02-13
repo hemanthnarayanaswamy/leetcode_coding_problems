767eeeec7dfbf8f9a221768609a9cbee0d070b0f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-13 12:06:32 -0500

    Time: 27 ms (47.5%), Space: 30.8 MB (62.81%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -2,8 +2,7 @@ class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
         diff = [0] * (n+1)
             
-        for booking in bookings:
-            f, l, s = booking
+        for f, l, s in bookings:
             diff[f] += s
             if l + 1 <= n:
                 diff[l+1] -= s
