ee577a7baeddedeabef2878cfcc76e41a0a852ac
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-19 13:38:31 -0500

    Time: 111 ms (27.94%), Space: 14.5 MB (28.74%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        groups = [1]
+        ans = 0
+
+        for i in range(n-1):
+            if s[i] != s[i+1]:
+                groups.append(1)
+            else:
+                groups[-1] += 1
+        
+        for i in range(len(groups)-1):
+            ans += min(groups[i], groups[i+1])
+        
+        return ans
+        
