d9f5b62c2d53f7991bd56f68dc201d9f85b51ba8
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-02-26 09:32:51 -0500

    Time: 2 ms (10.67%), Space: 19.3 MB (62.22%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        moves = 0
+
+        while target != 1:
+            if maxDoubles and target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+            else:
+                target -= 1
+            
+            moves += 1
+
+            if not maxDoubles:
+                moves += (target - 1)
+                break
+        
+        return moves
+        
+
+                
