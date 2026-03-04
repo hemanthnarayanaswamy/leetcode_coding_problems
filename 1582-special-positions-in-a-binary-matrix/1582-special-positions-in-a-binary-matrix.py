62fbe0a2a2e5b70d075f727533c4f6ab4c4bead3
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-04 09:44:26 -0500

    Time: 7 ms (55.34%), Space: 19.8 MB (63.75%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -5,15 +5,15 @@ class Solution:
 
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 1:
+                if mat[i][j]:
                     rows[i] += 1
                     cols[j] += 1
         
-        res = 0
-        
+        ans = 0
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 1 and rows[i] == 1 and cols[j] == 1:
-                    res += 1
+                if mat[i][j]:
+                    if rows[i] == 1 and cols[j] == 1:
+                        ans += 1
         
-        return res
+        return ans
