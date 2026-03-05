2c94dd0691476fd5938cf9ecc0b0d56d2f8a5b22
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-05 09:11:52 -0500

    Time: 3 ms (99.6%), Space: 19.4 MB (56.18%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -6,16 +6,12 @@ class Solution:
             if (i % 2):
                 if s[i] != '1':
                     res1 += 1
+                else:
+                    res2 += 1
             else:
                 if s[i] != '0':
                     res1 += 1
-        
-        for i in range(len(s)):
-            if (i % 2):
-                if s[i] != '0':
-                    res2 += 1
-            else:
-                if s[i] != '1':
+                else:
                     res2 += 1
         
         return min(res1, res2)
