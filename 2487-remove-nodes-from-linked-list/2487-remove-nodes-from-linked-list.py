43b90f265d390d9fd138745985eba6a7b0514ce2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-12 16:22:14 -0400

    Time: 151 ms (14.59%), Space: 64.3 MB (38.33%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         stack = []
@@ -11,11 +12,12 @@ class Solution:
         while cur:
             while stack and cur.val > stack[-1].val:
                 stack.pop()
-            
+
             if stack:
                 stack[-1].next = cur
-                
             stack.append(cur)
+
             cur = cur.next
-        
+            stack[-1].next = None
+
         return stack[0]
