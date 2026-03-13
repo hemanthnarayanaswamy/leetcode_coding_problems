9c54359f0e679bcdf1444fe323362fb13f408ac2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-13 13:21:25 -0400

    Time: 3 ms (65.7%), Space: 19.4 MB (38.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,34 +8,23 @@ class Solution:
         def add(a, b, c):
             res = a+b+c
             n = res % 10
-            c = res // 10
-            return n, c
+            car = res // 10
+            return n, car
         
         dummy = ListNode(0)
         cur = dummy
-        p1 = l1
-        p2 = l2
         carry = 0
 
-        while p1 or p2:
-            if p1 and p2:
-                n, c = add(p1.val, p2.val, carry)
-                p1 = p1.next
-                p2 = p2.next
-            elif p1 and not p2:
-                n, c = add(p1.val, 0, carry)
-                p1 = p1.next
-            else:
-                n, c = add(0, p2.val, carry)
-                p2 = p2.next
-            
+        while l1 or l2 or carry:
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+            n, carry = add(a, b, carry)
+                
             cur.next = ListNode(n)
-            carry = c
             cur = cur.next
-        
-        if carry:
-            cur.next = ListNode(carry)
-            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
         cur.next = None
 
         return dummy.next
