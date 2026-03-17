18664bf2bb6f257c941f41c7b6e877686cc51d9f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-17 09:05:37 -0400

    Time: 0 ms (100%), Space: 19.3 MB (83.21%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -1,12 +1,12 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        nums.sort(reverse=True)
+        nums.sort()
         res = []
 
-        while nums:
-            p1 = nums.pop()
-            p2 = nums.pop()
-            res.append(p2)
-            res.append(p1)
-        
+        # nums is sorted ascending, so smallest elements are at the front
+        # We take them in pairs and swap them
+        for i in range(0, len(nums), 2):
+            res.append(nums[i+1])
+            res.append(nums[i])
+
         return res
