5c75058b8d9a530afd14053863a657fe376c9d97
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-19 10:34:16 -0400

    Time: 11 ms (38.88%), Space: 19.3 MB (83.78%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -9,7 +9,11 @@ class Solution:
         res = 0
         for i in range(len(words)):
             for j in range(i+1, len(words)):
-                if isPrefixAndSuffix(words[i], words[j]):
+                str1, str2 = words[i], words[j]
+                if len(str1) > len(str2):
+                    continue
+
+                if isPrefixAndSuffix(str1, str2):
                     res += 1
         
         return res
