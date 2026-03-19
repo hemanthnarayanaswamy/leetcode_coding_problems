36af618de0b2dd51e0f00d46b5fd058caa688432
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-19 10:35:09 -0400

    Time: 12 ms (24.2%), Space: 19.2 MB (83.78%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -5,8 +5,9 @@ class Solution:
             return str2[:n] == str1 and str2[-n:] == str1
 
         res = 0
-        for i in range(len(words)):
-            for j in range(i+1, len(words)):
+        n = len(words)
+        for i in range(n):
+            for j in range(i+1, n):
                 str1, str2 = words[i], words[j]
                 if len(str1) > len(str2):
                     continue
