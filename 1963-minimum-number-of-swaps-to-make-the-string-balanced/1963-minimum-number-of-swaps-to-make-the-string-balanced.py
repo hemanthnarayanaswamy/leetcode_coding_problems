2c8d93654558da9b7aef091b0c367269659b87c1
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-26 11:00:17 -0400

    Time: 145 ms (64.44%), Space: 25 MB (94.23%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,23 +1,15 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        s = list(s)
-        brackets = {'[': 0, ']': 0}
-        l, r = 0, len(s)-1
-        swaps = 0
+        unmatched = 0
 
-        while l < r:
-            brackets[s[l]] += 1
-
-            while brackets[']'] > brackets['[']:
-                if s[l] == ']' and s[r] == '[':
-                    s[l], s[r] = s[r], s[l]
-                    brackets['['] += 1
-                    brackets[']'] -= 1
-                    swaps += 1
+        for ch in s:
+            if ch == '[':
+                unmatched +=1
+            else:
+                if unmatched > 0:
+                    unmatched -= 1
                 else:
-                    r -= 1  
-            l += 1
+                    unmatched += 1
         
-        return swaps
-
+        return (unmatched+1)//2
 
