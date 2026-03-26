27553be8826c1dd4de8a0e432bdd248b30512587
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-26 11:48:42 -0400

    Time: 3 ms (83.36%), Space: 19.3 MB (96.21%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
@@ -2,6 +2,7 @@ class Solution:
     def deleteGreatestValue(self, grid: List[List[int]]) -> int:
         for row in grid:
             row.sort()
+            
         res = 0
 
         for col in zip(*grid):
