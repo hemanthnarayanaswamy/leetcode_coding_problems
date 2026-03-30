63e4c2ad5deb86cca22e3eb7af3bfead91f7936d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-30 09:03:14 -0400

    Time: 43 ms (97.76%), Space: 20 MB (95.52%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,13 +1,8 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        n = len(s1)
-        
-        odds1 = s1[1:n:2]
-        evens1 = s1[0:n:2]
-        odds2 = s2[1:n:2]
-        evens2 = s2[0:n:2]
-        
-        if len(odds1) != len(odds2) or len(evens1) != len(evens2) or Counter(odds1) != Counter(odds2) or Counter(evens1) != Counter(evens2):
-            return False
-        
-        return True
+        # strategy:
+        # 1. odd indexes should contain the same elements in s1 and s2        
+        # 2. even indexes should contain the same elements in s1 and s2
+
+        return Counter(s1[::2]) == Counter(s2[::2]) and \
+               Counter(s1[1::2]) == Counter(s2[1::2])
