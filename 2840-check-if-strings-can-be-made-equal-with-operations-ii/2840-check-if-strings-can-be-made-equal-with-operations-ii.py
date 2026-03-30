245d1ef18f97fa3bcf532dd61536102b9d2ee363
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-03-30 09:01:13 -0400

    Time: 94 ms (70.15%), Space: 20.1 MB (82.09%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,28 +1,15 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
+        n = len(s1)
         if Counter(s1) != Counter(s2):
-            print('1')
             return False
         
-        oddParity_s1 = []
-        evenParity_s1 = []
-        oddParity_s2 = []
-        evenParity_s2 = []
+        odds1 = s1[1:n:2]
+        evens1 = s1[0:n:2]
+        odds2 = s2[1:n:2]
+        evens2 = s2[0:n:2]
         
-        for i in range(len(s1)):
-            if i % 2:
-                oddParity_s1.append(s1[i])
-                oddParity_s2.append(s2[i])
-            else:
-                evenParity_s1.append(s1[i])
-                evenParity_s2.append(s2[i])
-        
-        if len(oddParity_s1) != len(oddParity_s2) or len(evenParity_s1) != len(evenParity_s2):
-            print('2')
-            return False
-        
-        if Counter(oddParity_s1) != Counter(oddParity_s2) or Counter(evenParity_s1) != Counter(evenParity_s2):
-            print('3')
+        if len(odds1) != len(odds2) or len(evens1) != len(evens2) or Counter(odds1) != Counter(odds2) or Counter(evens1) != Counter(evens2):
             return False
         
         return True
