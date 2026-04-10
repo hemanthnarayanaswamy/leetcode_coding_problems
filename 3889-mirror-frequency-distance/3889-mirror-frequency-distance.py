1690eaa009790498c5977eba768a86e057495ac0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-10 13:04:27 -0400

    Time: 294 ms (62.73%), Space: 23.7 MB (71.56%) - LeetHub

--- 3889-mirror-frequency-distance/3889-mirror-frequency-distance.py
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.py
@@ -1,20 +1,19 @@
 class Solution:
     def mirrorFrequency(self, s: str) -> int:
         freq = Counter(s)
-        mirrorFreq = set()
         total = 0
+        visited = set()
 
-        for c in freq.keys():
+        for c in freq:
             if c.isnumeric():
                 mir = str(9 - int(c))
             else:
                 mir = chr(ord('z') - (ord(c) - ord('a')))
 
-            pair = (c, mir) if c > mir else (mir, c) # we generate character and mirror pair in sorted order
-
-            if pair not in mirrorFreq: # If the pair is unique only find the absolute difference
-                total += abs(freq[mir] - freq[c])
-                mirrorFreq.add(pair) 
+            if c not in visited: # If the pair is unique only find the absolute difference
+                total += abs(freq[c] - freq[mir]) 
+                visited.add(c)
+                visited.add(mir)
         
         return total # We need to return the sum of absolute difference of all unique pairs
             
