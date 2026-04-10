3a11dbd551fe8eaa7d583c3983944e9b34f94e15
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-10 13:55:17 -0400

    Time: 0 ms (100%), Space: 19.2 MB (69.98%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,9 +1,9 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        new = set([num for num in nums if num % k == 0])
+        nums = set(nums)
         max = 1000
 
         for i in range(1, max):
             n = i * k
-            if n not in new:
+            if n not in nums:
                 return n
