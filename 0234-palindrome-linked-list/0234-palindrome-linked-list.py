176048ca66dd75d555cace128b22e4888fb6e7f4
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-13 09:29:10 -0400

    Time: 47 ms (20.64%), Space: 42.4 MB (76.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,17 +3,36 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        cur = head
+        if not head or not head.next:
+            return True
 
-        while cur:
-            stack.append(cur.val)
-            cur = cur.next
-        
-        cur = head
-        while cur and cur.val == stack.pop():
-            cur = cur.next
-        
-        return cur is None # not cur
+        # 1. Find the middle using slow & fast pointers
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. If odd length, skip the middle element
+        if fast:
+            slow = slow.next
+
+        # 3. Reverse the second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # 4. Compare first half and reversed second half
+        left, right = head, prev
+        while right and left:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
