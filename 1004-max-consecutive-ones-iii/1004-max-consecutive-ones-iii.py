846591fdc57a834c0cefae30f4e35728b771eaa0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-14 09:33:49 -0400

    Time: 82 ms (11.1%), Space: 22.1 MB (58.84%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int: 
         maxCount = 0
-        freq = defaultdict(int)
+        freq = [0, 0]
         left = 0
 
         for right in range(len(nums)):
@@ -11,6 +11,6 @@ class Solution:
                 freq[nums[left]] -= 1
                 left += 1
             
-            maxCount = max(maxCount, freq[0]+freq[1])
+            maxCount = max(maxCount, sum(freq))
        
         return maxCount
