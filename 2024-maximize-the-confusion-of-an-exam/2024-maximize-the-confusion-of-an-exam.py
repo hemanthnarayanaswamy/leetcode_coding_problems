e01398b155f877ffeddd8c15d81550249f7437c5
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-21 09:25:23 -0400

    Time: 123 ms (74.46%), Space: 19.7 MB (4.06%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,21 +1,23 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        def countConsecutives(key):
-            left = 0
-            maxLen = count = 0
+        n = len(answerKey)
+        left = 0
+        countT = countF = 0
+        maxLen = 0
 
-
-            for right in range(len(answerKey)):
-                if answerKey[right] == key:
-                    count += 1
-
-                while count > k:
-                    if answerKey[left] == key:
-                        count -= 1
-                    left += 1
-                
-                maxLen = max(maxLen, right - left + 1)
+        for right in range(n):
+            if answerKey[right] == 'T':
+                countT += 1
+            else:
+                countF += 1
             
-            return maxLen
+            while min(countT, countF) > k:
+                if answerKey[left] == 'T':
+                    countT -= 1
+                else:
+                    countF -= 1
+                left += 1
+            
+            maxLen = max(maxLen, right-left+1)
         
-        return max(countConsecutives('T'), countConsecutives('F'))
+        return maxLen
