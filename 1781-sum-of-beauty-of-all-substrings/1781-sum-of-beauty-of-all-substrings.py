1f50b7c84165d3228659058d9b86f7456e808d7f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-04-29 10:30:15 -0400

    Time: 1331 ms (65.28%), Space: 19.3 MB (28%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,13 +1,14 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        n = len(s)
+        a = [ord(ch) - 97 for ch in s]
+        n = len(a)
         total = 0
 
         for i in range(n):
             arr = [0] * 26
             maxC = 0
             for j in range(i, n):
-                idx = ord(s[j])-97
+                idx = a[j]
                 arr[idx] += 1
                 maxC = max(maxC, arr[idx])
                 minC = min([val for val in arr if val != 0])
