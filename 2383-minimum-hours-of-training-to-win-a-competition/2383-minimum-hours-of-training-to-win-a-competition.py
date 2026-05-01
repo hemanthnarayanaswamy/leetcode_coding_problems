e219e8a0d8e7abb1c798faeda0bbd6db68c2d02f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-01 09:12:49 -0400

    Time: 0 ms (100%), Space: 19.2 MB (76.62%) - LeetHub

---
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minNumberOfHours(self, initialEnergy: int, initialExperience: int, energy: List[int], experience: List[int]) -> int:    
+        totalTime = 0
+        totalEnergy = sum(energy)
+
+        if totalEnergy >= initialEnergy:
+            energyTrainingTime = (totalEnergy - initialEnergy) + 1
+            initialEnergy += energyTrainingTime
+            totalTime += energyTrainingTime
+
+        for en, exp in zip(energy, experience):
+            if initialExperience > exp:
+                initialExperience += exp
+            else:
+                expTrainingTime = exp - initialExperience + 1
+                totalTime += expTrainingTime
+                initialExperience += (expTrainingTime + exp)
+            initialEnergy -= en
+        
+        return totalTime
