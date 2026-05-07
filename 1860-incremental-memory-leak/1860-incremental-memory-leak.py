8697d45cf54e774cbaf983d16b13ed9ad6426d5e
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-07 09:38:55 -0400

    Time: 127 ms (88.19%), Space: 19.3 MB (56.69%) - LeetHub

--- 1860-incremental-memory-leak/1860-incremental-memory-leak.py
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.py
@@ -2,7 +2,7 @@ class Solution:
     def memLeak(self, memory1: int, memory2: int) -> List[int]:
         memory_req = 1
 
-        while memory_req <= max(memory1, memory2):
+        while memory1 >= memory_req or memory2 >= memory_req:
             if memory1 >= memory2:
                 memory1 -= memory_req
             else:
