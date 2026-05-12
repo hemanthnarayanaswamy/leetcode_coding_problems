4b8c62bc3e93d2eaa78aac398c8560806962ffdc
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-12 11:14:06 -0400

    Time: 64 ms (36.63%), Space: 55.5 MB (60.4%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -3,13 +3,12 @@ class Solution:
         sorted_tasks = sorted(tasks, key=lambda x: x[1]-x[0], reverse=True)
         initial_energy = 0
         remaining_energy = 0
-        total_energy = 0
-
+    
         for energy, required_energy in sorted_tasks:
-            total_energy += energy
             if remaining_energy < required_energy:
                 initial_energy += required_energy - remaining_energy
-            remaining_energy = initial_energy - total_energy
+                remaining_energy += (required_energy - remaining_energy)
+            remaining_energy -= energy
         
         return initial_energy
 
