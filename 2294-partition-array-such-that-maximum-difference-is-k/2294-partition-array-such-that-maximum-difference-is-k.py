a906abcee5a34e870c8de332b8ad2a413761113a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-14 10:18:22 -0400

    Time: 83 ms (87.83%), Space: 33.5 MB (50.85%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.py
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.py
@@ -1,13 +1,13 @@
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        subCount = 1 # Atleast we'll have one subsequence
+        groups = 1 # Atleast we'll have one subsequence
         nums.sort()
         numMin = nums[0]
 
-        for i in range(1, n):
-            if nums[i] - numMin > k:
-                subCount += 1
-                numMin = nums[i]
+        for num in nums:
+            if num - numMin > k:
+                groups += 1
+                numMin = num
         
-        return subCount
+        return groups
