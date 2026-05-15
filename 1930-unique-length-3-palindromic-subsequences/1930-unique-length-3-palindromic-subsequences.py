c4dcce633253d7af3fdabf4c652370908c8f7ad2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-15 09:56:00 -0400

    Time: 429 ms (42.86%), Space: 19.9 MB (27.78%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -6,8 +6,8 @@ class Solution:
         for letter in letters:
             i, j = s.index(letter), s.rindex(letter)
             between = set()
-            for c in s[i+1:j]:
-                between.add(c)
+            for k in range(i+1, j):
+                between.add(s[k])
             count += len(between)
         
         return count
