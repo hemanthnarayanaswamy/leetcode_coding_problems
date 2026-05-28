e8fff655645f8c52fb5b623f1a5dc70d5b486069
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-05-28 10:27:51 -0400

    Time: 0 ms (100%), Space: 19.4 MB (25.13%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split()
+        s2 = sentence2.split()
+        n1 = len(s1)
+        n2 = len(s2)
+
+        # We make s1 is longer and s2 is shorter string
+        if n1 < n2:
+            return self.areSentencesSimilar(sentence2, sentence1)
+        
+        left = 0
+        right1 = n1-1
+        right2 = n2-1
+
+        while left < n2 and s1[left] == s2[left]:
+            left += 1
+        
+        while right2 >= 0 and s1[right1] == s2[right2]:
+            right1 -= 1
+            right2 -= 1
+        
+        print(left, right1, right2)
+        
+        if left > right2: 
+            return True
+        else:
+            return False
