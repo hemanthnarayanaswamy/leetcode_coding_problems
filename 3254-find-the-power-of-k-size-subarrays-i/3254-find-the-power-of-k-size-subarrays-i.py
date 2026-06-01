54dc4491f8ee97ecaf0c65ed3709ab5a7c9feede
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-01 10:39:57 -0400

    Time: 26 ms (29.91%), Space: 19.3 MB (46.73%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -1,20 +1,23 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        def checkSequence(arr):
-            maxNum = arr[0]
-            for i in range(1, len(arr)):
-                if arr[i-1] == arr[i]-1:
-                    maxNum = max(maxNum, arr[i])
-                else:
-                    return -1
-            return maxNum
-        
-        res = []
         n = len(nums)
+        prefix_good = []
+        res = []
 
-        for i in range(n-k+1):
-            tmp = nums[i:i+k]
-            res.append(checkSequence(tmp))
+        for i in range(1, n):
+            if nums[i] - nums[i-1] == 1:
+                prefix_good.append(1)
+            else:
+                prefix_good.append(0)
+        
+        print(prefix_good)
+        l, r = 0, k-1
+        while r < n:
+            if sum(prefix_good[l:r]) == k-1:
+                res.append(nums[r])
+            else:
+                res.append(-1)
+            l += 1
+            r += 1
         
         return res
-            
