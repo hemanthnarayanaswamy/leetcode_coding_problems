73f975f3f6a6384e86636c9f20ccc147e5e7708c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-02 11:39:55 -0400

    Time: 11 ms (66.86%), Space: 19.2 MB (98.26%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,24 +1,24 @@
 class Solution:
-    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        land_water = float('inf')
-        water_land = float('inf')
+    def earliestFinishTime(
+        self,
+        landStartTime: List[int],
+        landDuration: List[int],
+        waterStartTime: List[int],
+        waterDuration: List[int],
+    ) -> int:
+        def solve(start1, duration1, start2, duration2):
+            finish1 = inf
+            for i in range(len(start1)):
+                finish1 = min(finish1, start1[i] + duration1[i])
+            finish2 = inf
+            for i in range(len(start2)):
+                finish2 = min(finish2, max(start2[i], finish1) + duration2[i])
+            return finish2
 
-        for ls,le in zip(landStartTime, landDuration):
-            t1 = ls + le
-            for ws, we in zip(waterStartTime, waterDuration):
-                if t1 >= ws:
-                    t2 = t1 + we
-                else:
-                    t2 = (ws - t1) + t1 + we
-                land_water = min(land_water, t2)
-        
-        for ws, we in zip(waterStartTime, waterDuration):
-            t1 = ws + we
-            for ls,le in zip(landStartTime, landDuration):
-                if t1 >= ls:
-                    t2 = t1 + le
-                else:
-                    t2 = (ls - t1) + t1 + le
-                water_land = min(water_land, t2)
-        
-        return min(water_land, land_water)
+        land_water = solve(
+            landStartTime, landDuration, waterStartTime, waterDuration
+        )
+        water_land = solve(
+            waterStartTime, waterDuration, landStartTime, landDuration
+        )
+        return min(land_water, water_land)
