0ef23c3dac7e0768a27a22c857abd7658e1e6789
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-08 09:17:48 -0400

    Time: 71 ms (12.52%), Space: 33.6 MB (77.63%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1 
+
+        for i,j in zip(range(n), range(n-1,-1,-1)):
+            if nums[i] < pivot:
+                result[left] = nums[i]
+                left += 1
+            if nums[j] > pivot:
+                result[right] = nums[j]
+                right -= 1 
+        
+        while left <= right:
+            result[left] = pivot
+            left += 1
+        
+        return result
