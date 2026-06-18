90cd9afa9a0188dd6e98d68baa0cb25433d18e6a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-18 10:42:58 -0400

    Time: 481 ms (9.1%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,2 @@
 # Write your MySQL query statement below
-select p1.email Email
-from Person p1
-join Person p2 on p1.email = p2.email and p1.id != p2.id
-group by p1.email
+select email from person  group by email  having count(email)>1;
