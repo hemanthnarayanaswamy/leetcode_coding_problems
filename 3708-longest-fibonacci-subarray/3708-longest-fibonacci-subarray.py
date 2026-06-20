36aa7356fbf096a67e212d26f66faf1c13e321f2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-20 09:31:57 -0400

    Time: 31 ms (98.62%), Space: 33.7 MB (12.84%) - LeetHub

--- 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.py
+++ 3708-longest-fibonacci-subarray/3708-longest-fibonacci-subarray.py
@@ -5,18 +5,18 @@ class Solution:
         if n <= 2:
             return n
         
-        p1, p2 = nums[0], nums[1]
+        p1, p2 = nums[:2]
         maxFib = fib = 2
 
         for i in range(2, n):
             if p1 + p2 == nums[i]:
                 fib += 1
+                maxFib = max(maxFib, fib)
             else:
                 fib = 2
             
             p1, p2 = p2, nums[i]
-            maxFib = max(maxFib, fib)
-        
+            
         return maxFib
                 
         
