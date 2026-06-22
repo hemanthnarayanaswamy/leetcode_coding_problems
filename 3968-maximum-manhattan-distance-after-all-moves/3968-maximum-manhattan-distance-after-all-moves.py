15077b60be3abcde895215a5ee6faf4884fe0406
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-21 22:15:05 -0400

    Time: 143 ms (0%), Space: 20.1 MB (0%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.py
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxDistance(self, moves: str) -> int:
         dir = {'L': (-1, 0), 'D': (0, -1), 'U': (0, 1), 'R': (1, 0)}
-
-        x, y = 0, 0
-        count_ = 0
+        count_ = x = y = 0
 
         for d in moves:
             if d == '_':
                 count_ += 1
             else:
-                x1, y1 = dir[d]
-                x += x1
-                y += y1
+                x += dir[d][0]
+                y += dir[d][1]
         
-        dist = count_ + abs(0- x) + abs(0-y)
+        dist = count_ + abs(0 - x) + abs(0-y)
 
         return dist
