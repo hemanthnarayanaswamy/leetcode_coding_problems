807806b46b5868d964a17350bb95188a48303eea
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-25 09:47:28 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -3,6 +3,6 @@ select a.book_id, a.title, a.author, a.genre, a.publication_year, count(b.record
 from library_books a
 inner join borrowing_records b on a.book_id = b.book_id
 where b.return_date is null
-group by a.book_id, a.title, a.author, a.genre, a.publication_year, a.total_copies
+group by a.book_id
 having a.total_copies = count(b.record_id)
 order by current_borrowers desc, a.title asc
