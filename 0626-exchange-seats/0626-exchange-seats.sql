7f5ef2175cf48ad96abd27c7decc27357f612f9d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-06-26 11:10:41 -0400

    Time: 325 ms (99.41%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select (
+    case when id % 2 = 0 then id - 1 
+    when id=(select max(id) from seat) then id
+    else id + 1 end) id, student
+from Seat
+order by id
