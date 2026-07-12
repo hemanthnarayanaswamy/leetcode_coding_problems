7b5ceb9662edda8cedcc719b9decc8a9354826f8
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-07-12 10:24:27 -0400

    Time: 0 ms (100%), Space: 19.3 MB (66.45%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.py
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.py
@@ -2,8 +2,7 @@ class Solution:
     def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
         h1, m1, s1 = startTime.split(':')
         h2, m2, s2 = endTime.split(':')
-        print(h1, h2, m1, m2, s1, s2)
-
+        
         t1 = int(h1)*60*60 + int(m1)*60 + int(s1)
         t2 = int(h2)*60*60 + int(m2)*60 + int(s2)
 
