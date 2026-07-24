a8ac710bb2f9a61eae16b3c953f77911fa314650
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-07-24 13:59:12 -0400

    Time: 1644 ms (33.4%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,22 +1,11 @@
-SELECT c.category, COALESCE(a.accounts_count, 0) AS accounts_count
-FROM
-    (
-        SELECT 'Low Salary' AS category
-        UNION ALL
-        SELECT 'Average Salary'
-        UNION ALL
-        SELECT 'High Salary'
-    ) c
-LEFT JOIN
-    (
-        SELECT
-            CASE
-                WHEN income < 20000 THEN 'Low Salary'
-                WHEN income > 50000 THEN 'High Salary'
-                ELSE 'Average Salary'
-            END AS category,
-            COUNT(*) AS accounts_count
-        FROM Accounts
-        GROUP BY category
-    ) a
-ON c.category = a.category;
+SELECT 'Low Salary' AS category, 
+       COUNT(if(income<20000,1,null)) AS accounts_count
+FROM Accounts
+UNION ALL
+SELECT 'Average Salary', 
+       COUNT(if(income>=20000 and income<=50000,1,null))
+FROM Accounts
+UNION ALL
+SELECT 'High Salary', 
+       COUNT(if(income>50000,1,null))
+FROM Accounts;
