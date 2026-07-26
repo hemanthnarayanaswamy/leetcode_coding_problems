768b04c6b424eac4a80646e7ad0cb0d4e3e33f33
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-07-26 03:55:36 -0400

    Time: 0 ms (100%), Space: 19.2 MB (61.55%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
@@ -2,14 +2,16 @@ class Solution:
     def largestInteger(self, n: int, s: int) -> int:
         target = s
         nums = []
+        ans = ''
 
         for _ in range(n):
             digit = min(target, 9)
             target -= digit
             nums.append(digit)
+            ans += str(digit)
         
         if sum(nums) != s:
             return -1 
         
-        return int(''.join([str(num) for num in nums]))
+        return int(ans)
 
