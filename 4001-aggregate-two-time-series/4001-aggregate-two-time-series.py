a8bc4bf3917aad7987f0963365d3a9d620938e70
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-07-26 04:36:42 -0400

    Time: 188 ms (41.65%), Space: 76.2 MB (32.83%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.py
@@ -1,34 +1,29 @@
 class Solution:
     def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
-        n1 = len(series1)
-        n2 = len(series2)
-
         ans = []
         uniqueTime = set()
-        p1 = p2 = 0
 
-        for t1, v1 in series1:
-            while p2 < n2-1 and t1 > series2[p2][0]:
-                p2 += 1
-            
-            if t1 <= series2[p2][0]:
-                v2 = series2[p2][1]
-            else:
-                v2 = 0
-            ans.append([t1, v1+v2])
-            uniqueTime.add(t1)
-        
-        for t2, v2 in series2:
-            while p1 < n1-1 and t2 > series1[p1][0]:
-                p1 += 1
-            
-            if t2 <= series1[p1][0]:
-                v1 = series1[p1][1]
-            else:
-                v1 = 0
+        def aggregatedSeries(s1, s2):
+            p = 0
+            n1 = len(s1)
+            n2 = len(s2)
 
-            if t2 not in uniqueTime:
-                ans.append([t2, v1+v2])
-                uniqueTime.add(t2)
+            for t1, v1 in s1:
+                if t1 in uniqueTime:
+                    continue
 
+                while p < n2-1 and t1 > s2[p][0]:
+                    p += 1
+                
+                if t1 <= s2[p][0]:
+                    v2 = s2[p][1]
+                else:
+                    v2 = 0
+                
+                ans.append([t1, v1+v2])
+                uniqueTime.add(t1)
+            return
+
+        aggregatedSeries(series1, series2)
+        aggregatedSeries(series2, series1)
         return sorted(ans)
