849c345087f0a03cf11480f825243d0be4f19e50
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-02 16:51:43 -0400

    Time: 4060 ms (40%), Space: 19.4 MB (60%) - LeetHub

---
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxPairStrength(self, nums: list[int]) -> int:
+        n = len(nums)
+        strength = 0
+
+        for i in range(n):
+            for j in range(i+1, n):
+                a, b = nums[i], nums[j]
+
+                g = math.gcd(a, b)
+
+                tmp = (a * b) // g**2
+
+                if tmp > strength:
+                    strength = tmp
+        
+        return strength
+
