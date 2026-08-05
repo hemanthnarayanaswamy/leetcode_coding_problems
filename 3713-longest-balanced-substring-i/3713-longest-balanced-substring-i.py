0d97321f9e57ade9631f853f99c79b2012b5ffb4
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-05 09:39:22 -0400

    Time: 5084 ms (11.67%), Space: 19.2 MB (85.05%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -4,11 +4,22 @@ class Solution:
         n = len(s)
 
         for i in range(n):
-            freq = defaultdict(int)
+            freq = Counter()
+            countFreq = Counter()
             for j in range(i, n):
-                freq[s[j]] += 1
+                c = s[j]
+                old = freq[c]
 
-                if len(set(freq.values())) == 1:
+                if old > 0:
+                    countFreq[old] -= 1
+
+                    if countFreq[old] == 0:
+                        del countFreq[old]
+
+                freq[c] += 1
+                countFreq[freq[s[j]]] += 1
+        
+                if len(countFreq) == 1:
                     longest = max(longest, j - i + 1)
         
         return longest
