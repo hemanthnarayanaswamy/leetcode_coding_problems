9efe3bf041d540a46d59d152275994ebbe86a6b7
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-05 09:41:58 -0400

    Time: 2999 ms (28.03%), Space: 19.2 MB (58.57%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -8,10 +8,7 @@ class Solution:
             for j in range(i, n):
                 freq[s[j]] += 1
 
-                values = freq.values()
-                first = next(iter(values))
-
-                if all(v == first for v in values):
+                if min(freq.values()) == max(freq.values()):
                     longest = max(longest, j - i + 1)
         
         return longest
