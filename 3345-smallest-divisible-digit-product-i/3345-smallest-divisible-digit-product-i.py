227bef1cfd60a988a5ad75e77d30166132c1ea3e
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-05 22:25:43 -0400

    Time: 0 ms (100%), Space: 19.4 MB (12.2%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,21 +1,12 @@
 class Solution:
     def smallestNumber(self, n: int, t: int) -> int:
-        def digitProduct(num):
-            arr = []
-
-            while num:
-                num, m = divmod(num, 10) 
-                arr.append(m)
-            
-            ans = 1
-            for a in arr:
-                ans *= a
-            
-            return ans
-        
-        for num in range(n, n+11):
-            prod = digitProduct(num)
-
-            if prod % t == 0:
-                return num
+        for i in range(n,n+1000):
+            x=i
+            pr=1
+            while x>0:
+                pr*=x%10
+                x//=10
+            if pr%t==0:
+                return i
                 
+        
