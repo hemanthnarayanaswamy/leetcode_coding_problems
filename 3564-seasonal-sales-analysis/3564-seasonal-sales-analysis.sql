14a1e334620d7064b653e4cee20f5e0b6fe5083d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-10 11:42:04 -0400

    Time: 564 ms (61.25%), Space: 0B (100%) - LeetHub

--- 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
+++ 3564-seasonal-sales-analysis/3564-seasonal-sales-analysis.sql
@@ -1,37 +1,32 @@
-(select 'Fall' As Season, p.category as category, sum(s.quantity) as total_quantity, sum(quantity*price) as total_revenue
-from sales s
-join products p on p.product_id = s.product_id
-where month(sale_date) in (11, 10, 09)
-group by p.category
-order by total_quantity DESC, total_revenue DESC, p.category
-limit 1)
-
-union all
-
-(select 'Spring' As Season, p.category as category, sum(s.quantity) as total_quantity, sum(quantity*price) as total_revenue
-from sales s
-join products p on p.product_id = s.product_id
-where month(sale_date) in (3, 5, 4)
-group by p.category
-order by total_quantity DESC, total_revenue DESC, p.category
-limit 1)
-
-union all
-
-(select 'Summer' As Season, p.category as category, sum(s.quantity) as total_quantity, sum(quantity*price) as total_revenue
-from sales s
-join products p on p.product_id = s.product_id
-where month(sale_date) in (6, 7, 8)
-group by p.category
-order by total_quantity DESC, total_revenue DESC, p.category
-limit 1)
-
-union all
-
-(select 'Winter' As Season, p.category as category, sum(s.quantity) as total_quantity, sum(quantity*price) as total_revenue
-from sales s
-join products p on p.product_id = s.product_id
-where month(sale_date) in (12, 01, 02)
-group by p.category
-order by total_quantity DESC, total_revenue DESC, p.category
-limit 1)
+WITH season_sales AS (
+    SELECT
+        CASE
+            WHEN MONTH(sale_date) IN (3,4,5) THEN 'Spring'
+            WHEN MONTH(sale_date) IN (6,7,8) THEN 'Summer'
+            WHEN MONTH(sale_date) IN (9,10,11) THEN 'Fall'
+            ELSE 'Winter'
+        END AS season,
+        p.category,
+        SUM(s.quantity) AS total_quantity,
+        SUM(s.quantity * s.price) AS total_revenue
+    FROM sales s
+    JOIN products p
+        ON s.product_id = p.product_id
+    GROUP BY season, p.category
+),
+ranked AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY season
+               ORDER BY total_quantity DESC,
+                        total_revenue DESC,
+                        category
+           ) AS rn
+    FROM season_sales
+)
+SELECT season,
+       category,
+       total_quantity,
+       total_revenue
+FROM ranked
+WHERE rn = 1;
