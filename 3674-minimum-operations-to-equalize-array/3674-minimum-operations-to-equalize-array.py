1a68b6d9a1a48f28b4957a8b184f246f4daa68d0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-10 09:22:10 -0400

    Time: 0 ms (100%), Space: 19.2 MB (56.55%) - LeetHub

--- 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.py
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        if len(set(nums)) == 1:
+        if nums.count(nums[0]) == len(nums):
             return 0
         else:
             return 1
