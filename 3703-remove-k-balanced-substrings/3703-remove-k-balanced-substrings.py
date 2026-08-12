0a7264781e25c219d327cbaaeb44eb110bc297c9
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-12 10:07:08 -0400

    Time: 372 ms (50%), Space: 24.4 MB (50%) - LeetHub

--- 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.py
+++ 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.py
@@ -1,6 +1,28 @@
 class Solution:
     def removeSubstring(self, s: str, k: int) -> str:
-        pattern = "(" * k + ")" * k
-        while pattern in s:
-            s = s.replace(pattern, "")
-        return s
+        stack = []
+        res = ''
+
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+
+            if len(stack) >= 2:
+                if stack[-2][0] == '(' and min(stack[-1][1], stack[-2][1]) >= k:
+                    stack.pop()
+                    if stack[-1][1] == k:
+                        stack.pop()
+                    else:
+                        stack[-1][1] -= k
+        
+        for c, n in stack:
+            res += c * n
+        
+        return res
+
+
+
+
+
