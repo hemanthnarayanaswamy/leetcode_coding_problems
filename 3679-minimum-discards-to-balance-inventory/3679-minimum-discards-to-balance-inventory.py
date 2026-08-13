6a681e66c8564e688b76a0402fa793bce5831dcc
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-13 10:23:32 -0400

    Time: 143 ms (56.86%), Space: 43 MB (72.55%) - LeetHub

--- 3679-minimum-discards-to-balance-inventory/3679-minimum-discards-to-balance-inventory.py
+++ 3679-minimum-discards-to-balance-inventory/3679-minimum-discards-to-balance-inventory.py
@@ -5,16 +5,15 @@ class Solution:
         discardIdx = set()
 
         for curr, item in enumerate(arrivals):
-            inventory[item] += 1
-
             if curr - start + 1 > w:
                 if start not in discardIdx:
                     inventory[arrivals[start]] -= 1
-                start += 1
+                start += 1 
             
-            if inventory[item] > m:
-                inventory[item] -= 1
+            if inventory[item] + 1 > m:
                 discardIdx.add(curr)
+            else:
+                inventory[item] += 1
         
         return len(discardIdx)
 
