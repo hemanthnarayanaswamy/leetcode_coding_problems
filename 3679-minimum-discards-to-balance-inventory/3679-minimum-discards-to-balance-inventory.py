ce827c5842d82748fa627b1cf7cd993205a09832
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-13 10:20:40 -0400

    Time: 175 ms (30.72%), Space: 43.1 MB (40.52%) - LeetHub

---
+++ 3679-minimum-discards-to-balance-inventory/3679-minimum-discards-to-balance-inventory.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minArrivalsToDiscard(self, arrivals: List[int], w: int, m: int) -> int:
+        inventory = defaultdict(int)
+        start = 0
+        discard = 0
+        discardIdx = set()
+
+        for curr, item in enumerate(arrivals):
+            inventory[item] += 1
+
+            while curr - start + 1 > w:
+                if start not in discardIdx:
+                    inventory[arrivals[start]] -= 1
+                start += 1
+            
+            while inventory[item] > m:
+                inventory[item] -= 1
+                discardIdx.add(curr)
+        
+        return len(discardIdx)
+
+
