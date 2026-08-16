af6f032a138893a92927ba3c8be6a1a70e2028e2
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-16 11:12:07 -0400

    Time: 7 ms (9.27%), Space: 19.5 MB (13.93%) - LeetHub

---
+++ 4024-nearest-available-drone/4024-nearest-available-drone.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nearestDrone(self, drones: list[list[int]], target: list[int]) -> int:
+        x, y = target
+        res = -1
+        dist = float('inf')
+
+        for i, drone in enumerate(drones):
+            x2, y2, r = drone
+
+            m = abs(x - x2) + abs(y - y2)
+
+            if m <= r and m < dist:
+                dist = m
+                res = i
+        
+        return res
