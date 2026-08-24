bc605dabfe893cc1172c14ad5560dd541ac7e54d
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-24 12:17:44 -0400

    Time: 258 ms (72.27%), Space: 41.6 MB (31.64%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -1,21 +1,29 @@
 class Solution:
-    def rangeAddQueries(
-        self, n: int, queries: List[List[int]]
-    ) -> List[List[int]]:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
         diff = [[0] * (n + 1) for _ in range(n + 1)]
 
-        for row1, col1, row2, col2 in queries:
+        #  apply all operations using 4-point updates
+        for row1, col1, row2, col2 in queries: 
             diff[row1][col1] += 1
             diff[row2 + 1][col1] -= 1
             diff[row1][col2 + 1] -= 1
             diff[row2 + 1][col2 + 1] += 1
-
-        mat = [[0] * n for _ in range(n)]
         
+        # apply row wise prefix
+        for i in range(n+1):
+            for j in range(1, n+1):
+                diff[i][j] += diff[i][j-1]
+        
+        # apply column wise prefix
+        for j in range(n+1):
+            for i in range(1, n+1):
+                diff[i][j] += diff[i-1][j]
+        
+        # apply the diff to the original array
+        arr = [[0]*n for _ in range(n)]
+
         for i in range(n):
             for j in range(n):
-                x1 = 0 if i == 0 else mat[i - 1][j]
-                x2 = 0 if j == 0 else mat[i][j - 1]
-                x3 = 0 if i == 0 or j == 0 else mat[i - 1][j - 1]
-                mat[i][j] = diff[i][j] + x1 + x2 - x3
-        return mat
+                arr[i][j] = diff[i][j]
+        
+        return arr
