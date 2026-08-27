494720a91a94d7ff75b1684f5db0d096fd237f9a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-27 10:21:58 -0400

    Time: 136 ms (58.95%), Space: 20.2 MB (31.41%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,22 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        m = len(img)
+        n = len(img[0])
+        res = [[0]*n for _ in range(m)]
+
+        def getAvg(x, y):
+            count = 0
+            total = 0
+
+            for i in range(max(0, x-1), min(x+2, m)):
+                for j in range(max(0, y-1), min(y+2, n)):
+                    count += 1
+                    total += img[i][j]
+            
+            return math.floor(total/count)
+        
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = getAvg(i, j)
+
+        return res
