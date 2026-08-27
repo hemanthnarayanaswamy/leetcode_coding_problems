d1a91d0def63f0ce56f99044098f210151d6aafd
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-27 09:40:08 -0400

    Time: 685 ms (17.56%), Space: 27.8 MB (41.7%) - LeetHub

---
+++ 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maximumGap(self, skill: str, station: str) -> int:
+        n1 = len(skill)
+        n2 = len(station)
+
+        if n1 == 1:
+            return 0
+
+        earliest = [-1] * n1
+        latest = [-1] * n1
+
+        l1 = l2 = 0
+        while l1 < n1 and l2 < n2:
+            if skill[l1] == station[l2]:
+                earliest[l1] = l2
+                l1 += 1    
+            l2 += 1
+        
+        r1 = n1 -1
+        r2 = n2 - 1
+        while r1 > -1 and r2 > -1:
+            if skill[r1] == station[r2]:
+                latest[r1] = r2
+                r1 -= 1
+            r2 -= 1
+        
+        gap = 0
+        for i in range(1, n1):
+            gap = max(gap, latest[i]-earliest[i-1])
+        
+        return gap
+
