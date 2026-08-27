7e81d6e6748e4812dec240bc57224583e3ca66c7
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-27 09:51:40 -0400

    Time: 636 ms (23.5%), Space: 24.3 MB (94.64%) - LeetHub

--- 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.py
+++ 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.py
@@ -6,8 +6,8 @@ class Solution:
         if n1 <= 1:
             return 0
 
+        maxGap = 0
         earliest = [0] * n1
-        latest = [0] * n1
 
         l1 = l2 = 0
         while l1 < n1 and l2 < n2:
@@ -15,17 +15,15 @@ class Solution:
                 earliest[l1] = l2
                 l1 += 1    
             l2 += 1
+
         
         r1, r2 = n1 -1, n2 - 1
         while r1 > -1 and r2 > -1:
             if skill[r1] == station[r2]:
-                latest[r1] = r2
+                if r1 - 1 > -1:
+                    maxGap = max(maxGap, r2 - earliest[r1-1])
                 r1 -= 1
             r2 -= 1
-        
-        maxGap = 0
-        for i in range(1, n1):
-            maxGap = max(maxGap, latest[i]-earliest[i-1])
         
         return maxGap
 
