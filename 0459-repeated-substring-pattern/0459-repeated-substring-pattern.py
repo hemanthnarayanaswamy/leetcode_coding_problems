6bffded20853191bd9090f52f43884010dcf06a1
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-28 12:49:15 -0400

    Time: 92 ms (11.48%), Space: 19.8 MB (6.9%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -2,14 +2,21 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
         z = [0] * n 
+        l = r = 0
 
         for i in range(1, n):
-            p = 0 
-
-            while p + i < n and s[p] == s[i+p]:
-                p += 1
+            if i <= r:
+                mirror = i - l
+                z[i] = min(z[mirror], r - i + 1)
             
-            if i + p == n and n % i == 0:
-                return True
+            while z[i] + i < n and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+            
+            if i + z[i] - 1 > r:
+                l = i
+                r = i + z[i] - 1
+            
+            if i + z[i] == n and n % i == 0:
+                return True 
         
         return False
