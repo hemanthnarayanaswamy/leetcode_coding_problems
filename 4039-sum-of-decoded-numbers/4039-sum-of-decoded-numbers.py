2ca22ac06d3b05dd7923197be86aae9f40d89899
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-30 11:31:46 -0400

    Time: 438 ms (47.47%), Space: 32 MB (80.92%) - LeetHub

--- 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.py
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.py
@@ -1,11 +1,11 @@
 class Solution:
     def sumDecoded(self, nums: list[int]) -> int:
         total = 0
-        MOD = 10**9+7
+        MOD = 1000000007
 
         for num in nums:
-            w = num % 10
-            d = str(num // 10)
+            d, w = divmod(num, 10)
+            d = str(d)
             x, y = int(d[:w]), int(d[w:])
             subtotal = pow(x, y, MOD)
             total = (total + subtotal) % MOD
