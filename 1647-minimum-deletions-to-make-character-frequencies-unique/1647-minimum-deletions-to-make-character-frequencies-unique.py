25cb23dc44cea9316f52306649f101f45477fdf8
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-31 09:30:34 -0400

    Time: 58 ms (96.26%), Space: 20.3 MB (49.69%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,18 +1,14 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        arr = sorted(Counter(s).values(), reverse=True)
-        freq_val = Counter(arr)
-        n = len(arr)
-        count = 0
+        char_freq= Counter(s)
+        seen_freq = set()
+        answer = 0
         
-        if n == len(set(arr)):
-            return count
-
-        for f in arr:
-            while f > 0 and freq_val[f] > 1:
-                count += 1
-                freq_val[f] -= 1
-                f -= 1
-                freq_val[f] += 1
-        
-        return count
+        for cnt in char_freq.values():
+            if cnt > 0 and  cnt in seen_freq:
+                while cnt in seen_freq:
+                    cnt -= 1
+                    answer += 1
+            if cnt:
+                seen_freq.add(cnt)
+        return answer
