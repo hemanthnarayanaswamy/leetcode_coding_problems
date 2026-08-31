e5bfacb22559772aa2750ee0eb4ef5c19fdff17a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-31 10:14:36 -0400

    Time: 20 ms (57.1%), Space: 33.6 MB (63.61%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -5,11 +5,7 @@ class Solution:
         i1 = nums.index(min(nums))
         i2 = nums.index(max(nums))
         
-        c1 = max(i1, i2) + 1 # operations to delete from front
-        c2 = n - min(i1, i2) # operations to delete from back
-        c3 = min(i1, i2) + 1 + n - max(i1, i2) # operation to delete from both sides
+        if i1 > i2:
+            i1, i2 = i2, i1
 
-        return min(c1, c2, c3)
-
-
-        
+        return min(i2+1,n-i1,i1+1+n-i2)
