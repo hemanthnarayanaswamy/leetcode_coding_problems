b7b16b5de33e57601c551e62303238d91838c911
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-08-31 09:31:29 -0400

    Time: 66 ms (75.05%), Space: 20.4 MB (14.55%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -3,12 +3,13 @@ class Solution:
         char_freq= Counter(s)
         seen_freq = set()
         answer = 0
-        
+
         for cnt in char_freq.values():
-            if cnt > 0 and  cnt in seen_freq:
-                while cnt in seen_freq:
-                    cnt -= 1
-                    answer += 1
+            while cnt in seen_freq:
+                cnt -= 1
+                answer += 1
+
             if cnt:
                 seen_freq.add(cnt)
+
         return answer
