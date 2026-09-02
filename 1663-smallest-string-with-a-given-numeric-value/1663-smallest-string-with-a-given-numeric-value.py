a5abb2d575f202927141a1b8f8d0f39498febee5
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-02 10:05:30 -0400

    Time: 349 ms (36.06%), Space: 21 MB (35.58%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -2,13 +2,13 @@ class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
         s = 'abcdefghijklmnopqrstuvwxyz'
         res = ''
+        i = len(s)-1
 
         for _ in range(n):
-            for i in range(len(s)-1, -1, -1):
-                if i + n <= k:
-                    res += s[i]
-                    k -= (i+1)
-                    n -= 1
-                    break
+            while i + n > k:
+                i -= 1
+            res += s[i]
+            k -= (i+1)
+            n -= 1
         
         return res[::-1]
