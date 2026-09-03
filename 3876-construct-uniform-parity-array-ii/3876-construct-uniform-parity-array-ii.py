48b4d36813e7e47981020c736f38d76788a25d7c
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-03 10:01:59 -0400

    Time: 159 ms (9.75%), Space: 35.4 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,32 +1,28 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         n = len(nums1)
-        odds = []
-        evens = []
+        min_odd = min((x for x in nums1 if x % 2 == 1), default=float('inf'))
+        min_even = min((x for x in nums1 if x % 2 == 0), default=float('inf'))
 
-        for i in range(n):
-            num = nums1[i]
-            if num % 2:
-                odds.append(num)
-            else:
-                evens.append(num)
-        
-        if not odds or not evens:
+
+        if min_odd == float('inf') or min_even == float('inf'):
             return True
         
-        min_odd = min(odds)
-        min_even = min(evens)
-        e = o = True
-
+        flag = True 
         for i in range(n):
-            num = nums1[i]
-            if num % 2 and num - min_odd < 1:
-                o = False
-            
-            if num % 2 == 0 and num - min_odd < 1:
-                e = False
+            if nums1[i] % 2 and nums1[i] - min_odd < 1:
+                flag = False
+                break
+        
+        if flag:
+            return flag
+        
+        for i in range(n):
+            if nums1[i] % 2 == 0 and nums1[i] - min_odd < 1:
+                return False
+        return True
 
-        return e or o            
+
 
 
         
