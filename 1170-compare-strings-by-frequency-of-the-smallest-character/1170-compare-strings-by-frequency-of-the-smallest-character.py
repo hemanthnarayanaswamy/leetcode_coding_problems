7bed20d45408462814e219e74ba08c889926bbc0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-04 10:07:25 -0400

    Time: 21 ms (37.88%), Space: 19.9 MB (50.51%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -8,27 +8,20 @@ class Solution:
                     return freq[letter]
         
         arrW = []
-        n = len(words)
-
         for w in words:
             arrW.append(f(w))
         
-        arrW.sort()
+        valMap = Counter(arrW)
         res = []
 
         for q in queries:
             x = f(q)
+            count = 0
 
-            l, r = 0, n
-            while l < r:
-                m = (l + r) // 2
-
-                if arrW[m] <= x:
-                    l = m + 1
-                else:
-                    r = m
+            for k, v in valMap.items():
+                if x < k:
+                    count += v
             
-            res.append(n - l) 
+            res.append(count)
         
         return res
-
