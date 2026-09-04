1736452865edbbd515fba18fd86a654ebeafd5ae
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-04 10:06:23 -0400

    Time: 28 ms (28.33%), Space: 19.8 MB (82.94%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -1,30 +1,34 @@
 class Solution:
     def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        letters = 'abcdefghijklmnopqrstuvwxyz'
         def f(s):
             freq = Counter(s)
-            for i in range(ord('a'), ord('z')+1):
-                c = chr(i)
-                if c in freq:
-                    return freq[c]
+            for letter in letters:
+                if letter in freq:
+                    return freq[letter]
         
         arrW = []
+        n = len(words)
 
         for w in words:
             arrW.append(f(w))
         
-        valMap = Counter(arrW)
+        arrW.sort()
         res = []
 
         for q in queries:
             x = f(q)
-            count = 0
 
-            for k, v in valMap.items():
-                if x < k:
-                    count += v
+            l, r = 0, n
+            while l < r:
+                m = (l + r) // 2
+
+                if arrW[m] <= x:
+                    l = m + 1
+                else:
+                    r = m
             
-            res.append(count)
+            res.append(n - l) 
         
-        print(arrW, valMap)
         return res
 
