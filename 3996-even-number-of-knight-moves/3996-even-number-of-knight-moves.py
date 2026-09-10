d45383c2886b85305c70139e65e5efc417b42a7f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-10 10:45:03 -0400

    Time: 2 ms (24%), Space: 19.1 MB (97.25%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.py
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.py
@@ -1,9 +1,10 @@
 class Solution:
     def canReach(self, start: list[int], target: list[int]) -> bool:
-        if (start[0] % 2 == start[1] % 2) and (target[0] % 2 == target[1] % 2):
-            return True
+        x0, y0 = start
+        x, y = target
+
+        flag_start = (x0 + y0) % 2 == 0
+        flag_finish = (x + y) % 2 == 0
+
+        return flag_start == flag_finish
         
-        if (start[0] % 2 != start[1] % 2) and (target[0] % 2 != target[1] % 2):
-            return True
-        
-        return False
