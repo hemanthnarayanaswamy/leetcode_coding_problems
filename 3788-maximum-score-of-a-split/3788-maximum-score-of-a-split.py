df4f9a51d913bab566aca67695e33b226c3abd25
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-11 10:04:36 -0400

    Time: 194 ms (40.12%), Space: 37.5 MB (93.6%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.py
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.py
@@ -3,21 +3,18 @@ class Solution:
         n = len(nums)
         res = []
 
-        prefixSum = [0]*n
         suffixMin = [0]*n
+        prefix = 0
 
-        for i in range(n):
-            if i > 0:
-                prefixSum[i] = prefixSum[i-1] + nums[i]
-            else:
-                prefixSum[i] = nums[i]
-        
         for i in range(n-1, -1, -1):
             if i < n-1:
                 suffixMin[i] = min(suffixMin[i+1], nums[i+1])
-                res.append(prefixSum[i] - suffixMin[i])
             else:
                 suffixMin[i] = nums[i]
+
+        for i in range(n-1):
+            prefix += nums[i]
+            res.append(prefix - suffixMin[i])
         
         return max(res)
         
