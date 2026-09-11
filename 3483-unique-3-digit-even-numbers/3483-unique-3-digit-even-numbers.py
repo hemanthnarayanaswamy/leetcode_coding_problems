33c26107caa8b7171bcacb64542f6ea59c1a9700
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-11 09:43:42 -0400

    Time: 26 ms (41.1%), Space: 19.4 MB (38.46%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,17 +1,21 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        freq = Counter(digits)
+        even = set()
+        n = len(digits)
         res = 0
 
-        for num in range(100, 1000, 2):
-            needed = Counter([int(d) for d in str(num)])
-            valid = all(freq[d] >= needed[d] for d in needed)
-            
-            if valid:
-                res += 1
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    if i in (j, k) or j in (i, k) or digits[i] == 0 or digits[k] % 2:
+                        continue
+
+                    num = 100*digits[i]+10*digits[j]+digits[k]
+
+                    if num not in even:
+                        res += 1
+                        even.add(num)
         
         return res
-
-
 
 
