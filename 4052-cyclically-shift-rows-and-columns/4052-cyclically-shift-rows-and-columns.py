6fd4e5e6c5491c764b8fc09f41c6755aefb685d0
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-14 10:15:32 -0400

    Time: 4 ms (100%), Space: 19.3 MB (100%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.py
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.py
@@ -1,20 +1,18 @@
 class Solution:
     def cyclicShift(self, n: int, grid: list[list[int]], rowShift: list[int], colShift: list[int]) -> list[list[int]]:
-        n = len(rowShift)
-
         for i in range(n):
             k = rowShift[i]
             grid[i] = grid[i][k:]+grid[i][:k]
         
         for j in range(n):
-            k = colShift[j]
-            tmp = []
-            if k:
-                for i in range(n):
-                    tmp.append(grid[i][j])
-                tmp = tmp[k:] + tmp[:k]
+            k = colShift[j] % n
 
-                for i in range(n):
-                    grid[i][j] = tmp[i]
-        
+            if k == 0:
+                continue
+            cols = [grid[i][j] for i in range(n)]
+            cols = cols[k:] + cols[:k] 
+
+            for i in range(n):
+                grid[i][j] = cols[i]
+    
         return grid
