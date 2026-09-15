a23c1c2cdc49cd3ad7dabddf668e6af9ffb1ad08
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-15 09:27:22 -0400

    Time: 83 ms (75.57%), Space: 19.3 MB (54.2%) - LeetHub

--- 3722-lexicographically-smallest-string-after-reverse/3722-lexicographically-smallest-string-after-reverse.py
+++ 3722-lexicographically-smallest-string-after-reverse/3722-lexicographically-smallest-string-after-reverse.py
@@ -6,6 +6,11 @@ class Solution:
         for i in range(n):
             tmp1 = s[:i][::-1] + s[i:]
             tmp2 = s[:i]+ s[i:][::-1] 
-            res = min(tmp1, tmp2, res)
+            
+            if tmp1 > tmp2:
+                tmp1 = tmp2
+            
+            if res > tmp1:
+                res = tmp1
 
         return res
