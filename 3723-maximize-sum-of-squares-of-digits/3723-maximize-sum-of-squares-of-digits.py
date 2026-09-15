fdae96f91b896b76c9fe7e7d6c835685b9baeafa
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-15 09:14:07 -0400

    Time: 0 ms (100%), Space: 20.9 MB (87.8%) - LeetHub

--- 3723-maximize-sum-of-squares-of-digits/3723-maximize-sum-of-squares-of-digits.py
+++ 3723-maximize-sum-of-squares-of-digits/3723-maximize-sum-of-squares-of-digits.py
@@ -1,14 +1,7 @@
 class Solution:
-    def maxSumOfSquares(self, num: int, sum: int) -> str:
-        res = ""
-        if sum > 9 * num:
-            return res
+    def maxSumOfSquares(self, n: int, s: int) -> str:
+        if s > n * 9: return ''
+        q, r = divmod(s, 9)
         
-        while sum > 9:
-            res += str(9)
-            sum -= 9
-        
-        res += str(sum)
-        res += '0'*(num - len(res))
-
-        return res
+        res = '9' * q + (str(r) if r else '')
+        return res + '0' * (n - len(res))
