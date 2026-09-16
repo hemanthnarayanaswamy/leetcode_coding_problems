337355f6041d4497fbbf19bd11cf839dad324995
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-16 09:08:16 -0400

    Time: 131 ms (45.38%), Space: 19.3 MB (35.38%) - LeetHub

--- 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.py
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.py
@@ -1,20 +1,18 @@
 class Solution:
     def minAllOneMultiple(self, k: int) -> int:
-        if k % 2 == 0: # even k can't divide 11...
+        if k % 2 == 0 or k % 5 == 0:
             return -1
 
-        seen = {1}
-        num = 1
-        count = 1
+        rem = 0
+        seen = set()
 
-        while num % k != 0:
-            num = ((num * 10)+1) % k 
-            if num in seen:
+        for cnt in range(k):
+            rem = (rem * 10 + 1) % k
+
+            if rem == 0: 
+                return cnt+1
+
+            if rem in seen:
                 return -1
-                
-            seen.add(num)
-            count += 1
-        
-        return count
-        
 
+        return -1
