d0456628950dbc349809a750d212d5bd77d38309
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-16 09:39:22 -0400

    Time: 84 ms (100%), Space: 35.3 MB (53.93%) - LeetHub

--- 3424-minimum-cost-to-make-arrays-identical/3424-minimum-cost-to-make-arrays-identical.py
+++ 3424-minimum-cost-to-make-arrays-identical/3424-minimum-cost-to-make-arrays-identical.py
@@ -1,14 +1,13 @@
 class Solution:
     def minCost(self, arr: List[int], brr: List[int], k: int) -> int:
-        if arr == brr:
-            return 0
-        
-        c1 = sum([abs(a-b) for a,b in zip(arr, brr)])
-    
-        sortedArr = sorted(arr)
-        sortedBrr = sorted(brr)
+        n = len(arr)
+        c1 = sum([abs(arr[i] - brr[i]) for i in range(n)])
 
-        c2 = sum([abs(a-b) for a,b in zip(sortedArr, sortedBrr)])
-        c2 += k
+        if c1 <= k:
+            return c1
+
+        arr.sort()
+        brr.sort()
+        c2 = k + sum([abs(arr[i] - brr[i]) for i in range(n)])
 
         return min(c1, c2)
