2e91f8ec8ab549e5f520f4f9bce45feb227825fd
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-17 09:13:31 -0400

    Time: 441 ms (5.59%), Space: 21.5 MB (41.61%) - LeetHub

---
+++ 3412-find-mirror-score-of-a-string/3412-find-mirror-score-of-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calculateScore(self, s: str) -> int:
+        prev = defaultdict(list)
+        score = 0
+
+        for i, w in enumerate(s):
+            mir = chr(ord('z')-ord(w)+97)
+            print(mir, score)
+            if mir in prev and prev[mir]:
+                j = prev[mir].pop()
+                score += i - j
+            else:
+                prev[w].append(i)
+
+        return score
