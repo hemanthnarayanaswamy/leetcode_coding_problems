a25e92414d6bd80f9fae44c2b0e7698a55ef9e62
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-18 12:05:31 -0400

    Time: 50 ms (85.98%), Space: 20.1 MB (90.65%) - LeetHub

---
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freq = sorted(Counter(word).values())
+        deletions = float('inf')
+
+        for f1 in freq:
+            base = f1
+            tmp = []
+            for f2 in freq:
+                if f2 < base:
+                    tmp.append(f2)
+                elif f2 > base and f2 - base > k:
+                    tmp.append(f2 - base - k)
+                else:
+                    tmp.append(0)
+
+            
+            if tmp and sum(tmp) < deletions:
+                deletions = sum(tmp)
+        
+        return deletions if deletions != float('inf') else 0
+
