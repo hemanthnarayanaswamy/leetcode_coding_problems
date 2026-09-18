049badd2c60719c1eee1231be54598be8597b07f
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-18 12:56:30 -0400

    Time: 7 ms (95.36%), Space: 25 MB (66.49%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -2,9 +2,9 @@ class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
         ans = 0
         max_pos = 0
+        
         for step, pos in enumerate(flips, 1):
-            if pos > max_pos:
-                max_pos = pos
+            max_pos = max(max_pos, pos)
             if max_pos == step:
                 ans += 1
         return ans
