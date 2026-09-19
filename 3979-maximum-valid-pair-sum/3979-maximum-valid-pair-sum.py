8bfea3913943b0d018cbad34893fb0829bae8dfc
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-19 12:49:25 -0400

    Time: 427 ms (5.03%), Space: 33.6 MB (5.69%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxValidPairSum(self, nums: list[int], k: int) -> int:
+        total = 0
+        n = len(nums)
+        maxTracker = [0]*n
+
+        for i in range(n-1, -1, -1):
+            if i == n-1:
+                maxTracker[i] = nums[i]
+            else:
+                maxTracker[i] = max(maxTracker[i+1], nums[i])
+
+        print(maxTracker)
+        for i in range(n-k):
+            print(i, i+k)
+            total = max(total, nums[i]+maxTracker[i+k])
+            
+        return total
