8d86d45c4d560290806eaf53a087a8ba49605068
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-21 09:39:58 -0400

    Time: 75 ms (100%), Space: 19.3 MB (100%) - LeetHub

--- 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.py
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.py
@@ -1,6 +1,5 @@
 class Solution:
     def countIntersectingIntervals(self, intervals: list[list[int]]) -> int:
-        intervals.sort(key=lambda x: (x[0],x[1]))
         n = len(intervals)
         intersect = 0
 
@@ -9,7 +8,7 @@ class Solution:
             for j in range(i+1, n):
                 sj, ej = intervals[j]
 
-                if sj <= ei:
+                if max(si, sj) <= min(ei, ej):
                     intersect += 1
         
         return intersect
