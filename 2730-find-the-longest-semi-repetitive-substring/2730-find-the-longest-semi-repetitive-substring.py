c36252748f4d6f1106690c233785733c80d02a7a
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-21 10:44:31 -0400

    Time: 14 ms (38.34%), Space: 19.3 MB (51.81%) - LeetHub

---
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        n = len(s)
+
+        if n < 3:
+            return n
+
+        left = res = repeat= 0 
+
+        for right in range(1, n):
+            if s[right-1] == s[right]:
+                repeat += 1
+            
+            while repeat > 1 and left < right:
+                if s[left] == s[left + 1]:
+                    repeat -= 1
+                left += 1
+            
+            res = max(res, right - left + 1)
+        
+        return res
+
+
