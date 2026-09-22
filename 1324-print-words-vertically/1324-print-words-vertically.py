386a33d17cae2bea3a563aa6dabf3c7ca43b2faf
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-22 10:03:10 -0400

    Time: 56 ms (7.83%), Space: 19.3 MB (64.78%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -0,0 +1,23 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        arr = s.split()
+        matrix = []
+
+        maxLen = 0
+        for letter in arr:
+            maxLen = max(maxLen, len(letter))
+        
+        for letter in arr:
+            tmp = [w for w in letter]
+            if len(letter) < maxLen:
+                tmp.extend([" "]*(maxLen - len(letter)))
+            
+            matrix.append(tmp)
+        
+        res = []
+
+        for ver in zip(*matrix):
+            res.append(''.join(ver).rstrip())
+
+        return res
+
