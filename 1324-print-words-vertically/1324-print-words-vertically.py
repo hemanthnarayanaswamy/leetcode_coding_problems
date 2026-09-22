71d6da4737b90c9a843042ac7a23adda08bbd707
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-22 10:12:08 -0400

    Time: 47 ms (41.74%), Space: 19.4 MB (28.7%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -2,15 +2,16 @@ class Solution:
     def printVertically(self, s: str) -> List[str]:
         words = s.split()
         max_len = max(len(word) for word in words)
-        ans=[]
+        res = []
 
-        for i in range(max_len): # for each index
-            toadd=""
-            for word in words:  # Get the letter at each index
-                if i<len(word):
-                    toadd+=word[i]
+        for i in range(max_len):
+            newWord = ''
+            for word in words:
+                if i < len(word):
+                    newWord += word[i]
                 else:
-                    toadd+=" "
-            ans.append(toadd.rstrip(" "))
+                    newWord += " "
+            res.append(newWord.rstrip())
+        
+        return res
             
-        return ans
