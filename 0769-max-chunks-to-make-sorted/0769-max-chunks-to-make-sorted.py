fe7282561da71a6383fb11f3cf28cd173b2408f4
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-24 09:22:30 -0400

    Time: 0 ms (100%), Space: 19.1 MB (97.88%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,11 +1,16 @@
 class Solution:
     def maxChunksToSorted(self, arr: list[int]) -> int:
         total = chunk = 0 
+        actualSum = [0]
+
+        for i in range(1, len(arr)):
+            actualSum.append(actualSum[i-1] + i)
+
 
         for i, num in enumerate(arr):
             total += num 
 
-            if i * (i+1) // 2 == total:
+            if actualSum[i] == total:
                 chunk += 1
         
         return chunk
