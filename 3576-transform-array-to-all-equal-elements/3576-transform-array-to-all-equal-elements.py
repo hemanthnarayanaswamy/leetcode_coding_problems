296035cee42fa147fb597c26b8dab457552a50db
Hemanth Narayanaswamy <h_araya@live.concordia.ca>
2026-09-25 09:21:36 -0400

    Time: 94 ms (90.12%), Space: 29.9 MB (6.17%) - LeetHub

--- 3576-transform-array-to-all-equal-elements/3576-transform-array-to-all-equal-elements.py
+++ 3576-transform-array-to-all-equal-elements/3576-transform-array-to-all-equal-elements.py
@@ -1,31 +1,34 @@
 class Solution:
     def canMakeEqual(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-        ones = nums.count(1)
-        nones = n - ones
-
-        if ones % 2 and nones % 2:
-            return False
-        
-        if ones == n or nones == n:
-            return True
-
-        def convert(target):
-            arr = []
-            for i, num in enumerate(nums):
-                if num == target:
-                    arr.append(i)
-            
-            if len(arr) % 2:
-                return False
-            
+        def convert(arr):
             opex = sum([arr[i+1]-arr[i] for i in range(0, len(arr), 2)])
                     
             if opex > k:
                 return False
             
             return True
+
+        n = len(nums)
+        arr1 = []
+        arr2 = []
+
+        for i, num in enumerate(nums):
+            if num == 1:
+                arr1.append(i)
+            else:
+                arr2.append(i)
+
+        n1 = len(arr1)
+        n2 = n - n1
         
-        print(convert(1))
-        print(convert(-1))
-        return convert(1) or convert(-1)
+        if n1 == n or n2 == n:
+            return True
+
+        R1 = False if n1 % 2 else convert(arr1)
+        R2 = False if n2 % 2 else convert(arr2)
+
+        return R1 or R2
+        
+        
+        
+        return convert(arr1, n1) or convert(arr2, )
